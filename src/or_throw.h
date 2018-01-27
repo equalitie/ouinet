4c8fb45031b1533c97b8ef82ee62d8c954703753
Peter Jankuliak <p.jankuliak@gmail.com>
2018-01-27 12:40:41 +0100

    Describe the or_throw function

--- src/or_throw.h
+++ src/or_throw.h
@@ -2,6 +2,48 @@
 
 namespace ouinet {
 
+/*
+ * // This is a utility function to help working with asio::yield_context. In
+ * // particular, yield_context is missing some API to determine whether the
+ * // caller expects the async call to throw, or set an error code hiding in
+ * // yield.
+ *
+ * // Consider an implementation this function:
+ *
+ * int my_async_function(yield_context yield) {
+ *    ...
+ *
+ *    // Here we don't know whether the caller called this function as:
+ *    //
+ *    // A1: `my_async_function(yield);`
+ *    // Or
+ *    // B1: `my_async_function(yield[ec]);`
+ *    //
+ *    // If we want to "yield" an error, we don't know whether to do it
+ *    // with:
+ *    //
+ *    // A2: `throw system_error(my_error_code);`
+ *    // Or, assuming there exists a yield_context::set_error function
+ *    // B2: `yield.set_error(my_error_code); return {};`
+ *    //
+ *    // Note that A2 can only be used in context of A1 and B2 in context of
+ *    // B1.
+ *    //
+ *    // Thus, we need to either explicitly distinguish between A1 and B1 by
+ *    // inspecting the `yield.ec_` pointer to an error_code instance, or use
+ *    // the below defined function `or_throw` which does it for us.
+ *
+ *    return or_throw(yield, my_error, 42);
+ *
+ *    // Note that `or_throw` is always meant to be used after the `return`
+ *    // keyword (hence the name). Not using it with return would - again -
+ *    // lead to two non deterministic code paths (depending on whether
+ *    // we're in the A1 or B1 context).
+ *    ...
+ * }
+ *
+ */
+
 template<class Ret>
 inline
 Ret or_throw( asio::yield_context yield
