56fed661548a64faf9dc476aeba7b2caf488dc93
Peter Jankuliak <p.jankuliak@gmail.com>
2018-02-09 14:40:52 +0100

    Simplify scripts/build-android.sh

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -15,6 +15,9 @@ NDK_ARCH=arm
 NDK_STL='libc++'
 NDK_TOOLCHAIN_DIR=${DIR}/${NDK}-toolchain-android$NDK_PLATFORM-$NDK_ARCH-$NDK_STL
 
+BOOST_V=1_65_1
+BOOST_V_DOT=${BOOST_V//_/.} # 1.65.1
+
 ABI=armeabi-v7a
 
 ######################################################################
@@ -50,31 +53,31 @@ fi
 
 if [ ! -d "Boost-for-Android/build" ]; then
     cd Boost-for-Android
-    ./build-android.sh --arch=${ABI} --with-libraries=context,coroutine,program_options,system,test,thread,filesystem,date_time $NDK_DIR
+    ./build-android.sh \
+        --boost=${BOOST_V_DOT} \
+        --arch=${ABI} \
+        --with-libraries=context,coroutine,program_options,system,test,thread,filesystem,date_time \
+        $NDK_DIR
     cd ..
 fi
-
-######################################################################
-TOOLCHAIN_FILE=${DIR}/toolchain-android${NDK_PLATFORM}-${NDK_ARCH}-llvm.cmake
-if [ -f "$TOOLCHAIN_FILE" ]; then rm "$TOOLCHAIN_FILE"; fi
-
-echo "set(CMAKE_SYSTEM_NAME Android)"                                          >> ${TOOLCHAIN_FILE}
-echo "set(CMAKE_SYSTEM_VERSION ${NDK_PLATFORM})"                               >> ${TOOLCHAIN_FILE}
-echo "set(CMAKE_SYSTEM_PROCESSOR ${CMAKE_SYSTEM_PROCESSOR})"                   >> ${TOOLCHAIN_FILE}
-echo "set(CMAKE_ANDROID_ARCH_ABI ${ABI})"                                      >> ${TOOLCHAIN_FILE}
-echo "set(CMAKE_ANDROID_STANDALONE_TOOLCHAIN ${NDK_TOOLCHAIN_DIR})"            >> ${TOOLCHAIN_FILE}
-echo "set(BOOST_INCLUDEDIR ${DIR}/Boost-for-Android/build/out/${ABI}/include/boost-1_65_1)" >> ${TOOLCHAIN_FILE}
-echo "set(BOOST_LIBRARYDIR ${DIR}/Boost-for-Android/build/out/${ABI}/lib)"     >> ${TOOLCHAIN_FILE}
-echo "set(CMAKE_C_COMPILER ${NDK_TOOLCHAIN_DIR}/bin/clang)"                    >> ${TOOLCHAIN_FILE}
-echo "set(CMAKE_CXX_COMPILER ${NDK_TOOLCHAIN_DIR}/bin/clang++)"                >> ${TOOLCHAIN_FILE}
 
 ######################################################################
 mkdir -p build-ipfs-cache
 cd build-ipfs-cache
 
-cmake -DBoost_COMPILER="-clang" \
-    -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE \
+cmake \
+    -DBoost_COMPILER="-clang" \
+    -DCMAKE_SYSTEM_NAME=Android \
+    -DCMAKE_SYSTEM_VERSION=${NDK_PLATFORM} \
+    -DCMAKE_SYSTEM_PROCESSOR=${CMAKE_SYSTEM_PROCESSOR} \
+    -DCMAKE_ANDROID_ARCH_ABI=${ABI} \
+    -DCMAKE_ANDROID_STANDALONE_TOOLCHAIN=${NDK_TOOLCHAIN_DIR} \
+    -DBOOST_INCLUDEDIR=${DIR}/Boost-for-Android/build/out/${ABI}/include/boost-${BOOST_V} \
+    -DBOOST_LIBRARYDIR=${DIR}/Boost-for-Android/build/out/${ABI}/lib \
+    -DCMAKE_C_COMPILER=${NDK_TOOLCHAIN_DIR}/bin/clang \
+    -DCMAKE_CXX_COMPILER=${NDK_TOOLCHAIN_DIR}/bin/clang++ \
     ${ROOT}/modules/ipfs-cache
-make -B
+
+make
 
 cd ..
