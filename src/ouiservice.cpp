f33cab185f7521f766fd0683e762785a9031575f
Ruud Koolen <redlizard@redlizard.nl>
2018-03-02 07:22:12 +0100

    Corrected inconsistent indentation.

--- src/ouiservice.cpp
+++ src/ouiservice.cpp
@@ -9,55 +9,55 @@
 namespace ouinet {
 
 OuiServiceServer::OuiServiceServer(asio::io_service& ios):
-	_ios(ios),
-	_is_listening(false),
-	_is_canceling_listen(false),
-	_is_accepting(false),
-	_is_canceling_accept(false)
+    _ios(ios),
+    _is_listening(false),
+    _is_canceling_listen(false),
+    _is_accepting(false),
+    _is_canceling_accept(false)
 {}
 
 void OuiServiceServer::add(std::unique_ptr<OuiServiceImplementationServer> implementation)
 {
-	_implementations.push_back(std::move(implementation));
+    _implementations.push_back(std::move(implementation));
 }
 
 // NOTE Leaky abstraction: Even if we do start_listen in parallel, this API
 // forces all the implementations to wait for the slowest one.
 void OuiServiceServer::start_listen(asio::yield_context yield)
 {
-	if (_is_listening) {
-		return or_throw(yield, asio::error::invalid_argument);
-	}
-	
-	Blocker blocker(_ios);
-	
-	for (auto& it : _implementations) {
-		asio::spawn(_ios, [this, implementation = it.get(), b = blocker.make_block()] (asio::yield_context yield) {
-			sys::error_code ec;
-			implementation->start_listen(yield[ec]);
-			if (ec) return;
-			_listening_implementations.push_back(implementation);
-		});
-	}
-	
-	blocker.wait(yield);
-	
-	if (_listening_implementations.empty()) {
-		// TODO: Figure out how to use custom error codes
-		return or_throw(yield, asio::error::connection_refused);
-	}
-	
-	_is_listening = true;
+    if (_is_listening) {
+        return or_throw(yield, asio::error::invalid_argument);
+    }
+
+    Blocker blocker(_ios);
+
+    for (auto& it : _implementations) {
+        asio::spawn(_ios, [this, implementation = it.get(), b = blocker.make_block()] (asio::yield_context yield) {
+            sys::error_code ec;
+            implementation->start_listen(yield[ec]);
+            if (ec) return;
+            _listening_implementations.push_back(implementation);
+        });
+    }
+
+    blocker.wait(yield);
+
+    if (_listening_implementations.empty()) {
+        // TODO: Figure out how to use custom error codes
+        return or_throw(yield, asio::error::connection_refused);
+    }
+
+    _is_listening = true;
 }
 
 void OuiServiceServer::stop_listen(asio::yield_context yield)
 {
-	for (auto it : _listening_implementations) {
-		it->stop_listen(yield);
-	}
-	
-	_listening_implementations.clear();
-	_is_listening = false;
+    for (auto it : _listening_implementations) {
+        it->stop_listen(yield);
+    }
+
+    _listening_implementations.clear();
+    _is_listening = false;
 }
 
 // NOTE: Leaky abstraction: In the original architecture, each individual
@@ -71,85 +71,85 @@ void OuiServiceServer::stop_listen(asio::yield_context yield)
 // devs prefer to optimize extensivel, not the other way around.
 GenericConnection OuiServiceServer::accept(asio::yield_context yield)
 {
-	if (!_is_listening) {
-		// TODO: Figure out how to use custom error codes
-		return or_throw<GenericConnection>(yield, asio::error::invalid_argument);
-	}
-	
-	assert(!_is_accepting);
-	assert(!_is_canceling_accept);
-	_is_accepting = true;
-	
-	Blocker blocker(_ios);
-	
-	bool accepted = false;
-	sys::error_code first_error; // Only set if accepted == false
-	GenericConnection connection;
-	
-	for (auto it : _listening_implementations) {
-		asio::spawn(_ios, [
-			  this
-			, it
-			, &connection
-			, &accepted
-			, &first_error
-			, b = blocker.make_block()
-		] (asio::yield_context yield) {
-			sys::error_code ec;
-			connection = it->accept(yield[ec]);
-			if (accepted) return;
-			if (ec && !first_error) first_error = ec;
-			if (ec) return;
-			accepted = true;
-			for (auto it_ : _listening_implementations) {
-				if (it == it_ || !it_->is_accepting()) continue;
-				sys::error_code ec;
-				it_->cancel_accept();
-			}
-		});
-	}
-	
-	blocker.wait(yield);
-	
-	_is_accepting = false;
-	return or_throw(yield, first_error, std::move(connection));
+    if (!_is_listening) {
+        // TODO: Figure out how to use custom error codes
+        return or_throw<GenericConnection>(yield, asio::error::invalid_argument);
+    }
+
+    assert(!_is_accepting);
+    assert(!_is_canceling_accept);
+    _is_accepting = true;
+
+    Blocker blocker(_ios);
+
+    bool accepted = false;
+    sys::error_code first_error; // Only set if accepted == false
+    GenericConnection connection;
+
+    for (auto it : _listening_implementations) {
+        asio::spawn(_ios, [
+              this
+            , it
+            , &connection
+            , &accepted
+            , &first_error
+            , b = blocker.make_block()
+        ] (asio::yield_context yield) {
+            sys::error_code ec;
+            connection = it->accept(yield[ec]);
+            if (accepted) return;
+            if (ec && !first_error) first_error = ec;
+            if (ec) return;
+            accepted = true;
+            for (auto it_ : _listening_implementations) {
+                if (it == it_ || !it_->is_accepting()) continue;
+                sys::error_code ec;
+                it_->cancel_accept();
+            }
+        });
+    }
+
+    blocker.wait(yield);
+
+    _is_accepting = false;
+    return or_throw(yield, first_error, std::move(connection));
 }
 
 void OuiServiceServer::cancel_accept()
 {
-	assert(!_is_canceling_accept);
-	_is_canceling_accept = true;
-	
-	if (!_is_accepting) return;
-	
-	for (auto it : _listening_implementations) {
-		if (!it->is_accepting()) continue;
-		it->cancel_accept();
-	}
-	
-	_is_canceling_accept = false;
+    assert(!_is_canceling_accept);
+    _is_canceling_accept = true;
+
+    if (!_is_accepting) return;
+
+    for (auto it : _listening_implementations) {
+        if (!it->is_accepting()) continue;
+        it->cancel_accept();
+    }
+
+    _is_canceling_accept = false;
 }
 
 OuiServiceClient::OuiServiceClient(asio::io_service& ios):
-	_ios(ios)
+    _ios(ios)
 {}
 
 void OuiServiceClient::add(std::unique_ptr<OuiServiceImplementationClient> implementation)
 {
-	assert(_implementations.empty());
-	_implementations.push_back(std::move(implementation));
+    assert(_implementations.empty());
+    _implementations.push_back(std::move(implementation));
 }
 
 GenericConnection OuiServiceClient::connect(asio::yield_context yield)
 {
-	assert(_implementations.size() == 1);
-	return _implementations[0]->connect(yield);
+    assert(_implementations.size() == 1);
+    return _implementations[0]->connect(yield);
 }
 
 void OuiServiceClient::cancel_connect()
 {
-	assert(_implementations.size() == 1);
-	_implementations[0]->cancel_connect();
+    assert(_implementations.size() == 1);
+    _implementations[0]->cancel_connect();
 }
 
 } // ouinet namespace
--- src/ouiservice.h
+++ src/ouiservice.h
@@ -9,48 +9,48 @@ namespace ouinet {
 
 class OuiServiceImplementationServer
 {
-	public:
-	virtual ~OuiServiceImplementationServer() {}
-	
-	virtual void start_listen(asio::yield_context yield) = 0;
-	virtual void stop_listen(asio::yield_context yield) = 0;
-	
-	virtual GenericConnection accept(asio::yield_context yield) = 0;
-	virtual void cancel_accept() = 0;
-	
-	virtual bool is_accepting() = 0;
+    public:
+    virtual ~OuiServiceImplementationServer() {}
+
+    virtual void start_listen(asio::yield_context yield) = 0;
+    virtual void stop_listen(asio::yield_context yield) = 0;
+
+    virtual GenericConnection accept(asio::yield_context yield) = 0;
+    virtual void cancel_accept() = 0;
+
+    virtual bool is_accepting() = 0;
 };
 
 class OuiServiceServer
 {
-	public:
-	OuiServiceServer(asio::io_service& ios);
-	
-	void add(std::unique_ptr<OuiServiceImplementationServer> implementation);
-	
-	void start_listen(asio::yield_context yield);
-	void stop_listen(asio::yield_context yield);
-	
-	GenericConnection accept(asio::yield_context yield);
-	void cancel_accept();
-	
-	private:
-	asio::io_service& _ios;
-	std::vector<std::unique_ptr<OuiServiceImplementationServer>> _implementations;
-	bool _is_listening;
-	bool _is_canceling_listen;
-	bool _is_accepting;
-	bool _is_canceling_accept;
-	std::vector<OuiServiceImplementationServer*> _listening_implementations;
+    public:
+    OuiServiceServer(asio::io_service& ios);
+
+    void add(std::unique_ptr<OuiServiceImplementationServer> implementation);
+
+    void start_listen(asio::yield_context yield);
+    void stop_listen(asio::yield_context yield);
+
+    GenericConnection accept(asio::yield_context yield);
+    void cancel_accept();
+
+    private:
+    asio::io_service& _ios;
+    std::vector<std::unique_ptr<OuiServiceImplementationServer>> _implementations;
+    bool _is_listening;
+    bool _is_canceling_listen;
+    bool _is_accepting;
+    bool _is_canceling_accept;
+    std::vector<OuiServiceImplementationServer*> _listening_implementations;
 };
 
 class OuiServiceImplementationClient
 {
-	public:
-	virtual ~OuiServiceImplementationClient() {}
-	
-	virtual GenericConnection connect(asio::yield_context yield) = 0;
-	virtual void cancel_connect() = 0;
+    public:
+    virtual ~OuiServiceImplementationClient() {}
+
+    virtual GenericConnection connect(asio::yield_context yield) = 0;
+    virtual void cancel_connect() = 0;
 };
 
 /*
@@ -59,17 +59,17 @@ class OuiServiceImplementationClient
  */
 class OuiServiceClient
 {
-	public:
-	OuiServiceClient(asio::io_service& ios);
-	
-	void add(std::unique_ptr<OuiServiceImplementationClient> implementation);
-	
-	GenericConnection connect(asio::yield_context yield);
-	void cancel_connect();
-	
-	private:
-	asio::io_service& _ios;
-	std::vector<std::unique_ptr<OuiServiceImplementationClient>> _implementations;
+    public:
+    OuiServiceClient(asio::io_service& ios);
+
+    void add(std::unique_ptr<OuiServiceImplementationClient> implementation);
+
+    GenericConnection connect(asio::yield_context yield);
+    void cancel_connect();
+
+    private:
+    asio::io_service& _ios;
+    std::vector<std::unique_ptr<OuiServiceImplementationClient>> _implementations;
 };
 
 } // ouinet namespace
