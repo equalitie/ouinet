e52dfb5335fc0a69451664dcc9d706571b3e0625
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2018-04-03 15:07:38 -0400

    Derive architecture variables from ``$ABI``
    Have OpenSSL build and NDK toolchain preparation make use of these variables.

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -9,6 +9,44 @@ APP_ROOT="${ROOT}/android/browser"
 APK="${APP_ROOT}/build/outputs/apk/debug/browser-debug.apk"
 APK_ID=$(sed -En 's/^\s*\bapplicationId\s+"([^"]+)".*/\1/p' "${APP_ROOT}/build.gradle")
 
+# https://developer.android.com/ndk/guides/abis.html
+ABI=${ABI:-armeabi-v7a}
+
+# Derive other variables from the selected ABI.
+# See `$NDK/build/tools/make_standalone_toolchain.py:get_{triple,abis}()`.
+# See `$OPENSSL/config`.
+if [ "$ABI" = "armeabi-v7a" ]; then
+    NDK_ARCH="arm"
+    NDK_TOOLCHAIN_TARGET="arm-linux-androideabi"
+    CMAKE_SYSTEM_PROCESSOR="armv7-a"
+    OPENSSL_MACHINE="armv7"
+elif [ "$ABI" = "arm64-v8a" ]; then
+    NDK_ARCH="arm64"
+    NDK_TOOLCHAIN_TARGET="aarch64-linux-android"
+    CMAKE_SYSTEM_PROCESSOR="aarch64"
+    OPENSSL_MACHINE="arm64"
+elif [ "$ABI" = "armeabi" ]; then
+    NDK_ARCH="arm"
+    NDK_TOOLCHAIN_TARGET="arm-linux-androideabi"
+    CMAKE_SYSTEM_PROCESSOR="armv5te"
+    OPENSSL_MACHINE="armv4"
+elif [ "$ABI" = "x86" ]; then
+    NDK_ARCH="x86"
+    NDK_TOOLCHAIN_TARGET="i686-linux-android"
+    CMAKE_SYSTEM_PROCESSOR="i686"
+    OPENSSL_MACHINE="i686"
+elif [ "$ABI" = "x86_64" ]; then
+    NDK_ARCH="x86_64"
+    NDK_TOOLCHAIN_TARGET="x86_64-linux-android"
+    CMAKE_SYSTEM_PROCESSOR="x86_64"
+    OPENSSL_MACHINE="x86_64"
+else
+    # This may help:
+    # https://github.com/opencv/opencv/blob/5b868ccd829975da5372bf330994553e176aee09/platforms/android/android.toolchain.cmake#L658
+    >&2 echo "TODO: Need a mapping from \"$ABI\" to NDK architecture, NDK toolchain target triplet and CMAKE_SYSTEM_PROCESSOR"
+    exit 1
+fi
+
 NDK=android-ndk-r16b
 NDK_DIR=$DIR/$NDK
 NDK_ZIP=${NDK}-linux-x86_64.zip
@@ -16,8 +54,6 @@ NDK_ZIP=${NDK}-linux-x86_64.zip
 # `posix_fadvise`, required by Boost.Beast is was only added in LOLLIPOP
 # https://developer.android.com/guide/topics/manifest/uses-sdk-element.html#ApiLevels
 NDK_PLATFORM=21
-NDK_ARCH=${NDK_ARCH:-arm}  # TODO: Derive from $ABI below.
-#NDK_ARCH=x86_64
 NDK_STL='libc++'
 NDK_TOOLCHAIN_DIR=${DIR}/${NDK}-toolchain-android$NDK_PLATFORM-$NDK_ARCH-$NDK_STL
 
@@ -28,10 +64,6 @@ EMULATOR_AVD=${EMULATOR_AVD:-ouinet-test}
 
 # The following options may be limited by availability of SDK packages,
 # to get list of all packages, use `sdkmanager --list`.
-# https://developer.android.com/ndk/guides/abis.html
-ABI=${ABI:-armeabi-v7a}
-#ABI=arm64-v8a
-#ABI=x86_64
 
 # Android API level
 PLATFORM=${PLATFORM:-android-25}
@@ -161,24 +193,6 @@ fi
 }
 
 ######################################################################
-if [ "$ABI" = "armeabi-v7a" ]; then
-    CMAKE_SYSTEM_PROCESSOR="armv7-a"
-elif [ "$ABI" = "arm64-v8a" ]; then
-    CMAKE_SYSTEM_PROCESSOR="aarch64"
-elif [ "$ABI" = "armeabi" ]; then
-    CMAKE_SYSTEM_PROCESSOR="armv5te"
-elif [ "$ABI" = "x86" ]; then
-    CMAKE_SYSTEM_PROCESSOR="i686"
-elif [ "$ABI" = "x86_64" ]; then
-    CMAKE_SYSTEM_PROCESSOR="x86_64"
-else
-    # This may help:
-    # https://github.com/opencv/opencv/blob/5b868ccd829975da5372bf330994553e176aee09/platforms/android/android.toolchain.cmake#L658
-    >&2 echo "TODO: Need a mapping from \"$ABI\" to CMAKE_SYSTEM_PROCESSOR"
-    exit 1
-fi
-
-######################################################################
 function maybe_install_ndk {
 if [ ! -d "./$NDK" ]; then
     cd /tmp
@@ -203,7 +217,7 @@ fi
 
 export ANDROID_NDK_HOME=$DIR/android-ndk-r16b
 
-add_library $NDK_TOOLCHAIN_DIR/$NDK_ARCH-linux-android*/lib*/libc++_shared.so
+add_library $NDK_TOOLCHAIN_DIR/$NDK_TOOLCHAIN_TARGET/lib*/libc++_shared.so
 }
 
 ######################################################################
@@ -241,8 +255,8 @@ function build_openssl {
     export SYSTEM=android
     export CROSS_SYSROOT="$NDK_TOOLCHAIN_DIR/sysroot"
     export ANDROID_DEV="$SYSROOT/usr"
-    export MACHINE=armv7
-    export CROSS_COMPILE=arm-linux-androideabi-
+    export MACHINE="$OPENSSL_MACHINE"
+    export CROSS_COMPILE="$NDK_TOOLCHAIN_TARGET-"
     export TOOLCHAIN="$NDK_TOOLCHAIN_DIR"
     export PATH="$NDK_TOOLCHAIN_DIR/bin:$PATH"
     ./config -v no-shared -no-ssl2 -no-ssl3 -no-comp -no-hw -no-engine
