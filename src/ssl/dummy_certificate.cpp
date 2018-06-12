95e680f01bdf946d219b005ec5992b57b3132f05
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2018-06-12 12:44:32 -0400

    Fix validity range of dummy certificates

--- src/ssl/dummy_certificate.cpp
+++ src/ssl/dummy_certificate.cpp
@@ -13,8 +13,12 @@ DummyCertificate::DummyCertificate( CACertificate& ca_cert
     X509_set_version(_x, 2);
     ASN1_INTEGER_set(X509_get_serialNumber(_x), ca_cert.next_serial_number());
 
-    X509_gmtime_adj(X509_get_notBefore(_x), -ssl::util::ONE_HOUR);
-    X509_gmtime_adj(X509_get_notAfter(_x), 15 * ssl::util::ONE_YEAR);
+    // Avoid signature issues because of time zone differences.
+    // See [Mitmproxy can't record traffic when time set with 1 hour ago.](https://github.com/mitmproxy/mitmproxy/issues/200).
+    X509_gmtime_adj(X509_get_notBefore(_x), -48 * ssl::util::ONE_HOUR);
+    // A value close to maximum CA-emitted certificate validity (39 months), see
+    // [Validity Period, 9.4.1](https://cabforum.org/wp-content/uploads/BRv1.2.3.pdf).
+    X509_gmtime_adj(X509_get_notAfter(_x), 3 * ssl::util::ONE_YEAR);
 
     X509_set_pubkey(_x, ca_cert.get_private_key());
     
