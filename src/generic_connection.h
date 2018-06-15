198a430a86f465757d140940b2d6198f7f0754b9
Peter Jankuliak <p.jankuliak@gmail.com>
2018-06-15 10:27:02 +0200

    Allow moving unique_ptr<SomeStream> into GenericConnection
    This helps in cases where SomeStream isn't movable, such
    as the asio::ssl::stream at the time of writing (Boost 1.67.0).

--- src/generic_connection.h
+++ src/generic_connection.h
@@ -13,6 +13,34 @@
 
 namespace ouinet {
 
+namespace generic_connection_detail {
+    // Some stream implementations (such as the asio::ssl::stream in Boost
+    // <=1.67.0) are not movable. This template specialization shall allow us
+    // to move std::unique_ptr<NonMovableStream> into GenericConnection instead
+    // of NonMovableStream&& directly.
+    template<class T> struct Deref {
+        using type = T;
+
+              T& operator*()        { return value; }
+        const T& operator*()  const { return value; }
+              T* operator->()       { return &value; }
+        const T* operator->() const { return &value; }
+
+        T value;
+    };
+
+    template<class T> struct Deref<std::unique_ptr<T>> {
+        using type = T;
+
+              T& operator*()        { return *value; }
+        const T& operator*()  const { return *value; }
+              T* operator->()       { return value.get(); }
+        const T* operator->() const { return value.get(); }
+
+        std::unique_ptr<T> value;
+    };
+} // namespace
+
 class GenericConnection {
 private:
     template<class Token, class Ret>
@@ -45,40 +73,41 @@ private:
 
     template<class Impl>
     struct Wrapper : public Base {
-        using Shutter = std::function<void(Impl&)>;
+        using Shutter = std::function<
+            void(typename generic_connection_detail::Deref<Impl>::type&)>;
 
         Wrapper(Impl&& impl)
-            : _impl(std::move(impl))
+            : _impl{std::move(impl)}
             , _shutter([](Impl& impl) { impl.close(); })
         {}
 
         Wrapper(Impl&& impl, Shutter shutter)
-            : _impl(std::move(impl))
+            : _impl{std::move(impl)}
             , _shutter(std::move(shutter))
         {}
 
         virtual asio::io_service& get_io_service() override
         {
-            return _impl.get_io_service();
+            return _impl->get_io_service();
         }
 
         void read_impl(OnRead&& on_read) override
         {
-            _impl.async_read_some(read_buffers, std::move(on_read));
+            _impl->async_read_some(read_buffers, std::move(on_read));
         }
 
         void write_impl(OnWrite&& on_write) override
         {
-            _impl.async_write_some(write_buffers, std::move(on_write));
+            _impl->async_write_some(write_buffers, std::move(on_write));
         }
 
         void close() override
         {
-            _shutter(_impl);
+            _shutter(*_impl);
         }
 
     private:
-        Impl _impl;
+        generic_connection_detail::Deref<Impl> _impl;
         Shutter _shutter;
     };
 
