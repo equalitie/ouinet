a15a16c450a402a36e2b9500e9457cda8973bf1b
Peter Jankuliak <p.jankuliak@gmail.com>
2018-06-15 13:08:32 +0200

    Move {IPNS, injector ep, credentials} management to the browser

--- android/browser/src/main/java/ie/equalit/ouinet/browser/Util.java
+++ android/browser/src/main/java/ie/equalit/ouinet/browser/Util.java
@@ -14,7 +14,6 @@ class Util {
             outputStream.write(value.getBytes());
             outputStream.close();
         } catch (Exception e) {
-            log("3");
             e.printStackTrace();
         }
     }
