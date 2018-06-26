42ce6e78f0682190aed1e95d39c3f6505f5c229d
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2018-06-26 16:43:45 -0400

    Extract HTTP/HTTPS proxy request fetching code from injector
    So that it can be used by the client too.

--- src/injector.cpp
+++ src/injector.cpp
@@ -32,7 +32,6 @@
 #include "ouiservice/tcp.h"
 
 #include "util/signal.h"
-#include "ssl/util.h"
 
 using namespace std;
 using namespace ouinet;
@@ -126,50 +125,9 @@ public:
         : ios(ios)
         , injector(injector)
     {
-        cc.fetch_fresh = [this, &ios, &abort_signal]
+        cc.fetch_fresh = [&ios, &abort_signal]
                          (const Request& rq, asio::yield_context yield) {
-            auto target = rq.target().to_string();
-            sys::error_code ec;
-
-            // Parse the URL to tell HTTP/HTTPS, host, port.
-            util::url_match url;
-            if (!util::match_http_url(target, url)) {
-                ec = asio::error::operation_not_supported;  // unsupported URL
-                return or_throw<Response>(yield, ec);
-            }
-            string url_port;
-            bool ssl(false);
-            if (url.port.length() > 0)
-                url_port = url.port;
-            else if (url.scheme == "https") {
-                url_port = "443";
-                ssl = true;
-            } else  // url.scheme == "http"
-                url_port = "80";
-
-            auto con = connect_to_host(ios, url.host, url_port, abort_signal, yield[ec]);
-            if (ec) return or_throw<Response>(yield, ec);
-
-            if (ssl) {
-                // Subsequent access to the connection will use the encrypted channel.
-                con = ssl::util::client_handshake(move(con), url.host, yield[ec]);
-                if (ec) {
-                    cerr << "SSL client handshake error: "
-                         << url.host << ": " << ec.message() << endl;
-                    return or_throw<Response>(yield, ec);
-                }
-            }
-
-            // Now that we have a connection to the origin
-            // we can send a non-proxy request to it
-            // (i.e. with target "/foo..." and not "http://example.com/foo...").
-            // Actually some web servers do not like the full form.
-            Request origin_rq(rq);
-            origin_rq.target(target.substr(target.find( url.path
-                                                      // Length of "http://" or "https://",
-                                                      // do not fail on "http(s)://FOO/FOO".
-                                                      , url.scheme.length() + 3)));
-            return fetch_http_page(ios, con, origin_rq, abort_signal, yield);
+            return fetch_http_page(ios, rq, abort_signal, yield);
         };
 
         cc.fetch_stored = [this](const Request& rq, asio::yield_context yield) {
