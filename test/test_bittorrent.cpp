4d469ba84e6d5cd43e47478ef89f3624d05aa4f4
Peter Jankuliak <p.jankuliak@gmail.com>
2018-07-18 18:21:57 +0200

    Add test/test_bittorrent.cpp/test_bep_5 test

--- test/test_bittorrent.cpp
+++ test/test_bittorrent.cpp
@@ -8,6 +8,9 @@
 
 #define private public
 #include <bittorrent/node_id.h>
+#include <bittorrent/dht.h>
+#include <bittorrent/code.h>
+#include <bittorrent/byte_printer.h>
 
 BOOST_AUTO_TEST_SUITE(bittorrent)
 
@@ -30,6 +33,49 @@ BOOST_AUTO_TEST_CASE(test_generate_node_id)
     auto id = NodeID::generate(ip, 1).to_hex();
     BOOST_REQUIRE_EQUAL(id.substr(0, 6), "5fbfbf");
     BOOST_REQUIRE_EQUAL(id.substr(38), "01");
+}
+
+static udp::endpoint as_udp_endpoint(tcp::endpoint ep) {
+    return { ep.address(), ep.port() };
+}
+
+BOOST_AUTO_TEST_CASE(test_bep_5)
+{
+    using namespace ouinet::bittorrent::dht;
+
+    asio::io_service ios;
+
+    DhtNode dht(ios, asio::ip::make_address("0.0.0.0")); // TODO: IPv6
+
+    asio::spawn(ios, [&] (auto yield) {
+        sys::error_code ec;
+
+        stringstream ss;
+        NodeID infohash = util::sha1(ss.str());
+
+        dht.start(yield[ec]);
+        BOOST_REQUIRE(!ec);
+
+        dht.tracker_announce(infohash, dht.wan_endpoint().port(), yield[ec]);
+        BOOST_REQUIRE(!ec);
+
+        auto peers = dht.tracker_get_peers(infohash , yield);
+
+        bool found = false;
+
+        for (auto p : peers) {
+            if (as_udp_endpoint(p) == dht.wan_endpoint()) {
+                found = true;
+                break;
+            }
+        }
+
+        BOOST_REQUIRE(found);
+
+        dht.stop();
+    });
+
+    ios.run();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
