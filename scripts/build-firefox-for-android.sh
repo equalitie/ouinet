fa21cdb1234a9130125d0281c99b5179b167f465
Peter Jankuliak <p.jankuliak@gmail.com>
2018-08-09 14:51:13 +0200

    Add script to build Firefox For Android
    Tested on Vagrant, but RAM needs to be increased (clang crashed
    on me with the current 4MB and worked with 8MB).

---
+++ scripts/build-firefox-for-android.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+set -e
+
+DIR=`pwd`
+
+SDK_DIR="$DIR/sdk"
+
+NDK=android-ndk-r15c
+NDK_DIR=$DIR/$NDK
+NDK_ZIP=${NDK}-linux-x86_64.zip
+
+function clean {
+    rm -rf $HOME/.android
+    rm -rf $HOME/.cargo
+    rm -rf $HOME/.mozbuild
+    rm -rf $HOME/.multirust
+    rm -rf $HOME/.rustup
+    rm -rf $HOME/.cache
+    rm -rf $DIR/mozilla-central
+}
+
+function install_dependencies {
+    sudo apt-get update
+    sudo apt-get -y install curl mercurial libpulse-dev libpango1.0-dev \
+        libgtk-3-dev libgtk2.0-dev libgconf2-dev libdbus-glib-1-dev \
+        yasm libnotify-dev clang-4.0
+}
+
+function maybe_download_moz_sources {
+    # https://developer.mozilla.org/en-US/docs/Mozilla/Developer_guide/Build_Instructions/Simple_Firefox_for_Android_build
+    if [ ! -d mozilla-central ]; then
+        if [ -d mozilla-central-orig ]; then
+            cp -r mozilla-central-orig mozilla-central
+        else
+            hg clone https://hg.mozilla.org/mozilla-central
+            cp -r mozilla-central mozilla-central-orig
+        fi
+    fi
+}
+
+function maybe_install_sdk {
+    ######################################################################
+    # Install SDK dependencies.
+    local toolsfile=sdk-tools-linux-3859397.zip
+    local sdkmanager="$SDK_DIR/tools/bin/sdkmanager"
+    
+    if [ ! -f "$sdkmanager" ]; then
+        [ -d "$SDK_DIR/tools" ] || rm -rf "$SDK_DIR/tools"
+        if [ ! -f "$toolsfile" ]; then
+            # https://developer.android.com/studio/index.html#command-tools
+            wget "https://dl.google.com/android/repository/$toolsfile"
+        fi
+        unzip "$toolsfile" -d "$SDK_DIR"
+    fi
+}
+
+function maybe_install_ndk {
+    if [ ! -d $NDK_DIR ]; then
+        if [ ! -f $NDK_ZIP ]; then
+            wget https://dl.google.com/android/repository/$NDK_ZIP
+        fi
+        unzip $NDK_ZIP
+    fi
+}
+
+function maybe_install_rust {
+    export PATH="$HOME/.cargo/bin:$PATH"
+    if ! which rustc; then
+        # Install rust https://www.rust-lang.org/en-US/install.html
+        curl https://sh.rustup.rs -sSf | sh
+        rustup update
+        # https://bugzilla.mozilla.org/show_bug.cgi?id=1384231
+        rustup target add armv7-linux-androideabi
+    fi
+}
+
+################################################################################
+#clean
+install_dependencies
+(cd $DIR; maybe_install_rust)
+#(cd $DIR; maybe_install_sdk)
+#(cd $DIR; maybe_install_ndk)
+(cd $DIR; maybe_download_moz_sources)
+################################################################################
+
+cd mozilla-central
+
+# https://developer.mozilla.org/en-US/docs/Mozilla/Developer_guide/Build_Instructions/Simple_Firefox_for_Android_build#I_want_to_work_on_the_back-end
+cat > mozconfig <<EOL
+# Build Firefox for Android:
+ac_add_options --enable-application=mobile/android
+ac_add_options --target=arm-linux-androideabi
+
+# With the following Android SDK and NDK:
+ac_add_options --with-android-sdk="/home/vagrant/.mozbuild/android-sdk-linux"
+ac_add_options --with-android-ndk="/home/vagrant/.mozbuild/android-ndk-r15c"
+EOL
+
+# Install some dependencies and configure Firefox build
+./mach bootstrap --application-choice=mobile_android --no-interactive
+
+# Note: If during building clang crashes, try increasing vagrant's RAM
+./mach build
+./mach package
+
