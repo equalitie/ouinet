fa48ec1a106b86c5e10c7b28ae7c079f7971d3b0
Peter Jankuliak <p.jankuliak@gmail.com>
2018-08-30 15:28:54 +0200

    Increment default tcp connect timeout from 30secs to 2mins

--- src/default_timeout.h
+++ src/default_timeout.h
@@ -2,7 +2,7 @@
 
 namespace ouinet { namespace default_timeout {
 
-static inline auto tcp_connect() { return std::chrono::seconds(30); }
+static inline auto tcp_connect() { return std::chrono::minutes(2); }
 static inline auto fetch_http() { return std::chrono::minutes(3); }
 
 }} // namespaces
