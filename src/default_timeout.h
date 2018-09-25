134df58a480d1f08e35c54f1bccb0a066a5c536f
Peter Jankuliak <p.jankuliak@gmail.com>
2018-09-25 13:17:30 +0200

    Increase default http fetch timeouts

--- src/default_timeout.h
+++ src/default_timeout.h
@@ -2,7 +2,7 @@
 
 namespace ouinet { namespace default_timeout {
 
-static inline auto tcp_connect() { return std::chrono::minutes(2); }
-static inline auto fetch_http() { return std::chrono::minutes(3); }
+static inline auto tcp_connect() { return std::chrono::minutes(4); }
+static inline auto fetch_http() { return std::chrono::minutes(8); }
 
 }} // namespaces
