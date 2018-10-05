d864b7b7dba13e0ef6f44ee232716c827885879f
Peter Jankuliak <p.jankuliak@gmail.com>
2018-10-05 14:26:26 +0200

    Add variadic template sha1

--- src/util/crypto.cpp
+++ src/util/crypto.cpp
@@ -28,36 +28,6 @@ std::string random(unsigned int size)
     return std::string(buffer.data(), buffer.size());
 }
 
-static std::array<uint8_t, 20> sha1(const void* buffer, size_t size)
-{
-    ::gcry_md_hd_t digest;
-
-    if (::gcry_md_open(&digest, ::gcry_md_algos::GCRY_MD_SHA1, 0)) {
-        throw std::exception();
-    }
-
-    ::gcry_md_write(digest, buffer, size);
-    unsigned char *digest_buffer = ::gcry_md_read(digest, ::gcry_md_algos::GCRY_MD_SHA1);
-
-    std::array<uint8_t, 20> result;
-    memcpy(result.data(), digest_buffer, result.size());
-
-    ::gcry_md_close(digest);
-    return result;
-}
-
-std::array<uint8_t, 20> sha1(const std::string& data)
-{
-    return sha1(data.data(), data.size());
-}
-
-std::array<uint8_t, 20> sha1(const std::vector<unsigned char>& data)
-{
-    return sha1(data.data(), data.size());
-}
-
-
-
 Ed25519PublicKey::Ed25519PublicKey(std::array<uint8_t, 32> key):
     _public_key(nullptr)
 {
---
+++ src/util/sha1.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <array>
+#include <vector>
+#include <string>
+#include <boost/utility/string_view.hpp>
+
+namespace ouinet { namespace util {
+
+namespace sha1_detail {
+    struct Sha1;
+
+    size_t size_of_Sha1();
+    Sha1* init(void*);
+    void update(Sha1*, const void*, size_t);
+    std::array<uint8_t, 20> close(Sha1*);
+
+    inline void update(Sha1* digest, boost::string_view sv)
+    {
+        update(digest, sv.data(), sv.size());
+    }
+
+    inline void update(Sha1* digest, const std::string& data)
+    {
+        update(digest, data.data(), data.size());
+    }
+
+    inline void update(Sha1* digest, const std::vector<unsigned char>& data)
+    {
+        update(digest, data.data(), data.size());
+    }
+
+    template<size_t N>
+    inline void update(Sha1* digest, const std::array<uint8_t, N>& data)
+    {
+        update(digest, data.data(), N);
+    }
+
+    inline
+    std::array<uint8_t, 20> sha1(Sha1* digest)
+    {
+        return close(digest);
+    }
+
+    template<class Arg, class... Rest>
+    inline
+    std::array<uint8_t, 20> sha1(Sha1* digest, const Arg& arg, const Rest&... rest)
+    {
+        update(digest, arg);
+        return sha1(digest, rest...);
+    }
+
+} // detail namespace
+
+/*
+ * Usage:
+ *
+ * array<uint8_t, 20> hash = sha1(string("hello world"));
+ *
+ * Or:
+ *
+ * array<uint8_t, 20> hash = sha1(boost::string_view("hello world"));
+ *
+ * Or:
+ *
+ * string s = "hello ";
+ * string_view sv = "world";
+ * array<uint8_t, 20> hash = sha1(s + sv.to_string());
+ *
+ * Or:
+ *
+ * string s = "hello ";
+ * string_view sv = "world";
+ * array<uint8_t, 20> hash = sha1(hello, world);
+ *
+ * Note that the last one is more efficient than the one before because it
+ * does not allocate a new string as a result of applying the operator+
+ */
+
+template<class Arg, class... Rest>
+inline
+std::array<uint8_t, 20> sha1(const Arg& arg, const Rest&... rest)
+{
+    using namespace sha1_detail;
+    uint8_t mem[size_of_Sha1()];
+    Sha1* digest = init(mem);
+    return sha1(digest, arg, rest...);
+}
+
+}} // namespaces
