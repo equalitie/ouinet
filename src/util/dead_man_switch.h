a40f251d17906dcd946203252e557be159130914
Peter Jankuliak <p.jankuliak@gmail.com>
2018-10-26 16:03:22 +0200

    Add func to increase deadline in DeadManSwitch

--- src/util/dead_man_switch.h
+++ src/util/dead_man_switch.h
@@ -3,6 +3,9 @@
 namespace ouinet {
 
 class DeadManSwitch {
+private:
+    using Clock = std::chrono::steady_clock;
+
 public:
     template<class Duration, class OnTimeout>
     DeadManSwitch(asio::io_service& ios, Duration d, OnTimeout on_timeout)
@@ -10,25 +13,47 @@ public:
     {
         asio::spawn(ios, [&, d, on_timeout = std::move(on_timeout)]
                          (asio::yield_context yield) {
-            bool was_destroyed = false;
+            Clock::time_point deadline = Clock::now() + d;
+            bool was_destroyed         = false;
+
             asio::steady_timer timer(ios);
 
             _was_destroyed = &was_destroyed;
-            _timer = &timer;
+            _timer         = &timer;
+            _deadline      = &deadline;
 
-            auto on_exit = defer([&] {
-                    _was_destroyed = nullptr;
-                    _timer = nullptr;
-                });
+            {
+                auto on_exit = defer([&] {
+                        _was_destroyed = nullptr;
+                        _timer = nullptr;
+                        _deadline = nullptr;
+                    });
 
-            timer.expires_from_now(d);
-            sys::error_code ec;
-            timer.async_wait(yield[ec]);
-
-            if (was_destroyed) return;
+                auto now = Clock::now();
+                while (now < deadline) {
+                    timer.expires_after(deadline - now);
+                    sys::error_code ec;
+                    timer.async_wait(yield[ec]);
+                    if (was_destroyed) return;
+                    now = Clock::now();
+                }
+            }
 
             on_timeout();
         });
+    }
+
+    template<class Duration>
+    void expires_after(Duration d)
+    {
+        if (!_deadline) return;
+        auto old_deadline = *_deadline;
+        *_deadline = Clock::now() + d;
+
+        if (*_deadline < old_deadline) {
+            assert(_timer);
+            _timer->cancel();
+        }
     }
 
     ~DeadManSwitch() {
@@ -38,6 +63,7 @@ public:
 
 private:
     asio::io_service& _ios;
+    Clock::time_point* _deadline;
     bool* _was_destroyed = nullptr;
     asio::steady_timer* _timer;
 };
