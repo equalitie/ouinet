4b84f78028ac0d9545289357b023063e6513af3c
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2018-11-15 14:09:12 -0500

    Fix db inlining capability detection
    It was broken since `put_into_db` received an `InjectorDb&` so the overload
    `db_can_inline(Bep44InjectorDb&)` was never called and `can_inline` was always
    false.
    
    Now, instead of trying to detect inlining capabilities, inline insertion is
    attempted first for all data base types and then linked insertion is attempted
    if the first one failed with `boost::asio::error::message_size`.

--- src/cache/descdb.h
+++ src/cache/descdb.h
@@ -9,7 +9,6 @@
 #include "../or_throw.h"
 #include "../util.h"
 #include "db.h"
-#include "bep44_db.h"
 
 
 namespace ouinet {
@@ -18,23 +17,6 @@ namespace descriptor {
 
 static const std::string ipfs_prefix = "/ipfs/";
 static const std::string zlib_prefix = "/zlib/";
-
-// This is a decision we take here and not at the db level,
-// since a db just stores a string
-// and it does not differentiate between an inlined descriptor and a link to it.
-// An alternative would be to always attempt to store the descriptor inlined
-// and attempt again with a link in case of getting `asio::error::message_size`.
-// However at the moment we do not want to even attempt inlining
-// with the IPFS-based B-tree cache index.
-inline
-bool db_can_inline(InjectorDb& db) {
-    return false;
-}
-
-inline
-bool db_can_inline(Bep44InjectorDb& db) {
-    return true;  // only attempt inlining with BEP44
-}
 
 // Get the serialized descriptor pointed to by an entry
 // in the given `db` under the given `key`.
@@ -87,26 +69,26 @@ put_into_db( const std::string& key, const std::string& desc_data
            , StoreFunc ipfs_store
            , asio::yield_context yield)
 {
-    using namespace std;
+    using dcid_insd = std::pair<std::string, std::string>;
     sys::error_code ec;
 
     // Always store the descriptor itself in IPFS.
-    string desc_ipfs = ipfs_store(desc_data, yield[ec]);
+    std::string desc_ipfs = ipfs_store(desc_data, yield[ec]);
+    if (ec)
+        return or_throw<dcid_insd>(yield, ec);
 
-    string ins_data;
+    std::string ins_data;
     // Insert descriptor inline (if possible).
-    bool can_inline = db_can_inline(db);
-    if (!ec && can_inline) {
-        auto compressed_desc = util::zlib_compress(desc_data);
-        ins_data = db.insert(key, zlib_prefix + compressed_desc, yield[ec]);
-    }
-    // Insert IPFS link to descriptor.
-    if (!can_inline || ec == asio::error::message_size) {
-        ins_data = db.insert(key, ipfs_prefix + desc_ipfs, yield[ec]);
-    }
+    ins_data = db.insert( key, zlib_prefix + util::zlib_compress(desc_data)
+                        , yield[ec]);
+    if (ec && ec != asio::error::message_size)
+        return or_throw<dcid_insd>(yield, ec);
 
-    pair<string, string> ret(move(desc_ipfs), move(ins_data));
-    return or_throw(yield, ec, move(ret));
+    // Insert IPFS link to descriptor.
+    if (ec == asio::error::message_size)
+        ins_data = db.insert(key, ipfs_prefix + desc_ipfs, yield[ec]);
+
+    return or_throw(yield, ec, dcid_insd(move(desc_ipfs), move(ins_data)));
 }
 
 } // namespace descriptor
