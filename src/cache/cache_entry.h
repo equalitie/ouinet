eb5cbab82692774b5329e09e5dcb2f932911549a
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2018-12-10 14:10:29 -0500

    Write trivial URL canonicalization function
    It just returns the URL as is.

--- src/cache/cache_entry.h
+++ src/cache/cache_entry.h
@@ -4,13 +4,20 @@
 #include <boost/asio/error.hpp>
 #include <boost/beast.hpp>
 #include "../namespaces.h"
+#include "../util.h"
 
 namespace ouinet {
 
 template <class Request>
 inline
 std::string key_from_http_req(const Request& req) {
-    return req.target().to_string();  // TODO: canonical
+    // The key is currently the canonical URL itself.
+    return util::canonical_url(req.target());
+}
+
+inline
+std::string key_from_http_url(const std::string& url) {
+    return util::canonical_url(url);
 }
 
 struct CacheEntry {
