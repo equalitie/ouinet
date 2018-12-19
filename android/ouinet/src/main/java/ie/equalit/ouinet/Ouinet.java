deb2d530c228bc661ca767c4776e616aa38a8fc4
Peter Jankuliak <p.jankuliak@gmail.com>
2018-12-19 11:38:33 +0100

    Allow adding external tls ca certificates on Android

--- android/ouinet/src/main/java/ie/equalit/ouinet/Ouinet.java
+++ android/ouinet/src/main/java/ie/equalit/ouinet/Ouinet.java
@@ -32,6 +32,9 @@ public class Ouinet {
         public String injector_endpoint;
         public String injector_credentials;
         public String injector_tls_cert;
+        // Path to a .pem file with CA certificates.
+        // One can get it from e.g.: https://curl.haxx.se/docs/caextract.html
+        public String tls_ca_cert_store_path;
     }
 
     public Ouinet(Context ctx, Config conf) {
@@ -62,10 +65,31 @@ public class Ouinet {
         maybeAdd(args, "--injector-ipns",        conf.injector_ipfs_id);
         maybeAdd(args, "--injector-credentials", conf.injector_credentials);
 
+        if (conf.tls_ca_cert_store_path != null) {
+            String ca_cert_path;
+            String assetPrefix = "file:///android_asset/";
+
+            if (conf.tls_ca_cert_store_path.startsWith(assetPrefix)) {
+                // TODO: Ouinet's C++ code doesn't yet have a way to read asset
+                // files from the apk. As a temporary workaround we copy the
+                // asset file to a regular file and the pass path to that to
+                // the C++ code.
+                String asset = conf.tls_ca_cert_store_path.substring(assetPrefix.length());
+                ca_cert_path = dir() + "/assets/" + asset;
+
+                if (copyAssetToFile(ctx, asset, ca_cert_path)) {
+                    maybeAdd(args, "--tls-ca-cert-store-path", ca_cert_path);
+                }
+            }
+            else {
+                maybeAdd(args, "--tls-ca-cert-store-path", conf.tls_ca_cert_store_path);
+            }
+        }
+
         try {
             if (conf.injector_tls_cert != null) {
                 String cert_path = dir() + "/injector-tls-cert.pem";
-                writeToFile(cert_path, conf.injector_tls_cert);
+                writeToFile(cert_path, conf.injector_tls_cert.getBytes());
                 args.addElement("--injector-tls-cert-file=" + cert_path);
             }
         } catch (IOException e) {
@@ -80,6 +104,24 @@ public class Ouinet {
     public String pathToCARootCert()
     {
         return nPathToCARootCert();
+    }
+
+    public boolean copyAssetToFile(Context ctx, String asset, String path)
+    {
+        try {
+            java.io.InputStream stream = ctx.getAssets().open(asset);
+            int size = stream.available();
+            byte[] buffer = new byte[size];
+            stream.read(buffer);
+            stream.close();
+            writeToFile(path, buffer);
+        } catch (IOException e) {
+            Log.d("Ouinet", "Failed to write asset \"" + asset + "\" to file \"" + path + "\"");
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
     }
 
     // If this succeeds, we should be able to do UDP multicasts
@@ -141,15 +183,17 @@ public class Ouinet {
         args.addElement(key + "=" + value);
     }
 
-    private void writeToFile(String path, String content) throws IOException {
+    private void writeToFile(String path, byte[] bytes) throws IOException {
         File file = new File(path);
 
         if (!file.exists()) {
+            File dir = file.getParentFile();
+            if (!dir.exists()) { dir.mkdirs(); }
             file.createNewFile();
         }
 
         FileOutputStream stream = new FileOutputStream(file);
-        stream.write(content.getBytes());
+        stream.write(bytes);
         stream.close();
     }
 
