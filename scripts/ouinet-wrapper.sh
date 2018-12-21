6662752b30ddd1e37357df9f8af844b41cec03bf
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2018-12-21 15:51:24 -0500

    Enable loopback TCP port and create an I2P tunnel to it
    Configuration inspired on a conversation with @orignal.

--- scripts/ouinet-wrapper.sh
+++ scripts/ouinet-wrapper.sh
@@ -53,6 +53,7 @@ repo_arg=$(get_repo_from_args "$@")
 REPO="${repo_arg:-$REPO}"
 
 CLIENT_PROXY_PORT=8077
+INJECTOR_TCP_PORT=7070
 INJECTOR_TLS_PORT=7077
 
 if [ ! -d "$REPO" ] && ! has_help_arg "$@"; then
@@ -61,6 +62,11 @@ if [ ! -d "$REPO" ] && ! has_help_arg "$@"; then
     # Set a well-known client HTTP proxy port.
     if [ "$PROG" = client ]; then
         sed -i -E "s/^(listen-on-tcp\s*=\s*.*:)[0-9]+(.*)/\1${CLIENT_PROXY_PORT}\2/" "$CONF"
+    fi
+
+    # Set a well-known injector loopback TCP port (and enable it).
+    if [ "$PROG" = injector ]; then
+        sed -i -E "s/^#?(listen-on-tcp\s*=\s*127.0.0.1:)[0-9]+(.*)/\1${INJECTOR_TCP_PORT}\2/" "$CONF"
     fi
 
     # Set a well-known injector TLS port (and enable it).
@@ -78,6 +84,20 @@ if [ ! -d "$REPO" ] && ! has_help_arg "$@"; then
     if [ "$PROG" = injector ]; then
         sed -i -E 's/^#*\s*(ipv6\s*=\s*)false(\b.*)/\1true\2/' /etc/i2pd/i2pd.conf  # enable IPv6
         sed -i -E 's/^([^#].*)/#\1/' /etc/i2pd/tunnels.conf  # disable default tunnels
+        cat > /etc/i2pd/tunnels.conf.d/tunnels.d/ouinet-injector.conf <<- EOF
+		[ouinet-injector]
+		type=server
+		host=127.0.0.1
+		port=$INJECTOR_TCP_PORT
+		inport=$INJECTOR_TCP_PORT
+		keys=ouinet-injector-keys.dat
+		signaturetype=7
+		inbound.quantity=3
+		outbound.quantity=3
+		inbound.length=1
+		outbound.length=1
+		i2p.streaming.initialAckDelay=20
+		EOF
     fi
 
     # Enable and start the I2P daemon.
