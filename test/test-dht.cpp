0d52d9d723dd87a793e4ea7dcd779e3f5e79ed96
Peter Jankuliak <p.jankuliak@gmail.com>
2019-02-04 13:12:22 +0100

    Modify test-dht.cpp to do DHT get/put ops

--- test/test-dht.cpp
+++ test/test-dht.cpp
@@ -6,13 +6,17 @@
 #include <ifaddrs.h>
 #include <arpa/inet.h>
 #include <boost/asio/ip/address.hpp>
+#include <boost/optional.hpp>
+#include <boost/optional/optional_io.hpp>
+#include "../src/util/crypto.h"
 
-namespace asio = boost::asio;
-using udp = asio::ip::udp;
 
-using std::cerr;
-using std::endl;
+using namespace ouinet;
+using namespace std;
 using namespace ouinet::bittorrent;
+using boost::string_view;
+using udp = asio::ip::udp;
+using boost::optional;
 
 std::vector<asio::ip::address> linux_get_addresses()
 {
@@ -65,13 +69,94 @@ filter( bool loopback
     return ret;
 }
 
-void usage(std::ostream& os, const char* app_name) {
+void usage(std::ostream& os, const string& app_name, const char* what = nullptr) {
+    if (what) {
+        os << what << "\n" << endl;
+    }
+
     os << "Usage:" << endl
        << "  " << app_name << " [interface-address]" << endl
        << "E.g.:" << endl
-       << "  " << app_name << "              # All non loopback interfaces" << endl
-       << "  " << app_name << " 0.0.0.0      # Any ipv4 interface" << endl
-       << "  " << app_name << " 192.168.0.1  # Concrete interface" << endl;
+       << "  " << app_name << " all          [<get>|<put>] # All non loopback interfaces" << endl
+       << "  " << app_name << " 0.0.0.0      [<get>|<put>] # Any ipv4 interface" << endl
+       << "  " << app_name << " 192.168.0.1  [<get>|<put>] # Concrete interface" << endl
+       << "Where:" << endl
+       << "  <get>: get <public-key> <dht-key>" << endl
+       << "  <put>: put <private-key> <dht-key> <dht-value>" << endl;
+
+}
+
+template<size_t N>
+static
+boost::string_view as_string_view(const std::array<uint8_t, N>& a)
+{
+    return boost::string_view((char*) a.data(), a.size());
+}
+
+struct GetCmd {
+    util::Ed25519PublicKey public_key;
+    string dht_key;
+};
+
+struct PutCmd {
+    util::Ed25519PrivateKey private_key;
+    string dht_key;
+    string dht_value;
+};
+
+void parse_args( const vector<string>& args
+               , vector<asio::ip::address>* ifaddrs
+               , optional<GetCmd>* get_cmd
+               , optional<PutCmd>* put_cmd)
+{
+    if (args.size() == 2 && args[1] == "-h") {
+        usage(std::cout, args[0]);
+        exit(0);
+    }
+
+    if (args.size() < 3) {
+        usage(std::cerr, args[0], "Too few arguments");
+        exit(1);
+    }
+
+    if (args[1] == "all") {
+        *ifaddrs = filter( false
+                         , true
+                         , true
+                         , linux_get_addresses());
+    } else {
+        boost::system::error_code ec;
+        ifaddrs->push_back(asio::ip::make_address(args[1], ec));
+
+        if (ec) {
+            std::cerr << "Failed parsing \"" << args[1] << "\" as an IP "
+                      << "address: " << ec.message() << std::endl;
+            usage(std::cerr, args[0], "Failed to parse local endpoint");
+            exit(1);
+        }
+    }
+
+    if (args[2] == "get") {
+        if (args.size() != 5) {
+            usage(std::cerr, args[0]);
+            exit(1);
+        }
+        GetCmd c;
+        c.public_key = *util::Ed25519PublicKey::from_hex(args[3]);
+        c.dht_key = args[4];
+        *get_cmd = move(c);
+    }
+    if (args[2] == "put") {
+        if (args.size() != 6) {
+            usage(std::cerr, args[0]);
+            exit(1);
+        }
+        PutCmd c;
+        c.private_key = *util::Ed25519PrivateKey::from_hex(args[3]);
+        c.dht_key = args[4];
+        c.dht_value = args[5];
+        *put_cmd = move(c);
+    }
 }
 
 int main(int argc, const char** argv)
@@ -80,35 +165,91 @@ int main(int argc, const char** argv)
 
     MainlineDht dht(ios);
 
-    std::vector<asio::ip::address> ifaddrs;
+    vector<string> args;
 
-    if (argc <= 1) {
-        ifaddrs = filter( false
-                        , true
-                        , true
-                        , linux_get_addresses());
+    for (int i = 0; i < argc; ++i) {
+        args.push_back(argv[i]);
     }
-    else if (std::string(argv[1]) == "-h") {
-        usage(std::cout, argv[0]);
-        return 0;
-    }
-    else {
-        boost::system::error_code ec;
-        ifaddrs.push_back(asio::ip::make_address(argv[1], ec));
 
-        if (ec) {
-            std::cerr << "Failed parsing \"" << argv[1] << "\" as an IP "
-                      << "address: " << ec.message() << std::endl;
-            usage(std::cerr, argv[0]);
-            return 1;
-        }
-    }
+    vector<asio::ip::address> ifaddrs;
+
+    optional<GetCmd> get_cmd;
+    optional<PutCmd> put_cmd;
+
+    parse_args(args, &ifaddrs, &get_cmd, &put_cmd);
 
     for (auto addr : ifaddrs) {
         std::cout << "Spawning DHT node on " << addr << std::endl;
     }
 
     dht.set_interfaces(ifaddrs);
+
+    asio::spawn(ios, [&] (asio::yield_context yield) {
+        sys::error_code ec;
+        asio::steady_timer timer(ios);
+
+        while (!dht.all_ready() && !ec) {
+            cerr << "Not ready yet..." << endl;
+            timer.expires_from_now(chrono::seconds(1));
+            timer.async_wait(yield[ec]);
+        }
+
+        if (ec) {
+            cerr << "Error timer.async_wait " << ec.message() << endl;
+            return;
+        }
+
+        cerr << "Start" << endl;
+
+        Cancel cancel;
+
+        if (get_cmd) {
+            auto salt = ouinet::util::sha1(get_cmd->dht_key);
+
+            auto opt_data = dht.mutable_get( get_cmd->public_key
+                                           , as_string_view(salt)
+                                           , yield[ec], cancel);
+
+            if (ec) {
+                cerr << "Error dht.mutable_get " << ec.message() << endl;
+            }
+            else {
+                if (opt_data) {
+                    cerr << "Got Data!" << endl;
+                    cerr << "seq:   " << opt_data->sequence_number << endl;
+                    cerr << "value: " << opt_data->value << endl;
+                }
+                else {
+                    cerr << "No error, but also no data!" << endl;
+                }
+            }
+        }
+
+        if (put_cmd) {
+            auto salt = ouinet::util::sha1(put_cmd->dht_key);
+
+            using Time = boost::posix_time::ptime;
+            Time unix_epoch(boost::gregorian::date(1970, 1, 1));
+            Time ts = boost::posix_time::microsec_clock::universal_time();
+
+            auto seq = (ts - unix_epoch).total_milliseconds();
+
+            cerr << "seq: " << seq << endl;
+
+            auto item = MutableDataItem::sign( put_cmd->dht_value
+                                             , seq
+                                             , as_string_view(salt)
+                                             , put_cmd->private_key);
+
+            dht.mutable_put(item, cancel, yield[ec]);
+
+            if (ec) {
+                cerr << "Error dht.mutable_put " << ec.message() << endl;
+            }
+        }
+
+        cerr << "End" << endl;
+    });
 
     ios.run();
 }
