54e35c7f010329a9f390b257a6402b9c1ca9061a
Peter Jankuliak <p.jankuliak@gmail.com>
2019-02-12 12:23:22 +0100

    Add PersistentLruCache class

---
+++ src/util/persistent_lru_cache.h
@@ -0,0 +1,123 @@
+#pragma once
+
+#include <unordered_map>
+#include <list>
+#include <cstddef>
+#include <stdexcept>
+#include <boost/filesystem.hpp>
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/spawn.hpp>
+
+#include "../namespaces.h"
+#include "signal.h"
+
+namespace ouinet { namespace util {
+
+class PersistentLruCache {
+private:
+    struct Element;
+
+    using Key = std::string;
+    using Value = std::shared_ptr<Element>;
+    using KeyVal = std::pair<Key, Value>;
+    using ListIter = typename std::list<KeyVal>::iterator;
+    using Map = std::map<Key, ListIter>;
+    using MapIter = typename Map::iterator;
+
+public:
+    class iterator {
+        friend class PersistentLruCache;
+        MapIter i;
+    public:
+        iterator(MapIter i) : i(i) {}
+        KeyVal& operator*() { return *i->second; }
+        KeyVal* operator->() { return &*i->second; }
+
+        iterator& operator++() {
+            ++i;
+            return *this;
+        }
+
+        iterator operator++(int) {
+            iterator ret{i};
+            ++i;
+            return ret;
+        }
+
+        bool operator==(iterator j) const {
+            return i == j.i;
+        }
+
+        bool operator!=(iterator j) const {
+            return i != j.i;
+        }
+
+        std::vector<uint8_t> value(Cancel&, asio::yield_context);
+    };
+
+private:
+    /* private, use the static `load` function */
+    PersistentLruCache( asio::io_service&
+                      , boost::filesystem::path dir
+                      , size_t max_size);
+
+public:
+    PersistentLruCache(const PersistentLruCache&) = delete;
+    PersistentLruCache(PersistentLruCache&&) = delete;
+
+    static
+    std::unique_ptr<PersistentLruCache> load( asio::io_service&
+                                            , boost::filesystem::path dir
+                                            , size_t max_size
+                                            , Cancel&
+                                            , asio::yield_context);
+
+    void insert( std::string key
+               , std::vector<uint8_t> value
+               , Cancel&
+               , asio::yield_context);
+
+    iterator find(const std::string& key);
+
+    bool exists(const std::string& key) const {
+        return _map.count(key) != 0;
+    }
+
+    size_t size() const {
+        return _map.size();
+    }
+
+    bool empty() const { return _map.empty(); }
+
+    // TODO: Currently the returned iterator is not
+    // ordered by usage.
+    iterator begin() {
+        return iterator(_map.begin());
+    }
+
+    // TODO: Currently the returned iterator is not
+    // ordered by usage.
+    iterator end() {
+        return iterator(_map.end());
+    }
+
+    void move_to_front(iterator i) {
+        _list.splice(_list.begin(), _list, i.i->second);
+    }
+
+    const boost::filesystem::path& dir() const {
+        return _dir;
+    }
+
+private:
+    fs::path path_from_key(const std::string&);
+
+private:
+    asio::io_service& _ios;
+    boost::filesystem::path _dir;
+    std::list<KeyVal> _list;
+    std::map<Key, ListIter> _map;
+    size_t _max_size;
+};
+
+}} // namespaces
