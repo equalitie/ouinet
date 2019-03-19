2dd4d83c6a4b2f221939dcf56538f2e34bcb765b
Peter Jankuliak <p.jankuliak@gmail.com>
2019-03-19 13:45:09 +0100

    Make sure cancel results in operation_aborted in IPFS_LOAD_FUNC

--- src/cache/ipfs_util.h
+++ src/cache/ipfs_util.h
@@ -6,5 +6,8 @@
     [&](auto hash, auto& cancel, auto yield) { \
         function<void()> cancel_fn; \
         auto cancel_handle = cancel.connect([&] { if (cancel_fn) cancel_fn(); }); \
-        return (node).cat(hash, cancel_fn, yield); \
+        sys::error_code ec; \
+        auto ret = (node).cat(hash, cancel_fn, yield[ec]); \
+        if (cancel_handle) ec = asio::error::operation_aborted; \
+        return or_throw(yield, ec, std::move(ret)); \
     }
