ad64a095814faad5f6a4edc939928573a459cbdf
Sarah Fortune <sarah.fortune@gmail.com>
2019-05-10 16:38:01 -0700

    Add a start method to Ouinet to separate creating the object and starting the service.
    Make methods that are not needed outside of this class private.

--- android/ouinet/src/main/java/ie/equalit/ouinet/Ouinet.java
+++ android/ouinet/src/main/java/ie/equalit/ouinet/Ouinet.java
@@ -24,7 +24,9 @@ public class Ouinet {
         System.setProperty("https.proxyPort", "8080");
     }
     private static final String TAG = "Ouinet";
-    private Context _ctx;
+
+    private final Context ctx;
+    private final Config config;
     private WifiManager.MulticastLock _lock = null;
 
     public static class Config {
@@ -38,12 +40,14 @@ public class Ouinet {
         public String tls_ca_cert_store_path;
     }
 
-    public Ouinet(Context ctx, Config conf) {
-        _ctx = ctx;
+    public Ouinet(Context ctx, Config config) {
+        this.ctx = ctx;
+        this.config = config;
+    }
 
+    public void start() {
         List<String> args = new ArrayList<String>();
         List<String> path = new ArrayList<String>();
-
 
         new File(dir()).mkdirs();
 
@@ -65,21 +69,21 @@ public class Ouinet {
         //args.add("--disable-proxy-access");
         //args.add("--disable-injector-access");
 
-        maybeAdd(args, "--injector-ep",            conf.injector_endpoint);
-        maybeAdd(args, "--injector-credentials",   conf.injector_credentials);
-        maybeAdd(args, "--index-bep44-public-key", conf.index_bep44_pubkey);
-        maybeAdd(args, "--index-ipns-id",          conf.index_ipns_id);
+        maybeAdd(args, "--injector-ep",            config.injector_endpoint);
+        maybeAdd(args, "--injector-credentials",   config.injector_credentials);
+        maybeAdd(args, "--index-bep44-public-key", config.index_bep44_pubkey);
+        maybeAdd(args, "--index-ipns-id",          config.index_ipns_id);
 
-        if (conf.tls_ca_cert_store_path != null) {
+        if (config.tls_ca_cert_store_path != null) {
             String ca_cert_path;
             String assetPrefix = "file:///android_asset/";
 
-            if (conf.tls_ca_cert_store_path.startsWith(assetPrefix)) {
+            if (config.tls_ca_cert_store_path.startsWith(assetPrefix)) {
                 // TODO: Ouinet's C++ code doesn't yet have a way to read asset
                 // files from the apk. As a temporary workaround we copy the
                 // asset file to a regular file and the pass path to that to
                 // the C++ code.
-                String asset = conf.tls_ca_cert_store_path.substring(assetPrefix.length());
+                String asset = config.tls_ca_cert_store_path.substring(assetPrefix.length());
                 ca_cert_path = dir() + "/assets/" + asset;
 
                 if (copyAssetToFile(asset, ca_cert_path)) {
@@ -87,14 +91,14 @@ public class Ouinet {
                 }
             }
             else {
-                maybeAdd(args, "--tls-ca-cert-store-path", conf.tls_ca_cert_store_path);
+                maybeAdd(args, "--tls-ca-cert-store-path", config.tls_ca_cert_store_path);
             }
         }
 
         try {
-            if (conf.injector_tls_cert != null) {
+            if (config.injector_tls_cert != null) {
                 String cert_path = dir() + "/injector-tls-cert.pem";
-                writeToFile(cert_path, conf.injector_tls_cert.getBytes());
+                writeToFile(cert_path, config.injector_tls_cert.getBytes());
                 args.add("--injector-tls-cert-file=" + cert_path);
             }
         } catch (IOException e) {
@@ -117,41 +121,12 @@ public class Ouinet {
         return nPathToCARootCert();
     }
 
-    public boolean copyAssetToFile(String asset, String path)
-    {
-        try {
-            java.io.InputStream stream = _ctx.getAssets().open(asset);
-            int size = stream.available();
-            byte[] buffer = new byte[size];
-            stream.read(buffer);
-            stream.close();
-            writeToFile(path, buffer);
-        } catch (IOException e) {
-            Log.d(TAG, "Failed to write asset \"" + asset + "\" to file \"" + path + "\"", e);
-            return false;
-        }
-
-        return true;
-    }
-
-    public boolean copyExecutableToFile(String asset, String path) {
-        if (!copyAssetToFile(asset, path)) {
-            return false;
-        }
-        File executable = new File(path);
-        if (!executable.setExecutable(true)) {
-            Log.d(TAG, "Failed to set executable for file: " + path);
-            return false;
-        }
-        return true;
-    }
-
     // If this succeeds, we should be able to do UDP multicasts
     // from inside ouinet (currently know to be needed by IPFS' mDNS
     // but that's not essential for WAN).
     public boolean acquireMulticastLock()
     {
-        WifiManager wifi = (WifiManager) _ctx.getSystemService(Context.WIFI_SERVICE);
+        WifiManager wifi = (WifiManager) ctx.getSystemService(Context.WIFI_SERVICE);
 
         if (wifi == null){
             return false;
@@ -193,6 +168,35 @@ public class Ouinet {
     }
 
     //----------------------------------------------------------------
+    private boolean copyAssetToFile(String asset, String path)
+    {
+        try {
+            java.io.InputStream stream = ctx.getAssets().open(asset);
+            int size = stream.available();
+            byte[] buffer = new byte[size];
+            stream.read(buffer);
+            stream.close();
+            writeToFile(path, buffer);
+        } catch (IOException e) {
+            Log.d(TAG, "Failed to write asset \"" + asset + "\" to file \"" + path + "\"", e);
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean copyExecutableToFile(String asset, String path) {
+        if (!copyAssetToFile(asset, path)) {
+            return false;
+        }
+        File executable = new File(path);
+        if (!executable.setExecutable(true)) {
+            Log.d(TAG, "Failed to set executable for file: " + path);
+            return false;
+        }
+        return true;
+    }
+
     private void maybeAdd(List<String> args, String key, String value) {
         if (value == null || value.isEmpty()) return;
         args.add(key + "=" + value);
@@ -214,7 +218,7 @@ public class Ouinet {
 
     //----------------------------------------------------------------
     private String dir() {
-        return _ctx.getFilesDir().getAbsolutePath() + "/ouinet";
+        return ctx.getFilesDir().getAbsolutePath() + "/ouinet";
     }
 
     //----------------------------------------------------------------
