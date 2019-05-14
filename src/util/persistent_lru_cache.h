a1043270e4b122266a0b084579d12178570c4456
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2019-05-14 15:53:27 -0400

    Use atomic files in persistent LRU cache.

--- src/util/persistent_lru_cache.h
+++ src/util/persistent_lru_cache.h
@@ -18,10 +18,12 @@
 namespace ouinet { namespace util {
 
 namespace persisten_lru_cache_detail {
+    static const auto temp_file_prefix = "tmp.";
+    static const auto temp_file_model = std::string(temp_file_prefix) + "%%%%-%%%%-%%%%-%%%%";
+
     uint64_t ms_since_epoch();
     fs::path path_from_key(const fs::path&, const std::string&);
     bool is_cache_entry(const struct dirent*);
-    fs::path temp_file_along(const fs::path&, sys::error_code&);
 } // detail namespace
 
 template<class Value>
@@ -189,21 +191,19 @@ public:
 
         sys::error_code ec;
 
-        // TODO: Factor out along with `InjectorCacheControl::save_to_disk`.
         // Create a new entry file "atomically" (at least inside the program)
         // by writing data to a temporary file and replacing the existing file.
         // Otherwise we might be overwriting old data that others are reading.
-        auto temp_path = temp_file_along(_path, ec);
+        auto af = file_io::mkatomic( _ios, ec, _path
+                                   , persisten_lru_cache_detail::temp_file_model);
         if (ec) return or_throw(yield, ec);
-        auto f = file_io::open_or_create(_ios, temp_path, ec);
-        auto remove_temp = defer([&] { file_io::remove_file(temp_path); });
-
+        auto& f = af->lowest_layer();
         if (!ec) file_io::write_number<uint64_t>(f, ts, cancel, yield[ec]);
         if (!ec) file_io::write_number<uint32_t>(f, _key.size(), cancel, yield[ec]);
         if (!ec) file_io::write(f, asio::buffer(_key), cancel, yield[ec]);
         //if (!ec) file_io::write(f, asio::buffer(value), cancel, yield[ec]);
         if (!ec) _value.write(f, cancel, yield[ec]);
-        if (!ec) { f.close(); fs::rename(temp_path, _path, ec); };
+        if (!ec) af->commit(ec);
 
         return or_throw(yield, ec);
     }
