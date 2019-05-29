1eb23526aa33894ffd09be8e9cfec2a865957780
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2019-05-29 16:30:31 -0400

    Split out basic half-duplex operation for reuse.

--- src/full_duplex_forward.h
+++ src/full_duplex_forward.h
@@ -7,35 +7,37 @@
 
 namespace ouinet {
 
+static const auto half_duplex_timeout = std::chrono::seconds(60);
+
+// Low-level, one-direction operation.
+template<class Stream1, class Stream2>
+inline
+void half_duplex( Stream1& in, Stream2& out
+                , WatchDog& wdog
+                , asio::yield_context& yield)
+{
+    sys::error_code ec;
+    std::array<uint8_t, 2048> data;
+
+    for (;;) {
+        size_t length = in.async_read_some(asio::buffer(data), yield[ec]);
+        if (ec) break;
+
+        asio::async_write(out, asio::buffer(data, length), yield[ec]);
+        if (ec) break;
+
+        wdog.expires_after(half_duplex_timeout);
+    }
+}
+
 template<class Stream1, class Stream2>
 inline
 void full_duplex(Stream1 c1, Stream2 c2, asio::yield_context yield)
 {
-    static const auto timeout = std::chrono::seconds(60);
-
-    static const auto half_duplex = []( auto& in
-                                      , auto& out
-                                      , auto& wdog
-                                      , asio::yield_context& yield)
-    {
-        sys::error_code ec;
-        std::array<uint8_t, 2048> data;
-
-        for (;;) {
-            size_t length = in.async_read_some(asio::buffer(data), yield[ec]);
-            if (ec) break;
-
-            asio::async_write(out, asio::buffer(data, length), yield[ec]);
-            if (ec) break;
-
-            wdog.expires_after(timeout);
-        }
-    };
-
     assert(&c1.get_io_service() == &c2.get_io_service());
 
     WatchDog wdog( c1.get_io_service()
-                 , timeout
+                 , half_duplex_timeout
                  , [&] { c1.close(); c2.close(); });
 
     WaitCondition wait_condition(c1.get_io_service());
