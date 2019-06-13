4f6dbef502e8788ec00d3cc2cea52d6b14b9847b
Peter Jankuliak <p.jankuliak@gmail.com>
2019-06-13 13:09:39 +0200

    Make CacheClient an implementation of AbstractCache

--- src/cache/cache_client.h
+++ src/cache/cache_client.h
@@ -8,20 +8,20 @@
 #include <memory>
 #include <string>
 
-#include "cache_entry.h"
+#include "abstract_cache.h"
 #include "../namespaces.h"
 #include "../util/yield.h"
+#include "../util/scheduler.h"
 
 namespace asio_ipfs { class node; }
 namespace ouinet { namespace bittorrent { class MainlineDht; }}
 namespace ouinet { namespace util { class Ed25519PublicKey; }}
-namespace ouinet { class BTree; }
 
 namespace ouinet {
 
 class Bep44ClientIndex;
 
-class CacheClient {
+class CacheClient : public AbstractCache {
 public:
     // Construct the CacheClient without blocking the main thread as
     // constructing asio_ipfs::node takes some time.
@@ -32,6 +32,7 @@ public:
           , fs::path path_to_repo
           , bool autoseed_updated
           , unsigned int bep44_index_capacity
+          , bool wait_for_ready
           , Cancel& cancel
           , boost::asio::yield_context);
 
@@ -41,7 +42,15 @@ public:
     CacheClient(CacheClient&&) = delete;
     CacheClient& operator=(CacheClient&&) = delete;
 
+    CacheEntry load(const std::string& key, Cancel, Yield) override;
+
+    void store(const std::string& key, Response&, Cancel, asio::yield_context) override;
+
+    ~CacheClient();
+
     std::string ipfs_add(const std::string& content, boost::asio::yield_context);
+
+    std::string get_descriptor(const std::string& key, Cancel&, Yield);
 
     // Insert a signed key->descriptor mapping
     // into the index of the given type.
@@ -52,6 +61,9 @@ public:
                               , Cancel&
                               , boost::asio::yield_context);
 
+    std::string ipfs_id() const;
+
+private:
     // Find the content previously stored by the injector under `key`.
     // The descriptor identifier and cached content are returned.
     //
@@ -62,19 +74,35 @@ public:
                                                   , Cancel&
                                                   , Yield);
 
-    std::string get_descriptor(const std::string& key, Cancel&, Yield);
-
     std::string descriptor_from_path( const std::string& desc_path
                                     , Cancel&
                                     , asio::yield_context);
-
-    std::string ipfs_id() const;
 
     std::string ipfs() const;
 
     void wait_for_ready(Cancel&, boost::asio::yield_context) const;
 
-    ~CacheClient();
+    static
+    bool has_descriptor_hdr(const Response& rs);
+
+    static
+    bool has_bep44_insert_hdr(const Response& rs);
+
+    void seed_descriptor( const std::string& target
+                        , const std::string& encoded_desc
+                        , Scheduler& scheduler
+                        , Cancel& cancel
+                        , asio::yield_context yield);
+
+    void seed_response( const std::string& target
+                      , const Response& rs
+                      , Scheduler& scheduler
+                      // These three are for debugging
+                      , std::chrono::steady_clock::duration& scheduler_duration
+                      , std::chrono::steady_clock::duration& bep44_duration
+                      , std::chrono::steady_clock::duration& ipfs_add_duration
+                      , Cancel& cancel
+                      , asio::yield_context yield);
 
 private:
     // Private, use the static `build` function instead
@@ -90,6 +118,9 @@ private:
     std::unique_ptr<asio_ipfs::node> _ipfs_node;
     std::shared_ptr<bittorrent::MainlineDht> _bt_dht;
     std::unique_ptr<Bep44ClientIndex> _index;
+    Cancel _cancel;
+    Scheduler _store_scheduler;
+    Scheduler _fetch_stored_scheduler;
 };
 
 } // namespace
