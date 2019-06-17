2cbf180c50652176a69c47224dec35e09e89d19f
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2019-06-17 16:57:39 -0400

    Undo changes to full duplex forward code.
    The splitted-out, public half-duplex forward operation was no longer being
    used, with the HTTP forward operation taking care of it in a more customizable
    manner.

--- src/full_duplex_forward.h
+++ src/full_duplex_forward.h
@@ -1,101 +1,41 @@
 #pragma once
 
-#include <boost/asio/buffer.hpp>
 #include <boost/asio/read.hpp>
-
-#include "default_timeout.h"
 #include "generic_stream.h"
 #include "util/wait_condition.h"
 #include "util/watch_dog.h"
 
 namespace ouinet {
 
-static const size_t half_duplex_default_block = 2048;
-
-// Low-level, one-direction operation.
-// `already_read` bytes already present in `buffer` will be sent in the first batch.
-template<class StreamIn, class StreamOut>
-inline
-void half_duplex( StreamIn& in, StreamOut& out
-                , asio::mutable_buffer& buffer
-                , size_t already_read, size_t max_transfer
-                , WatchDog& wdog
-                , asio::yield_context& yield)
-{
-    assert(max_transfer >= already_read);
-    sys::error_code ec;
-
-    while (max_transfer > 0) {
-        auto buf = asio::buffer(buffer, max_transfer);
-        size_t length = already_read + in.async_read_some(buf + already_read, yield[ec]);
-        already_read = 0;  // only usable on first read
-        if (ec) break;
-
-        asio::async_write(out, asio::buffer(buf, length), yield[ec]);
-        if (ec) break;
-
-        max_transfer -= length;
-
-        wdog.expires_after(default_timeout::half_duplex());
-    }
-}
-
-template<class StreamIn, class StreamOut>
-inline
-void half_duplex( StreamIn& in, StreamOut& out
-                , WatchDog& wdog
-                , asio::yield_context& yield)
-{
-    std::array<uint8_t, half_duplex_default_block> data;
-    auto buffer = asio::buffer(data);
-    return half_duplex( in, out, buffer, 0
-                      , std::numeric_limits<std::size_t>::max()
-                      , wdog, yield);
-}
-
-template<class StreamIn, class StreamOut>
-inline
-void half_duplex( StreamIn& in, StreamOut& out
-                , asio::mutable_buffer& buffer
-                , size_t already_read, size_t max_transfer
-                , asio::yield_context yield)
-{
-    assert(&in.get_io_service() == &out.get_io_service());
-
-    WatchDog wdog( in.get_io_service()
-                 , default_timeout::half_duplex()
-                 , [&] { in.close(); out.close(); });
-
-    WaitCondition wait_condition(in.get_io_service());
-
-    asio::spawn
-        ( yield
-        , [&, lock = wait_condition.lock()](asio::yield_context yield) {
-              half_duplex(in, out, buffer, already_read, max_transfer, wdog, yield);
-          });
-
-    wait_condition.wait(yield);
-}
-
-template<class StreamIn, class StreamOut>
-inline
-void half_duplex(StreamIn& in, StreamOut& out, asio::yield_context yield)
-{
-    std::array<uint8_t, half_duplex_default_block> data;
-    auto buffer = asio::buffer(data);
-    return half_duplex( in, out, buffer, 0
-                      , std::numeric_limits<std::size_t>::max()
-                      , yield);
-}
-
 template<class Stream1, class Stream2>
 inline
 void full_duplex(Stream1 c1, Stream2 c2, asio::yield_context yield)
 {
+    static const auto timeout = std::chrono::seconds(60);
+
+    static const auto half_duplex = []( auto& in
+                                      , auto& out
+                                      , auto& wdog
+                                      , asio::yield_context& yield)
+    {
+        sys::error_code ec;
+        std::array<uint8_t, 2048> data;
+
+        for (;;) {
+            size_t length = in.async_read_some(asio::buffer(data), yield[ec]);
+            if (ec) break;
+
+            asio::async_write(out, asio::buffer(data, length), yield[ec]);
+            if (ec) break;
+
+            wdog.expires_after(timeout);
+        }
+    };
+
     assert(&c1.get_io_service() == &c2.get_io_service());
 
     WatchDog wdog( c1.get_io_service()
-                 , default_timeout::half_duplex()
+                 , timeout
                  , [&] { c1.close(); c2.close(); });
 
     WaitCondition wait_condition(c1.get_io_service());
