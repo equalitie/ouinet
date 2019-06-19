9d3e5b20c1e83db4bf10c30e3c83505db702e4ff
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2019-06-19 11:23:53 -0400

    Use `ouinet::parse::number` for getting content length.

--- src/http_forward.cpp
+++ src/http_forward.cpp
@@ -1,14 +1,18 @@
 #include "http_forward.h"
+#include "parse/number.h"
 
 namespace ouinet { namespace detail {
 
 size_t
 get_content_length(const http::response_header<>& rph, sys::error_code& ec) {
-    auto length = util::parse_num<size_t>( rph[http::field::content_length]
-                                         , max_size_t);
-    if (length == max_size_t && rph.version() != 10)
-        ec = asio::error::invalid_argument;
-    return length;
+    boost::string_view cl = rph[http::field::content_length];
+    auto length = parse::number<size_t>(cl);  // alters `cl` boundaries
+    if (!length) {
+        if (rph.version() != 10)
+            ec = asio::error::invalid_argument;
+        return max_size_t;
+    }
+    return *length;
 }
 
 http::fields
