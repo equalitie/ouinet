920abe3afd3b4183d6d3d7e7eda9d92cf775d222
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2019-07-04 18:14:54 -0400

    Add creation time stamp to signature.

--- src/cache/http_sign.cpp
+++ src/cache/http_sign.cpp
@@ -25,10 +25,15 @@ http_signature( const http::response_header<>& rsh
               , const ouinet::util::Ed25519PrivateKey& sk)
 {
     // TODO: Compute proper signature string and sign it.
-    auto fmt = boost::format("keyId=\"ed25519:%s\",algorithm=\"hs2019\"");
+    auto fmt = boost::format("keyId=\"ed25519:%s\""
+                             ",algorithm=\"hs2019\""
+                             ",created=%d");
+
     // TODO: Cache somewhere, doing this every time is quite inefficient.
     auto encoded_pk = ouinet::util::base64_encode(sk.public_key().serialize());
-    return (fmt % encoded_pk).str();
+    auto ts = std::chrono::seconds(std::time(nullptr)).count();
+
+    return (fmt % encoded_pk % ts).str();
 }
 
 }} // namespaces
