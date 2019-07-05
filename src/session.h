5d07f6d796d4651eb6a1eeb43cf481c6f3cbd2d7
Peter Jankuliak <p.jankuliak@gmail.com>
2019-07-05 15:09:11 +0200

    Make Session moveable

--- src/session.h
+++ src/session.h
@@ -5,14 +5,32 @@
 namespace ouinet { namespace cache {
 
 class Session {
+private:
+    struct State {
+        GenericStream con;
+        beast::static_buffer<16384> buffer;
+        http::response_parser<http::buffer_body> parser;
+
+        State(GenericStream&& con) : con(std::move(con)) {}
+    };
+
 public:
+    Session() = default;
+
+    Session(const Session&) = delete;
+    Session& operator=(const Session&) = delete;
+
+    Session(Session&&) = default;
+    Session& operator=(Session&&) = default;
+
     Session(GenericStream con)
-        : _con(std::move(con))
+        : _state(new State(std::move(con)))
     {}
 
     http::response_header<>* response_header() {
-        if (!_parser.is_header_done()) return nullptr;
-        return &_parser.get().base();
+        if (!_state) return nullptr;
+        if (!_state->parser.is_header_done()) return nullptr;
+        return &_state->parser.get().base();
     }
 
     http::response_header<>* read_response_header(Cancel&, asio::yield_context);
@@ -21,26 +39,32 @@ public:
     void flush_response(SinkStream&, Cancel&, asio::yield_context);
 
 private:
-    GenericStream _con;
-    beast::static_buffer<16384> _buffer;
-    http::response_parser<http::buffer_body> _parser;
+    std::unique_ptr<State> _state;
 };
 
 inline
 http::response_header<>*
 Session::read_response_header(Cancel& cancel, asio::yield_context yield)
 {
+    if (!_state) {
+        return or_throw<http::response_header<>*>(
+                yield, asio::error::bad_descriptor);
+    }
+
     if (auto h = response_header()) return h;
 
-    auto c = cancel.connect([&] { _con.close(); });
+    auto c = cancel.connect([&] { _state->con.close(); });
 
     sys::error_code ec;
-    http::async_read_header(_con, _buffer, _parser, yield[ec]);
+    http::async_read_header(_state->con,
+                            _state->buffer,
+                            _state->parser,
+                            yield[ec]);
 
     if (c) ec = asio::error::operation_aborted;
     if (ec) return or_throw<http::response_header<>*>(yield, ec, nullptr);
 
-    assert(_parser.is_header_done());
+    assert(_state->parser.is_header_done());
 
     return response_header();
 }
@@ -52,14 +76,18 @@ Session::flush_response(SinkStream& sink,
                         Cancel& cancel,
                         asio::yield_context yield)
 {
+    if (!_state) {
+        return or_throw(yield, asio::error::bad_descriptor);
+    }
+
     // Used this as an example
     // https://www.boost.org/doc/libs/1_70_0/libs/beast/doc/html/beast/more_examples/http_relay.html
 
-    auto c = cancel.connect([&] { _con.close(); });
+    auto c = cancel.connect([&] { _state->con.close(); });
 
     sys::error_code ec;
 
-    http::response_serializer<http::buffer_body> sr{_parser.get()};
+    http::response_serializer<http::buffer_body> sr{_state->parser.get()};
     
     read_response_header(cancel , yield[ec]); // Won't read if already read.
     return_or_throw_on_error(yield, cancel, ec);
@@ -70,27 +98,27 @@ Session::flush_response(SinkStream& sink,
     char buf[2048];
 
     do {
-        if (!_parser.is_done()) {
-            _parser.get().body().data = buf;
-            _parser.get().body().size = sizeof(buf);
-            http::async_read(_con, _buffer, _parser, yield[ec]);
+        if (!_state->parser.is_done()) {
+            _state->parser.get().body().data = buf;
+            _state->parser.get().body().size = sizeof(buf);
+            http::async_read(_state->con, _state->buffer, _state->parser, yield[ec]);
 
             if (ec == http::error::need_buffer) ec = {};
             return_or_throw_on_error(yield, cancel, ec);
 
-            _parser.get().body().size = sizeof(buf) - _parser.get().body().size;
-            _parser.get().body().data = buf;
-            _parser.get().body().more = !_parser.is_done();
+            _state->parser.get().body().size = sizeof(buf) - _state->parser.get().body().size;
+            _state->parser.get().body().data = buf;
+            _state->parser.get().body().more = !_state->parser.is_done();
         } else {
-            _parser.get().body().data = nullptr;
-            _parser.get().body().size = 0;
+            _state->parser.get().body().data = nullptr;
+            _state->parser.get().body().size = 0;
         }
 
         http::async_write(sink, sr, yield[ec]);
         if (ec == http::error::need_buffer) ec = {};
         return_or_throw_on_error(yield, cancel, ec);
     }
-    while (!_parser.is_done() && !sr.is_done());
+    while (!_state->parser.is_done() && !sr.is_done());
 }
 
 }} // namespaces
