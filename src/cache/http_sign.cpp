ada3a867a5bb7277e21b97adec1e12ccbaf1208c
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2019-07-05 17:10:12 -0400

    Compute signature string and signature `headers` parameter.

--- src/cache/http_sign.cpp
+++ src/cache/http_sign.cpp
@@ -1,5 +1,11 @@
 #include "http_sign.h"
 
+#include <map>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include <boost/algorithm/string/case_conv.hpp>
 #include <boost/beast/http/field.hpp>
 
 #include "../util.h"
@@ -20,6 +26,61 @@ http_digest(const http::response<http::dynamic_body>& rs)
     return "SHA-256=" + encoded_digest;
 }
 
+template<class Head>
+static void
+prep_sig_head(const Head& inh, Head& outh)
+{
+    using namespace std;
+
+    // Lowercase header names, to more-or-less respect input order.
+    vector<string> hdr_sorted;
+    // Lowercase header name to `, `-concatenated, trimmed values.
+    map<string, string> hdr_values;
+
+    for (auto& hdr : inh) {
+        auto name = hdr.name_string().to_string();  // lowercase
+        boost::algorithm::to_lower(name);
+
+        auto value_v = hdr.value();  // trimmed
+        while (value_v.starts_with(' ')) value_v.remove_prefix(1);
+        while (value_v.ends_with  (' ')) value_v.remove_suffix(1);
+
+        auto vit = hdr_values.find(name);
+        if (vit == hdr_values.end()) {  // new entry, add
+            hdr_values[name] = value_v.to_string();
+            hdr_sorted.push_back(name);
+        } else {  // existing entry, concatenate
+            vit->second += ", ";
+            vit->second.append(value_v.data(), value_v.length());
+        }
+    }
+
+    for (auto name : hdr_sorted)
+        outh.set(name, hdr_values[name]);
+}
+
+template<class Head>
+static std::pair<std::string, std::string>
+get_sig_str_hdrs(const Head& sig_head)
+{
+    std::string sig_string, headers;
+    bool ins_sep = false;
+    for (auto& hdr : sig_head) {
+        auto name = hdr.name_string();
+        auto value = hdr.value();
+
+        if (ins_sep) sig_string += '\n';
+        sig_string += (boost::format("%s: %s") % name % value).str();
+
+        if (ins_sep) headers += ' ';
+        headers.append(name.data(), name.length());
+
+        ins_sep = true;
+    }
+
+    return {sig_string, headers};
+}
+
 std::string
 http_signature( const http::response_header<>& rsh
               , const ouinet::util::Ed25519PrivateKey& sk)
@@ -27,13 +88,21 @@ http_signature( const http::response_header<>& rsh
     // TODO: Compute proper signature string and sign it.
     auto fmt = boost::format("keyId=\"ed25519=%s\""
                              ",algorithm=\"hs2019\""
-                             ",created=%d");
+                             ",created=%d"
+                             ",headers=\"%s\"");
 
     // TODO: Cache somewhere, doing this every time is quite inefficient.
     auto encoded_pk = ouinet::util::base64_encode(sk.public_key().serialize());
     auto ts = std::chrono::seconds(std::time(nullptr)).count();
 
-    return (fmt % encoded_pk % ts).str();
+    http::response_header<> sig_head;
+    sig_head.set("(created)", ts);
+    prep_sig_head(rsh, sig_head);  // unique fields, lowercase names, trimmed values
+
+    std::string sig_string, headers;
+    std::tie(sig_string, headers) = get_sig_str_hdrs(sig_head);
+
+    return (fmt % encoded_pk % ts % headers).str();
 }
 
 }} // namespaces
