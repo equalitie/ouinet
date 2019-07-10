71d2812ad6533d4103f486a3cb08edc4202603bc
Sarah Fortune <sarah.fortune@gmail.com>
2019-07-10 14:06:53 -0700

    Use a temporary directory for unit test files.
    Assert that the files created by the test contain the expected content.

--- android/ouinet/src/test/java/ie/equalit/ouinet/ConfigTest.java
+++ android/ouinet/src/test/java/ie/equalit/ouinet/ConfigTest.java
@@ -3,7 +3,9 @@ package ie.equalit.ouinet;
 import android.content.Context;
 import android.content.res.AssetManager;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.powermock.api.mockito.PowerMockito;
@@ -17,8 +19,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
+import static org.assertj.core.api.Assertions.contentOf;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.when;
 
@@ -31,26 +33,37 @@ public class ConfigTest {
     private static String INJECTOR_ENDPOINT = "injectorendpoint789123";
     private static String INJECTOR_CREDENTIALS = "injectorcredentials29384293847928498492849284";
     private static String INJECTOR_TLS_CERT = "injectortlscert123123123123123";
-    private static String TLS_CA_CERT_PATH = "/tls/store/path.pem";
-    private static String FILES_DIR = "/data/ie.equalit.ceno/files/";
-    private static String OUINET_DIR = "/data/ie.equalit.ceno/files/ouinet";
-    private static String CA_ROOT_CERT_PATH = "/data/ie.equalit.ceno/files/ouinet/ssl-ca-cert.pem";
+    private static String TLS_CA_CERT_PATH = "file:///android_asset/tls-ca-cert.pem";
+    private static String TLS_CA_CERT = "tlscacertcontent";
+    private static String OBFS_PROXY_CONTENT = "obfs4assetcontent";
 
     @Mock
     private Context mockContext;
     @Mock
     private AssetManager mockAssetManager;
+    @Rule
+    public TemporaryFolder tmpDir = new TemporaryFolder();
 
     @Test
     public void test_build() throws IOException {
-        when(mockContext.getFilesDir()).thenReturn(new File(FILES_DIR));
+        File filesDir = tmpDir.newFolder("files/");
+        String ouinetDir = filesDir.getPath() + "/ouinet";
+        String caRootCertPath = ouinetDir + "/ssl-ca-cert.pem";
+        String injectorTlsCertPath = ouinetDir + "/injector-tls-cert.pem";
+        String tlsCaCertPath = ouinetDir + "/assets/tls-ca-cert.pem";
+        String obfsFilePath = ouinetDir + "/" + Config.OBFS4_PROXY;
 
+        when(mockContext.getFilesDir()).thenReturn(filesDir);
         when(mockContext.getAssets()).thenReturn(mockAssetManager);
-        InputStream is = new ByteArrayInputStream("obfs4assetcontent".getBytes(StandardCharsets.UTF_8));
-        when(mockAssetManager.open(Config.OBFS4_PROXY)).thenReturn(is);
+        InputStream obfsproxyIs =
+                new ByteArrayInputStream(OBFS_PROXY_CONTENT.getBytes(StandardCharsets.UTF_8));
+        when(mockAssetManager.open(Config.OBFS4_PROXY)).thenReturn(obfsproxyIs);
+        InputStream tlsCaCertIs =
+                new ByteArrayInputStream(TLS_CA_CERT.getBytes(StandardCharsets.UTF_8));
+        when(mockAssetManager.open("tls-ca-cert.pem")).thenReturn(tlsCaCertIs);
 
         PowerMockito.mockStatic(Ouinet.class);
-        when(Ouinet.getCARootCert(OUINET_DIR)).thenReturn(CA_ROOT_CERT_PATH);
+        when(Ouinet.getCARootCert(ouinetDir)).thenReturn(caRootCertPath);
 
         Config config = new Config.ConfigBuilder(mockContext)
                 .setIndexBep44PubKey(INDEX_BEP_44_PUB_KEY)
@@ -60,14 +73,20 @@ public class ConfigTest {
                 .setInjectorTlsCert(INJECTOR_TLS_CERT)
                 .setTlsCaCertStorePath(TLS_CA_CERT_PATH)
                 .build();
-        assertThat(config.getOuinetDirectory(), is(OUINET_DIR));
+        assertThat(config.getOuinetDirectory(), is(ouinetDir));
         assertThat(config.getIndexBep44PubKey(), is(INDEX_BEP_44_PUB_KEY));
         assertThat(config.getIndexIpnsId(), is(INDEX_IPNS_ID));
         assertThat(config.getInjectorEndpoint(), is(INJECTOR_ENDPOINT));
         assertThat(config.getInjectorCredentials(), is(INJECTOR_CREDENTIALS));
-        assertThat(config.getInjectorTlsCertPath(), is(nullValue())); // Cannot write to file in unit test
-        assertThat(config.getTlsCaCertStorePath(), is(TLS_CA_CERT_PATH));
-        assertThat(config.getCaRootCertPath(), is(CA_ROOT_CERT_PATH));
-        assertThat(config.getObfs4ProxyPath(), is(nullValue())); // Cannot write to file in unit test
+        assertThat(config.getCaRootCertPath(), is(caRootCertPath));
+
+        assertThat(config.getTlsCaCertStorePath(), is(tlsCaCertPath));
+        assertThat(contentOf(new File(config.getTlsCaCertStorePath())), is(TLS_CA_CERT));
+
+        assertThat(config.getInjectorTlsCertPath(), is(injectorTlsCertPath));
+        assertThat(contentOf(new File(config.getInjectorTlsCertPath())), is(INJECTOR_TLS_CERT));
+
+        assertThat(config.getObfs4ProxyPath(), is(ouinetDir));
+        assertThat(contentOf(new File(obfsFilePath)), is(OBFS_PROXY_CONTENT));
     }
 }
