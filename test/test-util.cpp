f11dbe8a67be85858d7397523a2b96c9ecd4d7a2
Peter Jankuliak <p.jankuliak@gmail.com>
2019-08-21 13:13:43 +0200

    Add tests to test-utils.cpp

--- test/test-util.cpp
+++ test/test-util.cpp
@@ -5,6 +5,7 @@
 #include <boost/asio/spawn.hpp>
 #include <namespaces.h>
 #include <async_sleep.h>
+#include <util/async_generator.h>
 #include <iostream>
 #include <chrono>
 
@@ -39,7 +40,7 @@ BOOST_AUTO_TEST_CASE(test_cancel) {
                     ios.post(yield);
                     cancel();
                 });
-                
+
                 BOOST_REQUIRE(!cancel);
                 async_sleep(ios, 1s, cancel, yield[ec]);
                 BOOST_REQUIRE(millis_since(start) < 100);
@@ -68,6 +69,72 @@ BOOST_AUTO_TEST_CASE(test_cancel) {
                 BOOST_REQUIRE(!c2);
                 async_sleep(ios, 1s, c2, yield[ec]);
                 BOOST_REQUIRE(millis_since(start) < 100);
+        });
+    }
+
+    {
+        asio::io_service ios;
+
+        asio::spawn(ios, [&] (asio::yield_context yield) {
+                sys::error_code ec;
+                Cancel c;
+
+                {
+                    Cancel cc = c;
+                }
+
+                c();
+        });
+
+        ios.run();
+    }
+
+    {
+        Cancel parent;
+        unique_ptr<Cancel> child1(new Cancel(parent));
+        Cancel child2(std::move(*child1));
+        child1 = nullptr;
+        parent();
+    }
+}
+
+BOOST_AUTO_TEST_CASE(test_async_generator) {
+    {
+        using util::AsyncGenerator;
+
+        asio::io_service ios;
+
+        asio::spawn(ios, [&] (asio::yield_context yield) {
+            AsyncGenerator<int> gen(ios, [&] (auto& q, auto c, auto y) {
+                q.push_back(1);
+            });
+
+            Cancel cancel;
+            auto opt_val = gen.async_get_value(cancel, yield);
+
+            BOOST_REQUIRE(opt_val && *opt_val == 1);
+        });
+
+        ios.run();
+    }
+
+    {
+        using util::AsyncGenerator;
+
+        asio::io_service ios;
+
+        asio::spawn(ios, [&] (asio::yield_context yield) {
+            AsyncGenerator<int> gen(ios, [&] (auto& q, auto c, auto y) {
+                ios.post(y);
+                q.push_back(1);
+                ios.post(y);
+                if (c) or_throw(y, asio::error::operation_aborted);
+            });
+
+            Cancel cancel;
+            auto opt_val = gen.async_get_value(cancel, yield);
+
+            BOOST_REQUIRE(opt_val && *opt_val == 1);
         });
 
         ios.run();
