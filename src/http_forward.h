1f9e94ee79fbcca7fc1dd11ec829bd9e0f62e388
Peter Jankuliak <p.jankuliak@gmail.com>
2019-10-11 12:22:58 -0400

    Revert "Increment http_forward_block 2048 -> 16384"
    This reverts commit 32b40bab32c26a9e194cf0b3c8c8fd47dc06561e.
    
    It was causing websites not to load properly, will check what the
    actual problem later.

--- src/http_forward.h
+++ src/http_forward.h
@@ -28,8 +28,7 @@
 
 namespace ouinet {
 
-// 2048 was too small to read twitter.com's response head
-static const size_t http_forward_block = 16384;
+static const size_t http_forward_block = 2048;
 
 // Get copy of response head from input, return response head for output.
 using ProcHeadFunc = std::function<
