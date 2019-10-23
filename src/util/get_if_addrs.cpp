6f6e5bf7f254ae61fd8bf3c62fa8040296085d3a
Peter Jankuliak <p.jankuliak@gmail.com>
2019-10-23 16:19:58 +0200

    Add util::get_networks(ec)
    To return addresses of network interfaces + their masks.

--- src/util/get_if_addrs.cpp
+++ src/util/get_if_addrs.cpp
@@ -38,3 +38,58 @@ set<asio::ip::address> util::get_if_addrs(sys::error_code& ec)
     return output;
 }
 
+vector<util::Network> util::get_networks(sys::error_code& ec)
+{
+    vector<Network> output;
+
+    struct ifaddrs* ifaddrs;
+
+    if (getifaddrs(&ifaddrs)) {
+        ec = make_error_code(static_cast<boost::system::errc::errc_t>(errno));
+        return output;
+    }
+
+    for (struct ifaddrs* ifaddr = ifaddrs; ifaddr != nullptr; ifaddr = ifaddr->ifa_next) {
+        if (!ifaddr->ifa_addr) {
+            continue;
+        }
+
+        if (ifaddr->ifa_addr->sa_family == AF_INET) {
+            struct sockaddr_in* addr_ = (struct sockaddr_in*)ifaddr->ifa_addr;
+            struct sockaddr_in* mask_ = (struct sockaddr_in*)ifaddr->ifa_netmask;
+
+            asio::ip::address_v4 addr(ntohl(addr_->sin_addr.s_addr));
+            asio::ip::address_v4 mask(ntohl(mask_->sin_addr.s_addr));
+
+            output.emplace_back(asio::ip::network_v4(addr, mask));
+        } else if (ifaddr->ifa_addr->sa_family == AF_INET6) {
+            struct sockaddr_in6* addr_ = (struct sockaddr_in6*)ifaddr->ifa_addr;
+            struct sockaddr_in6* mask_ = (struct sockaddr_in6*)ifaddr->ifa_netmask;
+
+            std::array<unsigned char, 16> addr_bytes;
+
+            memcpy(addr_bytes.data(), addr_->sin6_addr.s6_addr, addr_bytes.size());
+            asio::ip::address_v6 addr(addr_bytes, addr_->sin6_scope_id);
+
+            // https://stackoverflow.com/a/48375002/273348
+            unsigned char *c = mask_->sin6_addr.s6_addr;
+            int i = 0, prefix_len = 0;
+            unsigned char n = 0;
+            while (i < 16) {
+                n = c[i];
+                while (n > 0) {
+                    if (n & 1) ++prefix_len;
+                    n = n >> 1;
+                }
+                ++i;
+            }
+
+            output.emplace_back(asio::ip::network_v6(addr, prefix_len));
+        }
+    }
+
+    freeifaddrs(ifaddrs);
+
+    return output;
+}
+
--- src/util/get_if_addrs.h
+++ src/util/get_if_addrs.h
@@ -2,10 +2,52 @@
 
 #include "../namespaces.h"
 #include <set>
+#include <vector>
 #include <boost/asio/ip/address.hpp>
+#include <boost/asio/ip/network_v4.hpp>
+#include <boost/asio/ip/network_v6.hpp>
+#include <boost/variant.hpp>
 
 namespace ouinet { namespace util {
 
+struct Network : public boost::variant< asio::ip::network_v4
+                                      , asio::ip::network_v6 >
+{
+    using Parent = boost::variant< asio::ip::network_v4
+                                 , asio::ip::network_v6 >;
+
+    // Use Paren's constructors
+    using Parent::Parent;
+
+    bool is_v4() const {
+        return boost::get<asio::ip::network_v4>(this);
+    }
+
+    bool has_address(asio::ip::address addr) const {
+        struct visitor : public boost::static_visitor<bool> {
+            const asio::ip::address& addr;
+
+            visitor(const asio::ip::address& addr) : addr(addr) {}
+
+            bool operator()(const asio::ip::network_v4& net) const {
+                if (!addr.is_v4()) return false;
+                auto range = net.hosts();
+                return range.find(addr.to_v4()) != range.end();
+            }
+
+            bool operator()(const asio::ip::network_v6& net) const {
+                if (!addr.is_v6()) return false;
+                auto range = net.hosts();
+                return range.find(addr.to_v6()) != range.end();
+            }
+        };
+
+        return boost::apply_visitor(visitor(addr), *this);
+    }
+};
+
 std::set<asio::ip::address> get_if_addrs(sys::error_code&);
+
+std::vector<Network> get_networks(sys::error_code&);
 
 }} // namespaces
