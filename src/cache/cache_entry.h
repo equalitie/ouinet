598d91da57248b220804ad6ba122bb498b77bae7
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2019-11-13 15:46:25 -0500

    Remove unused function `ouinet::key_from_http_url`.

--- src/cache/cache_entry.h
+++ src/cache/cache_entry.h
@@ -17,16 +17,6 @@ std::string key_from_http_req(const Request& req) {
     return util::canonical_url(req.target());
 }
 
-// Uses of this function may be an indication that request information is missing,
-// which could cause problems if at a later point we want to
-// take other request parameters to compute cache index keys.
-inline
-std::string key_from_http_url(const std::string& url) {
-    beast::string_view sw(url);
-    assert(sw.starts_with("http"));
-    return util::canonical_url(url);
-}
-
 template <class Key>
 inline
 Key uri_from_key(Key&& key) {
