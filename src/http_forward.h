f4e269f545b77edd56972afeb7e46532c6fbbf2d
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2019-11-21 17:27:21 -0500

    Increment `http_forward_block` from 2048 to 16384 (again).
    So that some sites with big HTTP response heads (like Twitter) work again.
    
    Using forward blocks this big should be working again after the Boost Beast
    patch applied in commit 8b79fc8c.

--- src/http_forward.h
+++ src/http_forward.h
@@ -28,10 +28,8 @@
 
 namespace ouinet {
 
-// This should be raised to 16384 to support reading twitter.com's head,
-// however it triggers a bug in `boost::http::basic_parser::put`
-// (which we will report ASAP).
-static const size_t http_forward_block = 2048;
+// 2048 was too small to read twitter.com's response head
+static const size_t http_forward_block = 16384;
 
 // Get copy of response head from input, return response head for output.
 using ProcHeadFunc = std::function<
