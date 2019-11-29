c54eb29eee6eff76da4cc76e42c6780a3220f489
Ruud Koolen <redlizard@redlizard.nl>
2019-11-29 05:43:08 +0100

    Improved build-android.sh to fit better into the ouifennec build process.

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 set -e
-set -x
 
 DIR=`pwd`
-SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
-ROOT=$(cd ${SCRIPT_DIR}/.. && pwd)
+BUILD_DIR=$(pwd)
+SOURCE_DIR=$(dirname -- "$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")")
+TOOLS_DIR=${TOOLS_DIR:-${BUILD_DIR}/tools}
 ABI=${ABI:-armeabi-v7a}
+NDK=android-ndk-r19b
+NDK_ZIP=${NDK}-linux-x86_64.zip
 
-RELEASE_BUILD=0
+VARIANT=debug
 while getopts r option; do
     case "$option" in
-        r) RELEASE_BUILD=1;;
+        r) VARIANT=release ;;
     esac
 done
 shift $((OPTIND -1))
@@ -22,88 +24,28 @@ shift $((OPTIND -1))
 if [ "$ABI" = "armeabi-v7a" ]; then
     NDK_ARCH="arm"
     NDK_PLATFORM=19
-
 elif [ "$ABI" = "arm64-v8a" ]; then
     NDK_ARCH="arm64"
     NDK_PLATFORM=21
-
 elif [ "$ABI" = "x86" ]; then
     NDK_ARCH="x86"
     NDK_PLATFORM=19
-
 elif [ "$ABI" = "x86_64" ]; then
     NDK_ARCH="x86_64"
     NDK_PLATFORM=21
-
 else
     >&2 echo "Unsupported ABI: '$ABI', valid values are armeabi-v7a, arm64-v8a, x86, x86_64."
     exit 1
 fi
 
-# Destination directory for Ouinet build outputs
-OUTPUT_DIR=build-android-${ABI}
-if [ $RELEASE_BUILD -eq 1 ]; then
-    OUTPUT_DIR=${OUTPUT_DIR}-release
-fi
-mkdir -p "${DIR}/${OUTPUT_DIR}"
-
-SDK_DIR=${SDK_DIR:-"$DIR/sdk"}
-
-NDK=android-ndk-r19b
-NDK_DIR=${NDK_DIR:-"$DIR/$NDK"}
-NDK_ZIP=${NDK}-linux-x86_64.zip
-
 # Android API level, see https://redmine.equalit.ie/issues/12143
 PLATFORM=android-${NDK_PLATFORM}
 
 EMULATOR_AVD=${EMULATOR_AVD:-ouinet-test}
-
-# The following options may be limited by availability of SDK packages,
-# to get list of all packages, use `sdkmanager --list`.
-
-# The image to be used by the emulator AVD
 EMULATOR_IMAGE_TAG=google_apis  # uses to be available for all platforms and ABIs
-EMULATOR_IMAGE="system-images;$PLATFORM;$EMULATOR_IMAGE_TAG;$ABI"
-
-# To get list of all devices, use `avdmanager list device`.
-EMULATOR_DEV=${EMULATOR_DEV:-Nexus 6}
+EMULATOR_IMAGE="system-images;$PLATFORM;$EMULATOR_IMAGE_TAG;$ABI" # The image to be used by the emulator AVD
+EMULATOR_DEV=${EMULATOR_DEV:-Nexus 6} # To get list of all devices, use `avdmanager list device`.
 EMULATOR_SKIN=1440x2560  # automatically scaled down on smaller screens
-
-echo "NDK_DIR: "$NDK_DIR
-echo "SDK_DIR: "$SDK_DIR
-echo "PLATFORM: "$PLATFORM
-
-######################################################################
-# This variable shall contain paths to generated libraries which
-# must all be included in the final Android package.
-OUT_LIBS=()
-
-function add_library {
-    local libs=("$@") lib
-    for lib in "${libs[@]}"; do
-        if [ ! -f "$lib" ]; then
-            echo "Cannot add library \"$lib\": File doesn't exist"
-            exit 1
-        fi
-        OUT_LIBS+=("$lib")
-    done
-}
-
-######################################################################
-# This variable shall contain paths to generated binaries which
-# must all be included in the final Android package.
-OUT_BINARIES=()
-
-function add_binary {
-    local binaries=("$@") binaries
-    for binary in "${binaries[@]}"; do
-        if [ ! -f "$binary" ]; then
-            echo "Cannot add binary \"$binary\": File doesn't exist"
-            exit 1
-        fi
-        OUT_BINARIES+=("$binary")
-    done
-}
 
 ######################################################################
 MODES=
@@ -119,24 +61,24 @@ function check_mode {
 
 ######################################################################
 function setup_deps {
+    local SDK_DIR="${TOOLS_DIR}/sdk"
+
     # Install SDK dependencies.
     local toolsfile=sdk-tools-linux-4333796.zip
     local sdkmanager="$SDK_DIR/tools/bin/sdkmanager"
 
-    # Reuse downloaded SDK stuff from old versions of this script.
-    if [ -d "$DIR/sdk_root" -a ! -d "$SDK_DIR" ]; then
-        mv "$DIR/sdk_root" "$SDK_DIR"
-    fi
-
     if [ ! -f "$sdkmanager" ]; then
         echo "cannot find sdk manager: $sdkmangaer"
-        echo "downlodaing sdk.."
+        echo "downloading sdk.."
+        mkdir -p "${TOOLS_DIR}"
+        pushd "${TOOLS_DIR}" >/dev/null
         [ -d "$SDK_DIR/tools" ] || rm -rf "$SDK_DIR/tools"
         if [ ! -f "$toolsfile" ]; then
             # https://developer.android.com/studio/index.html#command-tools
             wget -nv "https://dl.google.com/android/repository/$toolsfile"
         fi
         unzip -q "$toolsfile" -d "$SDK_DIR"
+        popd >/dev/null
     fi
 
     # SDK packages needed by the different modes.
@@ -178,29 +120,34 @@ emulator
     fi
 
     # Prefer locally installed platform tools to those in the system.
-    export PATH="$SDK_DIR/platform-tools:$PATH"
-
+    export PATH="$SDK_DIR/platform-tools:$SDK_DIR/tools/bin:$SDK_DIR/tools:$PATH"
     export ANDROID_HOME=$(dirname $(dirname $(which adb)))
 }
 
 ######################################################################
 # Create Android virtual device for the emulator.
 function maybe_create_avd {
-    if ! "$SDK_DIR/tools/emulator" -list-avds | grep -q "^$EMULATOR_AVD$"; then
-        echo no | "$SDK_DIR/tools/bin/avdmanager" create avd -n "$EMULATOR_AVD" \
-                                                  -k "$EMULATOR_IMAGE" -g "$EMULATOR_IMAGE_TAG" \
-                                                  -b "$ABI" -d "$EMULATOR_DEV"
+    if ! emulator -list-avds | grep -q "^$EMULATOR_AVD$"; then
+        echo no | avdmanager create avd \
+            -n "$EMULATOR_AVD" \
+            -k "$EMULATOR_IMAGE" \
+            -g "$EMULATOR_IMAGE_TAG" \
+            -b "$ABI" \
+            -d "$EMULATOR_DEV"
     fi
 }
 
 ######################################################################
 function maybe_install_ndk {
-    if [ ! -d "$NDK_DIR" ]; then
+    if [ ! -d "${TOOLS_DIR}/${NDK}" ]; then
         echo "Installing NDK..."
+        mkdir -p "${TOOLS_DIR}"
+        pushd "${TOOLS_DIR}" >/dev/null
         if [ ! -f ${NDK_ZIP} ]; then
             wget -nv https://dl.google.com/android/repository/${NDK_ZIP}
         fi
         unzip -q ${NDK_ZIP}
+        popd >/dev/null
     fi
 }
 
@@ -234,36 +181,44 @@ function maybe_install_gradle {
     if [ $NEED_GRADLE == true ]; then
         local GRADLE=gradle-5.5.1
         local GRADLE_ZIP=$GRADLE-bin.zip
-        if [ ! -d "$GRADLE" ]; then
+        if [ ! -d "${TOOLS_DIR}/${GRADLE}" ]; then
+            mkdir -p "${TOOLS_DIR}"
+            pushd "${TOOLS_DIR}" >/dev/null
             if [ ! -f $GRADLE_ZIP ]; then
                 echo "downloading gradle..."
                 wget -nv https://services.gradle.org/distributions/$GRADLE_ZIP
             fi
             #TODO: Check SHA256
             unzip -q $GRADLE_ZIP
+            popd >/dev/null
         fi
-        export PATH="`pwd`/$GRADLE/bin:$PATH"
+        export PATH="${TOOLS_DIR}/${GRADLE}/bin:$PATH"
     fi
 }
 
 ######################################################################
 # Build the Ouinet AAR
 function build_ouinet_aar {
-    GRADLE_BUILDDIR="${DIR}/${OUTPUT_DIR}/ouinet"
-    OUINET_VERSION_NAME=$(cat "${ROOT}"/version.txt)
-    OUINET_BUILD_ID=$(cd "${ROOT}" && "${ROOT}"/scripts/git-version-string.sh)
+    OUTPUT_DIR="${BUILD_DIR}/build-android-${ABI}-${VARIANT}"
+    GRADLE_BUILDDIR="${OUTPUT_DIR}/ouinet"
+    OUINET_VERSION_NAME=$(cat "${SOURCE_DIR}"/version.txt)
+    OUINET_BUILD_ID=$(cd "${SOURCE_DIR}" && "${SOURCE_DIR}"/scripts/git-version-string.sh)
+    BUILD_COMMAND=assemble${VARIANT^}
+
+    mkdir -p "${OUTPUT_DIR}"
     mkdir -p "${GRADLE_BUILDDIR}"
-    ( cd "${GRADLE_BUILDDIR}";
-      gradle build \
+    pushd "${GRADLE_BUILDDIR}" >/dev/null
+    gradle ${BUILD_COMMAND} \
         -Pandroid_abi=${ABI} \
         -PversionName="${OUINET_VERSION_NAME}" \
         -PbuildId="${OUINET_BUILD_ID}" \
         -PbuildDir="${GRADLE_BUILDDIR}" \
-        --project-dir="${ROOT}"/android \
+        --project-dir="${SOURCE_DIR}"/android \
         --gradle-user-home "${GRADLE_BUILDDIR}"/.gradle-home \
-        --project-cache-dir "${GRADLE_BUILDDIR}"/.gradle-cache \
-        --no-daemon
-    )
+        --project-cache-dir "${GRADLE_BUILDDIR}"/.gradle-cache
+    popd >/dev/null
+
+    APK="${OUTPUT_DIR}/ouinet/outputs/aar/ouinet-${variant}.aar"
 }
 
 ######################################################################
@@ -273,8 +228,8 @@ function build_ouinet_aar {
 # `X_GLXCreateNewContext`.
 function run_emulator {
     echo "Starting Android emulator, first boot may take more than 10 minutes..."
-    "$SDK_DIR/tools/emulator" -avd "$EMULATOR_AVD" -skin "$EMULATOR_SKIN" \
-                              -use-system-libs "$@" &
+    emulator -avd "$EMULATOR_AVD" -skin "$EMULATOR_SKIN" -use-system-libs "$@" &
+
     local emupid=$!
     # Inspired in <https://android.stackexchange.com/q/83726>.
     adb -e wait-for-device
