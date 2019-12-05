c69c2dce49f6e420943c64f0be0dfdd8ffa59c08
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2019-12-05 17:23:01 -0500

    Add `ResponseWriter` unit tests corresponding to `ResponseReader`'s.
    For the moment they only check that correct output results from correct
    input (at the moment, incorrect input is also accepted).

--- test/test-response-writer.cpp
+++ test/test-response-writer.cpp
@@ -79,7 +79,7 @@ vector<uint8_t> str_to_vec(boost::string_view s) {
 
 BOOST_AUTO_TEST_SUITE(ouinet_response_writer)
 
-BOOST_AUTO_TEST_CASE(test_simple) {
+BOOST_AUTO_TEST_CASE(test_http10_no_body) {
     asio::io_service ios;
 
     asio::spawn(ios, [&] (auto y_) {
@@ -89,15 +89,44 @@ BOOST_AUTO_TEST_CASE(test_simple) {
         stringstream outs;
         WaitCondition outwc(ios);
         {
-            RW rw(stream(outs, outwc, ios, y));
+            http::response_header<> rh;
+            rh.version(10);
+            rh.result(http::status::ok);
 
-            const string rb("an example body");
+            RW rw(stream(outs, outwc, ios, y));
+            HR::Part part;
+
+            part = HR::Head(move(rh));
+            rw.async_write_part(part, c, y);
+        }
+        outwc.wait(y);
+
+        const string rsp =
+            "HTTP/1.0 200 OK\r\n"
+            "\r\n";
+        BOOST_REQUIRE_EQUAL(outs.str(), rsp);
+    });
+
+    ios.run();
+}
+
+BOOST_AUTO_TEST_CASE(test_http10_body_no_length) {
+    asio::io_service ios;
+
+    asio::spawn(ios, [&] (auto y_) {
+        Cancel c;
+        Yield y(ios, y_);
+
+        stringstream outs;
+        WaitCondition outwc(ios);
+        {
+            const string rb("abcdef");
 
             http::response_header<> rh;
-            rh.version(11);
+            rh.version(10);
             rh.result(http::status::ok);
-            rh.set(http::field::content_length, rb.size());
 
+            RW rw(stream(outs, outwc, ios, y));
             HR::Part part;
 
             part = HR::Head(move(rh));
@@ -108,12 +137,306 @@ BOOST_AUTO_TEST_CASE(test_simple) {
         }
         outwc.wait(y);
 
-        const string rs =
-            "HTTP/1.1 200 OK\r\n"
-            "Content-Length: 15\r\n"
+        const string rsp =
+            "HTTP/1.0 200 OK\r\n"
             "\r\n"
-            "an example body";
-        BOOST_REQUIRE_EQUAL(outs.str(), rs);
+            "abcdef";
+        BOOST_REQUIRE_EQUAL(outs.str(), rsp);
+    });
+
+    ios.run();
+}
+
+BOOST_AUTO_TEST_CASE(test_http11_body) {
+    asio::io_service ios;
+
+    asio::spawn(ios, [&] (auto y_) {
+        Cancel c;
+        Yield y(ios, y_);
+
+        stringstream outs;
+        WaitCondition outwc(ios);
+        {
+            const string rb("0123456789");
+
+            http::response_header<> rh;
+            rh.version(11);
+            rh.result(http::status::ok);
+            rh.set(http::field::date, "Mon, 27 Jul 2019 12:30:20 GMT");
+            rh.set(http::field::content_type, "text/html");
+            rh.set(http::field::content_length, rb.size());
+
+            RW rw(stream(outs, outwc, ios, y));
+            HR::Part part;
+
+            part = HR::Head(move(rh));
+            rw.async_write_part(part, c, y);
+
+            part = HR::Body(true, str_to_vec(rb));
+            rw.async_write_part(part, c, y);
+        }
+        outwc.wait(y);
+
+        const string rsp =
+            "HTTP/1.1 200 OK\r\n"
+            "Date: Mon, 27 Jul 2019 12:30:20 GMT\r\n"
+            "Content-Type: text/html\r\n"
+            "Content-Length: 10\r\n"
+            "\r\n"
+            "0123456789";
+        BOOST_REQUIRE_EQUAL(outs.str(), rsp);
+    });
+
+    ios.run();
+}
+
+BOOST_AUTO_TEST_CASE(test_http11_chunk) {
+    asio::io_service ios;
+
+    asio::spawn(ios, [&] (auto y_) {
+        Cancel c;
+        Yield y(ios, y_);
+
+        stringstream outs;
+        WaitCondition outwc(ios);
+        {
+            http::response_header<> rh;
+            rh.version(11);
+            rh.result(http::status::ok);
+            rh.set(http::field::date, "Mon, 27 Jul 2019 12:30:20 GMT");
+            rh.set(http::field::content_type, "text/html");
+            rh.set(http::field::transfer_encoding, "chunked");
+
+            RW rw(stream(outs, outwc, ios, y));
+            HR::Part part;
+
+            part = HR::Head(move(rh));
+            rw.async_write_part(part, c, y);
+
+            part = HR::ChunkHdr(4, "");
+            rw.async_write_part(part, c, y);
+
+            part = HR::ChunkBody(str_to_vec("1234"), 0);
+            rw.async_write_part(part, c, y);
+
+            part = HR::ChunkHdr(0, "");
+            rw.async_write_part(part, c, y);
+
+            part = HR::Trailer();
+            rw.async_write_part(part, c, y);
+        }
+        outwc.wait(y);
+
+        const string rsp =
+            "HTTP/1.1 200 OK\r\n"
+            "Date: Mon, 27 Jul 2019 12:30:20 GMT\r\n"
+            "Content-Type: text/html\r\n"
+            "Transfer-Encoding: chunked\r\n"
+            "\r\n"
+            "4\r\n"
+            "1234\r\n"
+            "0\r\n"
+            "\r\n";
+        BOOST_REQUIRE_EQUAL(outs.str(), rsp);
+    });
+
+    ios.run();
+}
+
+BOOST_AUTO_TEST_CASE(test_http11_trailer) {
+    asio::io_service ios;
+
+    asio::spawn(ios, [&] (auto y_) {
+        Cancel c;
+        Yield y(ios, y_);
+
+        stringstream outs;
+        WaitCondition outwc(ios);
+        {
+            http::response_header<> rh;
+            rh.version(11);
+            rh.result(http::status::ok);
+            rh.set(http::field::date, "Mon, 27 Jul 2019 12:30:20 GMT");
+            rh.set(http::field::content_type, "text/html");
+            rh.set(http::field::transfer_encoding, "chunked");
+            rh.set(http::field::trailer, "Hash");
+
+            RW rw(stream(outs, outwc, ios, y));
+            HR::Part part;
+
+            part = HR::Head(move(rh));
+            rw.async_write_part(part, c, y);
+
+            part = HR::ChunkHdr(4, "");
+            rw.async_write_part(part, c, y);
+
+            part = HR::ChunkBody(str_to_vec("1234"), 0);
+            rw.async_write_part(part, c, y);
+
+            part = HR::ChunkHdr(0, "");
+            rw.async_write_part(part, c, y);
+
+            http::fields trailer;
+            trailer.set("Hash", "hash_of_1234");
+
+            part = HR::Trailer(move(trailer));
+            rw.async_write_part(part, c, y);
+        }
+        outwc.wait(y);
+
+        const string rsp =
+            "HTTP/1.1 200 OK\r\n"
+            "Date: Mon, 27 Jul 2019 12:30:20 GMT\r\n"
+            "Content-Type: text/html\r\n"
+            "Transfer-Encoding: chunked\r\n"
+            "Trailer: Hash\r\n"
+            "\r\n"
+            "4\r\n"
+            "1234\r\n"
+            "0\r\n"
+            "Hash: hash_of_1234\r\n"
+            "\r\n";
+        BOOST_REQUIRE_EQUAL(outs.str(), rsp);
+    });
+
+    ios.run();
+}
+
+BOOST_AUTO_TEST_CASE(test_http11_restart_body_body) {
+    asio::io_service ios;
+
+    asio::spawn(ios, [&] (auto y_) {
+        Cancel c;
+        Yield y(ios, y_);
+
+        stringstream outs;
+        WaitCondition outwc(ios);
+        {
+            const string rb1("0123456789");
+
+            http::response_header<> rh1;
+            rh1.version(11);
+            rh1.result(http::status::ok);
+            rh1.set(http::field::date, "Mon, 27 Jul 2019 12:30:20 GMT");
+            rh1.set(http::field::content_type, "text/html");
+            rh1.set(http::field::content_length, rb1.size());
+
+            const string rb2("abcde");
+
+            http::response_header<> rh2;
+            rh2.version(11);
+            rh2.result(http::status::ok);
+            rh2.set(http::field::date, "Mon, 27 Jul 2019 12:30:21 GMT");
+            rh2.set(http::field::content_type, "text/html");
+            rh2.set(http::field::content_length, rb2.size());
+
+            RW rw(stream(outs, outwc, ios, y));
+            HR::Part part;
+
+            part = HR::Head(move(rh1));
+            rw.async_write_part(part, c, y);
+
+            part = HR::Body(true, str_to_vec(rb1));
+            rw.async_write_part(part, c, y);
+
+            part = HR::Head(move(rh2));
+            rw.async_write_part(part, c, y);
+
+            part = HR::Body(true, str_to_vec(rb2));
+            rw.async_write_part(part, c, y);
+        }
+        outwc.wait(y);
+
+        const string rsp =
+            "HTTP/1.1 200 OK\r\n"
+            "Date: Mon, 27 Jul 2019 12:30:20 GMT\r\n"
+            "Content-Type: text/html\r\n"
+            "Content-Length: 10\r\n"
+            "\r\n"
+            "0123456789"
+
+            "HTTP/1.1 200 OK\r\n"
+            "Date: Mon, 27 Jul 2019 12:30:21 GMT\r\n"
+            "Content-Type: text/html\r\n"
+            "Content-Length: 5\r\n"
+            "\r\n"
+            "abcde";
+        BOOST_REQUIRE_EQUAL(outs.str(), rsp);
+    });
+
+    ios.run();
+}
+
+BOOST_AUTO_TEST_CASE(test_http11_restart_chunks_body) {
+    asio::io_service ios;
+
+    asio::spawn(ios, [&] (auto y_) {
+        Cancel c;
+        Yield y(ios, y_);
+
+        stringstream outs;
+        WaitCondition outwc(ios);
+        {
+            http::response_header<> rh1;
+            rh1.version(11);
+            rh1.result(http::status::ok);
+            rh1.set(http::field::date, "Mon, 27 Jul 2019 12:30:20 GMT");
+            rh1.set(http::field::content_type, "text/html");
+            rh1.set(http::field::transfer_encoding, "chunked");
+
+            const string rb2("abcde");
+
+            http::response_header<> rh2;
+            rh2.version(11);
+            rh2.result(http::status::ok);
+            rh2.set(http::field::date, "Mon, 27 Jul 2019 12:30:21 GMT");
+            rh2.set(http::field::content_type, "text/html");
+            rh2.set(http::field::content_length, rb2.size());
+
+            RW rw(stream(outs, outwc, ios, y));
+            HR::Part part;
+
+            part = HR::Head(move(rh1));
+            rw.async_write_part(part, c, y);
+
+            part = HR::ChunkHdr(4, "");
+            rw.async_write_part(part, c, y);
+
+            part = HR::ChunkBody(str_to_vec("1234"), 0);
+            rw.async_write_part(part, c, y);
+
+            part = HR::ChunkHdr(0, "");
+            rw.async_write_part(part, c, y);
+
+            part = HR::Trailer();
+            rw.async_write_part(part, c, y);
+
+            part = HR::Head(move(rh2));
+            rw.async_write_part(part, c, y);
+
+            part = HR::Body(true, str_to_vec(rb2));
+            rw.async_write_part(part, c, y);
+        }
+        outwc.wait(y);
+
+        const string rsp =
+            "HTTP/1.1 200 OK\r\n"
+            "Date: Mon, 27 Jul 2019 12:30:20 GMT\r\n"
+            "Content-Type: text/html\r\n"
+            "Transfer-Encoding: chunked\r\n"
+            "\r\n"
+            "4\r\n"
+            "1234\r\n"
+            "0\r\n"
+            "\r\n"
+
+            "HTTP/1.1 200 OK\r\n"
+            "Date: Mon, 27 Jul 2019 12:30:21 GMT\r\n"
+            "Content-Type: text/html\r\n"
+            "Content-Length: 5\r\n"
+            "\r\n"
+            "abcde";
+        BOOST_REQUIRE_EQUAL(outs.str(), rsp);
     });
 
     ios.run();
