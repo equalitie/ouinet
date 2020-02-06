fd848109e9c5e6b0323b244c6bd46ea01d890fed
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2020-02-06 13:50:13 +0100

    Make `atomic_file` and `temp_file` creation interfaces consistent.

--- src/util/atomic_file.h
+++ src/util/atomic_file.h
@@ -24,9 +24,11 @@ public:
                                      , sys::error_code&);
 
     static
-    boost::optional<atomic_file> make( const asio::executor&
-                                     , fs::path
-                                     , sys::error_code&);
+    boost::optional<atomic_file> make( const asio::executor& ex
+                                     , fs::path path
+                                     , sys::error_code& ec) {
+        return make(ex, std::move(path), default_temp_model, ec);
+    }
 
 public:
     using lowest_layer_type = temp_file::lowest_layer_type;
