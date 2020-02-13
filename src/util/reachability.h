1f89e662172a072fdfe4d53502bb322b6b027e00
Peter Jankuliak <p.jankuliak@gmail.com>
2020-02-13 12:19:33 +0100

    Change reachability analysis timeouts

--- src/util/reachability.h
+++ src/util/reachability.h
@@ -37,7 +37,7 @@ public:
      * any firewall connection tracking entries have expired. Incoming data
      * after this period is considered unsolicited.
      */
-    static constexpr long connectionTrackingExpiracyTime = 60 * 60;
+    static constexpr long connectionTrackingExpiracyTime = 10 * 60;
 
     /*
      * Period after startup during which connection tracking entries from
@@ -47,7 +47,7 @@ public:
      * This period may be shorter than connectionTrackingExpiracyTime. Traffic
      * in this gap will set the judgement to UnconfirmedReachable.
      */
-    static constexpr long lingeringConnectionTrackingTime = 10 * 60;
+    static constexpr long lingeringConnectionTrackingTime = 5 * 60;
 
     /*
      * If no unsolicited traffic arrives for this long while reachability
