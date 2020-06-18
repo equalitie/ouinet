d080994796d1067ba13c39b87a6db1383c4d5ffc
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2020-06-18 13:53:44 -0400

    Build actual DoH request (except for a valid DNS query).

--- src/doh.cpp
+++ src/doh.cpp
@@ -1,4 +1,7 @@
 #include "doh.h"
+
+#include <boost/utility/string_view.hpp>
+
 #include "util.h"
 
 namespace ouinet { namespace doh {
@@ -13,12 +16,38 @@ endpoint_from_base(const std::string& base)
     return um.reassemble();
 }
 
+static
+std::string
+dns_query(const std::string& name)
+{
+    // TODO: implement
+    return "TEST";
+}
+
 Request
 build_request( const std::string& name
              , const Endpoint& ep)
 {
-    // TODO: implement
-    return Request{};
+    // DoH uses unpadded base64url as defined in RFC4648#5 (RFC8484#6).
+    auto dq_b64 = util::base64_encode(dns_query(name));
+    for (auto& c : dq_b64) {
+        if (c == '+') c = '-';
+        else if (c == '/') c = '_';
+    }
+    auto target_padded = ep + dq_b64;
+    boost::string_view target(target_padded);
+    while (target.ends_with('=')) target.remove_suffix(1);
+
+    // TODO: keep host in endpoint
+    auto ep_hstart = ep.find("://") + 3;
+    auto ep_hend = ep.find('/', ep_hstart);
+    boost::string_view ep_host(ep);
+    ep_host.remove_suffix(ep.size() - ep_hend);
+    ep_host.remove_prefix(ep_hstart);
+
+    Request rq{http::verb::get, target, 11 /* HTTP/1.1 */};
+    rq.set(http::field::host, ep_host);
+    return rq;
 }
 
 TcpLookup
