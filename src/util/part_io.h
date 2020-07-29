c7e03dcbd0202af338cd55af8bfd7327aeb403c9
Peter Jankuliak <p.jankuliak@gmail.com>
2020-07-29 11:31:54 +0200

    Simplify the std::ostream interface for http parts

--- src/util/part_io.h
+++ src/util/part_io.h
@@ -3,47 +3,37 @@
 #include "../response_part.h"
 #include <ostream>
 
-namespace ouinet { namespace util {
-
-template<class T>
-struct DebugBrief {
-    const T& t;
-};
-
-template<class T>
-inline
-DebugBrief<T> debug_brief(const T& t) { return DebugBrief<T>{t}; }
+namespace ouinet { namespace http_response {
 
 inline
-std::ostream& operator<<(std::ostream& o, const DebugBrief<http_response::Head>& h) {
+std::ostream& operator<<(std::ostream& o, const http_response::Head& h) {
     o << "Head";
     return o;
 }
 
 inline
-std::ostream& operator<<(std::ostream& o, const DebugBrief<http_response::ChunkHdr>& d) {
-    auto& c = d.t;
+std::ostream& operator<<(std::ostream& o, const http_response::ChunkHdr& c) {
     o << "ChunkHdr size:" << c.size << " exts:" << c.exts;
     return o;
 }
 
 inline
-std::ostream& operator<<(std::ostream& o, const DebugBrief<http_response::ChunkBody>& b) {
-    o << "ChunkBody size:" << b.t.size() << " remain:" << b.t.remain;
+std::ostream& operator<<(std::ostream& o, const http_response::ChunkBody& b) {
+    o << "ChunkBody size:" << b.size() << " remain:" << b.remain;
     return o;
 }
 
 inline
-std::ostream& operator<<(std::ostream& o, const DebugBrief<http_response::Body>& b) {
+std::ostream& operator<<(std::ostream& o, const http_response::Body& b) {
     o << "Body";
     return o;
 }
 
 inline
-std::ostream& operator<<(std::ostream& o, const DebugBrief<http_response::Trailer>& b) {
+std::ostream& operator<<(std::ostream& o, const http_response::Trailer& t) {
     o << "Trailer";
     bool first = true;
-    for (auto& f : b.t) {
+    for (auto& f : t) {
         if (!first) o << ";";
         first = false;
         o << " " << f.name_string() << ":" << f.value();
@@ -52,8 +42,8 @@ std::ostream& operator<<(std::ostream& o, const DebugBrief<http_response::Traile
 }
 
 inline
-std::ostream& operator<<(std::ostream& o, const DebugBrief<http_response::Part>& p) {
-    util::apply(p.t, [&](const auto& p_) { o << debug_brief(p_); });
+std::ostream& operator<<(std::ostream& o, const http_response::Part& p) {
+    util::apply(p, [&](const auto& p_) { o << p_; });
     return o;
 }
 
