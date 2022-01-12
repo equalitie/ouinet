0e7a2ca8e77c9d90c3144c6e655f0394bc313086
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2022-01-12 13:46:56 +0100

    Fix overridden error code `operator<<` in Android.
    The signature was slightly different, and in the case of Android (maybe
    because of the particular compiler), it needs to be placed right in the
    `boost::system` namespace.

--- src/util/quote_error_message.h
+++ src/util/quote_error_message.h
@@ -3,15 +3,34 @@
 // Include this to represent error codes as a quoted message string
 // (e.g. for `ouinet::util::str`).
 
-#include <sstream>
+#include <ostream>
 
 #include <boost/system/error_code.hpp>
 
 namespace ouinet { namespace util {
 
 inline
-std::ostream& operator<<(std::ostream& s, const boost::system::error_code& ec) {
-    return s << '"' << ec.message() << '"';
+std::ostream& operator<<(std::ostream& s, boost::system::error_code ec)
+{
+    s << '"' << ec.message() << '"';
+    return s;
 }
 
 }} // namespaces
+
+
+#ifdef __ANDROID__
+// For some reason the operator defined above is ignored when building for Android,
+// so also place it explicitly in the namespace of the one we try to override.
+
+namespace boost { namespace system {
+
+inline
+std::ostream& operator<<(std::ostream& s, error_code ec)
+{
+    s << '"' << ec.message() << '"';
+    return s;
+}
+
+}} // namespaces
+#endif // ifdef __ANDROID__
