109d01b43ba1ddcbf3d1c503b8dfe9090d6597a4
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2022-01-19 16:49:17 +0100

    Log HTTP statuses as quoted status code + string for readability.

--- src/util/str.h
+++ src/util/str.h
@@ -2,23 +2,31 @@
 
 #include <sstream>
 
+#include <boost/beast/http/status.hpp>
 #include <boost/system/error_code.hpp>
 
 namespace ouinet { namespace util {
 
+// Unfortunately, defining these for particular types
+// in separate headers (to keep this one clean)
+// does not seem to work reliably.
+
 inline
 void arg_to_stream(std::ostream& s, boost::system::error_code ec) {
-    // Unfortunately, defining this for this particular type
-    // in a separate header (to keep this one clean)
-    // does not seem to work reliably.
     s << '"' << ec.message() << '"';
 }
 
+inline
+void arg_to_stream(std::ostream& s, boost::beast::http::status st) {
+    s << '"' << static_cast<unsigned>(st) << ' ' << st << '"';
+}
+
+
+// This allows overriding the format of a given type
+// without having to override `operator<<`.
 template<class Arg>
 inline
 void arg_to_stream(std::ostream& s, Arg&& arg) {
-    // This allows overriding the format of a given type
-    // without having to override `operator<<`.
     s << arg;
 }
 
@@ -31,6 +39,7 @@ void args_to_stream(std::ostream& s, Arg&& arg, Args&&... args) {
     arg_to_stream(s, std::forward<Arg>(arg));
     args_to_stream(s, std::forward<Args>(args)...);
 }
+
 
 template<class... Args>
 inline
