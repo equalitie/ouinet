equalitie/ouinet

commit f3fb08f6a49fe632b0ed8cc53d16c3725d69d791

Move some variables into loop.
For consistency, readability and to avoid cross-iteration value pollution.

src/client.cpp
@@ -578,16 +578,14 @@ Client::State::serve_utp_request(GenericStream con, Yield yield)
     Cancel cancel = _shutdown_signal;
     auto cancel_slot = cancel.connect([&] { con.close(); });
 
-    sys::error_code ec;
-
-    http::request<http::empty_body> req;
-    beast::flat_buffer buffer;
-
     // We expect the first request right a way. Consecutive requests may arrive with
     // various delays.
     bool is_first_request = true;
 
     while (true) {
+        sys::error_code ec;
+
+        http::request<http::empty_body> req;
         {
             auto rq_read_timeout = default_timeout::http_recv_simple();
             if (is_first_request) {
@@ -598,6 +596,7 @@ Client::State::serve_utp_request(GenericStream con, Yield yield)
             auto wd = watch_dog(_ctx, rq_read_timeout, [&] { con.close(); });
 
             yield[ec].tag("read_req").run([&] (auto y) {
+                beast::flat_buffer buffer;
                 http::async_read(con, buffer, req, y);
             });
 

commit 43bf35be320c8c17a6519e400845eb2e805358b3

Reuse connection read buffer for whole client-to-client connection.
This fixes a regression introduced by commit f3fb08f6, as data read but not
used to build the request should stay in the buffer for the next read.

As a result of the issue, subsequent requests from other clients may be
broken.

src/client.cpp
@@ -581,6 +581,7 @@ Client::State::serve_utp_request(GenericStream con, Yield yield)
     // We expect the first request right a way. Consecutive requests may arrive with
     // various delays.
     bool is_first_request = true;
+    beast::flat_buffer con_rbuf;  // accumulate reads across iterations here
 
     while (true) {
         sys::error_code ec;
@@ -596,8 +597,7 @@ Client::State::serve_utp_request(GenericStream con, Yield yield)
             auto wd = watch_dog(_ctx, rq_read_timeout, [&] { con.close(); });
 
             yield[ec].tag("read_req").run([&] (auto y) {
-                beast::flat_buffer buffer;
-                http::async_read(con, buffer, req, y);
+                http::async_read(con, con_rbuf, req, y);
             });
 
             if (!wd.is_running()) {