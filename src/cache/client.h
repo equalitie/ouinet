237e3234cab299757cca73dfdc17b4ba054b3180
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2022-03-24 10:45:30 +0100

    Fix bogus include paths in cache client header.

--- src/cache/client.h
+++ src/cache/client.h
@@ -7,10 +7,10 @@
 #include <boost/filesystem.hpp>
 #include <boost/optional.hpp>
 
-#include "../../bittorrent/dht.h"
-#include "../../response_reader.h"
-#include "../../util/crypto.h"
-#include "../../util/yield.h"
+#include "../bittorrent/dht.h"
+#include "../response_reader.h"
+#include "../util/crypto.h"
+#include "../util/yield.h"
 #include "cache_entry.h"
 #include "dht_groups.h"
 
