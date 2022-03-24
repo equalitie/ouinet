3d6471960a5665b24554a73638abf99606d720eb
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2022-03-24 14:42:41 +0100

    Be specific about DHT/uTP initialization failing for cache setup.

--- src/client.cpp
+++ src/client.cpp
@@ -2448,10 +2448,10 @@ void Client::State::setup_cache(asio::yield_context yield)
     fail_on_error("Failed to initialize cache::Client");
 
     idempotent_start_accepting_on_utp(yield[ec]);
-    fail_on_error("Failed to start accepting on uTP");
+    fail_on_error("Failed to start accepting on uTP for cache::Client");
 
     auto dht = bittorrent_dht(yield[ec]);
-    fail_on_error("Failed to initialize BT DHT");
+    fail_on_error("Failed to initialize BT DHT for cache::Client");
 
     if (!_cache->enable_dht(dht)) ec = asio::error::invalid_argument;
     fail_on_error("Failed to enable BT DHT in cache::Client");
