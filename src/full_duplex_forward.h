b7bdd08bb4e67d4d4f2e7f8828009c36db1e84bc
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2022-03-29 10:51:48 +0200

    Have full-duplex op return counts of successfully forwarded bytes.

--- src/full_duplex_forward.h
+++ src/full_duplex_forward.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <utility>
+
 #include <boost/asio/read.hpp>
 #include "default_timeout.h"
 #include "generic_stream.h"
@@ -11,14 +13,18 @@ namespace ouinet {
 
 // This assumes that there is no data already read from either connection,
 // but pending send.  If there is, please send it beforehand.
+//
+// A pair of counts is returned
+// for bytes successfully forwarded (from c1 to c2, from c2 to c1).
 template<class Stream1, class Stream2>
-inline
-void full_duplex(Stream1 c1, Stream2 c2, Cancel cancel, asio::yield_context yield)
+std::pair<std::size_t, std::size_t>
+full_duplex(Stream1 c1, Stream2 c2, Cancel cancel, asio::yield_context yield)
 {
     static const auto timeout = default_timeout::activity();
 
     static const auto half_duplex = []( auto& in
                                       , auto& out
+                                      , auto& fwd_bytes_in_out
                                       , auto& wdog
                                       , asio::yield_context& yield)
     {
@@ -32,6 +38,7 @@ void full_duplex(Stream1 c1, Stream2 c2, Cancel cancel, asio::yield_context yiel
             asio::async_write(out, asio::buffer(data, length), yield[ec]);
             if (ec) break;
 
+            fwd_bytes_in_out += length;  // the data was successfully forwarded
             wdog.expires_after(timeout);
         }
         // On error, force the other half-duplex task to finish by closing both streams.
@@ -54,20 +61,23 @@ void full_duplex(Stream1 c1, Stream2 c2, Cancel cancel, asio::yield_context yiel
                          , [&] { c1.close(); c2.close(); });
 
     WaitCondition wait_condition(c1.get_executor());
+    std::size_t fwd_bytes_c1_c2 = 0, fwd_bytes_c2_c1 = 0;
 
     asio::spawn
         ( yield
         , [&, lock = wait_condition.lock()](asio::yield_context yield) {
-              half_duplex(c1, c2, wdog, yield);
+              half_duplex(c1, c2, fwd_bytes_c1_c2, wdog, yield);
           });
 
     asio::spawn
         ( yield
         , [&, lock = wait_condition.lock()](asio::yield_context yield) {
-              half_duplex(c2, c1, wdog, yield);
+              half_duplex(c2, c1, fwd_bytes_c2_c1, wdog, yield);
           });
 
     wait_condition.wait(yield);
+
+    return std::make_pair(fwd_bytes_c1_c2, fwd_bytes_c2_c1);
 }
 
 } // ouinet namespace
