d65ccc1484ad4b168dec8d0a2b70abcfaef91559
Miguel <miguel@equalit.ie>
2022-05-31 18:16:24 -0500

    Import Ouinet and initialize its Config instance

--- android/kotlin/app/src/main/java/ie/equalit/ouinet_examples/android_kotlin/MainActivity.kt
+++ android/kotlin/app/src/main/java/ie/equalit/ouinet_examples/android_kotlin/MainActivity.kt
@@ -2,10 +2,15 @@ package ie.equalit.ouinet_examples.android_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ie.equalit.ouinet.Config
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var config = Config.ConfigBuilder(this)
+            .setCacheType("bep5-http")
+            .build()
     }
 }
