8deca9aec87574016d6833546858313987eca1ac
Miguel <miguel@equalit.ie>
2022-06-01 14:47:54 -0500

    Init Android application in Java

---
+++ android/java/app/src/main/java/ie/equalit/ouinet_examples/android_java/MainActivity.java
@@ -0,0 +1,13 @@
+package ie.equalit.ouinet_examples.android_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
