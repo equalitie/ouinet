46ea7e879e7b9214cf8b87a7c09b6259c6220a5f
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2022-06-27 12:36:09 +0200

    CMake: Notes on the use of each dependency.

--- cmake/OuinetDependencies.cmake
+++ cmake/OuinetDependencies.cmake
@@ -1,9 +1,21 @@
+# For Boost ASIO SSL and the I2P ouiservice.
 # Must precede boost for asio_ssl.cpp
 include(${CMAKE_CURRENT_LIST_DIR}/dependencies/openssl.cmake)
 
+# This is used all over Ouinet's source.
 include(${CMAKE_CURRENT_LIST_DIR}/dependencies/boost.cmake)
+
+# For internal DNS-over-HTTPS support.
 include(${CMAKE_CURRENT_LIST_DIR}/dependencies/zdnsparser.cmake)
+
+# For crypto, hash and random utilities.
 include(${CMAKE_CURRENT_LIST_DIR}/dependencies/gcrypt.cmake)
+
+# For Pluggable Transport modules (obfs4 and lampshade).
 include(${CMAKE_CURRENT_LIST_DIR}/dependencies/golang.cmake)
+
+# For client front-end status API.
 include(${CMAKE_CURRENT_LIST_DIR}/dependencies/json.cmake)
+
+# For URI parsing and encoding/decoding.
 include(${CMAKE_CURRENT_LIST_DIR}/dependencies/uri.cmake)
