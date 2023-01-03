5995ff1b131645da9754e5cd7d94e89756043d59
paidforby <20746091+paidforby@users.noreply.github.com>
2023-01-03 14:01:17 -0500

    Revert "Merge pull request #55 from equalitie/fix-stop-pre-android-12"
    This reverts commit ae4348b861f79c20d703b5aa9ee2e2dcad987608, reversing
    changes made to 8f15df5240fcfa8eb6cc4df033ecad1e289b98cd.

--- android/ouinet/src/main/java/ie/equalit/ouinet/Ouinet.java
+++ android/ouinet/src/main/java/ie/equalit/ouinet/Ouinet.java
@@ -167,8 +167,7 @@ public class Ouinet {
 
         nStartClient(args.toArray(new String[0]), path.toArray(new String[0]));
 
-        // Remove receivers, they were unused and causing memory leaks
-        //registerBroadcastReceivers();
+        registerBroadcastReceivers();
     }
 
     // If this succeeds, we should be able to do UDP multicasts
@@ -199,14 +198,9 @@ public class Ouinet {
         if (getState() == RunningState.Stopped) return;
 
         nStopClient();
-
         if (lock != null && lock.isHeld()) {
             lock.release();
         }
-
-        /*
-        // Remove receivers, they were unused and causing memory leaks,
-        // changes in network connectivity or charging status should be handled by the application
         if (wifiChangeReceiver != null) {
             context.unregisterReceiver(wifiChangeReceiver);
             wifiChangeReceiver = null;
@@ -215,7 +209,6 @@ public class Ouinet {
             context.unregisterReceiver(chargingChangeReceiver);
             chargingChangeReceiver = null;
         }
-        */
     }
 
     private void registerBroadcastReceivers() {
