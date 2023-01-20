f052a085d9489aae5e23412e131e8ed8deda9233
Miguel <miguel@equalit.ie>
2023-01-20 17:41:56 -0600

    Add draft for test_cache_announcer

---
+++ test/test_cache_announcer.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE cache_announcer
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(ouinet_cache_announcer)
+
+BOOST_AUTO_TEST_CASE(test_announcer_loop)
+{
+   BOOST_REQUIRE_EQUAL(1, 1);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
