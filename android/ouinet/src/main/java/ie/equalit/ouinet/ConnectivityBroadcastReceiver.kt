cd1fe840d09e11cc0284e16bcc619ad0500ad4a5
paidforby <20746091+paidforby@users.noreply.github.com>
2023-04-04 16:42:45 -0400

    Force restart of ouinet on connectivity change 10s after stop is requested, regardless of whether stop has finished

--- android/ouinet/src/main/java/ie/equalit/ouinet/ConnectivityBroadcastReceiver.kt
+++ android/ouinet/src/main/java/ie/equalit/ouinet/ConnectivityBroadcastReceiver.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.util.Log
+import ie.equalit.ouinet.OuinetNotification.Companion.MILLISECOND
 
 class ConnectivityBroadcastReceiver (
     background: OuinetBackground
@@ -25,16 +26,19 @@ class ConnectivityBroadcastReceiver (
 
     private fun restartOnConnectivityChange (state : NetworkInfo.State) {
         if (state == NetworkInfo.State.CONNECTED || state == NetworkInfo.State.DISCONNECTED) {
-            Log.d(TAG, "Stopping OuinetService on connectivity change")
-            background?.stop {
-                // TODO: Insert a pause / check client state.
-                try {
-                    Log.d(TAG, "Starting OuinetService on connectivity change")
-                    background?.start()
-                } catch (ex: Exception) {
-                    // TODO: if the start fails, we should try restarting the service later
-                    Log.w(TAG, "startOuinetService failed with exception: $ex")
-                }
+            try {
+                Log.d(TAG, "Stopping OuinetService on connectivity change")
+                background?.stop()!!.join(10 * MILLISECOND)
+            } catch (ex: Exception) {
+                Log.w(TAG, "stopOuinetService failed with exception: $ex")
+            }
+            // TODO: Insert a pause / check client state.
+            try {
+                Log.d(TAG, "Starting OuinetService on connectivity change")
+                background?.start()
+            } catch (ex: Exception) {
+                // TODO: if the start fails, we should try restarting the service later
+                Log.w(TAG, "startOuinetService failed with exception: $ex")
             }
         }
     }
