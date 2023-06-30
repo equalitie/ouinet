19828c3a59257d9b88f57f0000cf0fb7482f6252
paidforby <20746091+paidforby@users.noreply.github.com>
2023-06-30 19:13:41 -0400

    Add support for iOS target to openssl.cmake

--- cmake/dependencies/openssl.cmake
+++ cmake/dependencies/openssl.cmake
@@ -62,6 +62,55 @@ if (${CMAKE_SYSTEM_NAME} STREQUAL "Android")
 
     set(OpenSSL_DIR ${CMAKE_CURRENT_LIST_DIR}/inline-openssl)
     list(INSERT CMAKE_MODULE_PATH 0 ${OpenSSL_DIR})
+elseif (${CMAKE_SYSTEM_NAME} STREQUAL "iOS")
+    set(OPENSSL_VERSION "1.1.1q")
+
+    get_filename_component(COMPILER_DIR ${CMAKE_CXX_COMPILER} DIRECTORY)
+
+    # TODO: Do we need to support arm7 iOS targets?
+    #if (${CMAKE_SYSTEM_PROCESSOR} STREQUAL "armv7")
+    #    set(OPENSSL_TARGET "ios-cross")
+    #elseif (${CMAKE_SYSTEM_PROCESSOR} STREQUAL "arm64")
+    set(OPENSSL_TARGET "ios64-cross")
+    #else()
+    #    message(FATAL_ERROR "Unsupported CMAKE_SYSTEM_PROCESSOR ${CMAKE_SYSTEM_PROCESSOR}")
+    #endif()
+
+    set(BUILT_OPENSSL_VERSION ${OPENSSL_VERSION})
+    set(BUILT_OPENSSL_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl/installrosslude)
+    set(BUILT_OPENSSL_SSL_LIBRARY ${CMAKE_CURRENT_BINARY_DIR}/openssl/install/lib/${CMAKE_STATIC_LIBRARY_PREFIX}ssl${CMAKE_STATIC_LIBRARY_SUFFIX})
+    set(BUILT_OPENSSL_CRYPTO_LIBRARY ${CMAKE_CURRENT_BINARY_DIR}/openssl/install/lib/${CMAKE_STATIC_LIBRARY_PREFIX}crypto${CMAKE_STATIC_LIBRARY_SUFFIX})
+
+    externalproject_add(built_openssl
+        URL "https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz"
+        URL_HASH SHA256=d7939ce614029cdff0b6c20f0e2e5703158a489a72b2507b8bd51bf8c8fd10ca
+        PREFIX "${CMAKE_CURRENT_BINARY_DIR}/openssl"
+        CONFIGURE_COMMAND
+               cd ${CMAKE_CURRENT_BINARY_DIR}/openssl/src/built_openssl
+            && export ANDROID_NDK_HOME=${CMAKE_ANDROID_NDK}
+            && export PATH=${COMPILER_DIR}:$ENV{PATH}
+            && ./Configure
+                ${OPENSSL_TARGET}
+                no-shared -no-dso -no-hw -no-engine
+                --prefix=${CMAKE_CURRENT_BINARY_DIR}/openssl/install
+                #-D__ANDROID_API__=${OPENSSL_ANDROID_VERSION}
+        BUILD_COMMAND
+               cd ${CMAKE_CURRENT_BINARY_DIR}/openssl/src/built_openssl
+            #&& export ANDROID_NDK_HOME=${CMAKE_ANDROID_NDK}
+            && export PATH=${COMPILER_DIR}:$ENV{PATH}
+            && make depend
+            && make build_libs
+        BUILD_BYPRODUCTS
+            ${BUILT_OPENSSL_SSL_LIBRARY}
+            ${BUILT_OPENSSL_CRYPTO_LIBRARY}
+        INSTALL_COMMAND
+               cd ${CMAKE_CURRENT_BINARY_DIR}/openssl/src/built_openssl
+            && export PATH=${COMPILER_DIR}:$ENV{PATH}
+            && make install_dev
+    )
+
+    set(OpenSSL_DIR ${CMAKE_CURRENT_LIST_DIR}/inline-openssl)
+    list(INSERT CMAKE_MODULE_PATH 0 ${OpenSSL_DIR})
 endif()
 
 find_package(OpenSSL REQUIRED)
