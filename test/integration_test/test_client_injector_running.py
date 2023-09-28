35f3ca8f393be5efeeabe324c5170a1c12bb5273
Miguel <miguel@equalitie.org>
2023-09-27 23:23:17 -0600

    Fixed OuinetBasicSanityTest::test_run_client

--- test/integration_test/test_client_injector_running.py
+++ test/integration_test/test_client_injector_running.py
@@ -1,5 +1,4 @@
 import unittest
-import pdb
 
 from itestlib import OuinetClient, OuinetInjector
 from test_fixtures import TestFixtures
@@ -7,14 +6,18 @@ from test_fixtures import TestFixtures
 
 class OuinetBasicSanityTest(unittest.TestCase):
     def test_run_client(self):
-        ouinet_client_process = OuinetClient(TestFixtures.FIRST_CLIENT["name"],
-                ( "--listen-on-tcp", "127.0.0.1:"+TestFixtures.FIRST_CLIENT["port"]
-                , "--injector-ipns", TestFixtures.INJECTOR_IPNS_PERSISTANT_IDENTITY["Identity"]["PeerID"]
-                , "--injector-ep", "127.0.0.1:" + str(TestFixtures.INJECTOR_PORT)
-                , "http://localhost/"))
+        ouinet_client_process = OuinetClient(
+            TestFixtures.OUINET_CLIENT["name"],
+            "",
+            "ouinet-client.conf",
+            ("--listen-on-tcp", "127.0.0.1:" + TestFixtures.OUINET_CLIENT["port"],
+             "--injector-ep", "127.0.0.1:" + str(TestFixtures.OUINET_INJECTOR["port"]),
+             "http://localhost/"),
+            30)
 
     def test_run_injector(self):
         pass
+
 
 if __name__ == '__main__':
     from unittest import main
