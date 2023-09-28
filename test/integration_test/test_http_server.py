6a04f278a9dc2f33c7491313cad26b8baeac1443
Miguel <miguel@equalitie.org>
2023-09-28 13:27:00 -0600

    Remove unused imports and fix code formatting

--- test/integration_test/test_http_server.py
+++ test/integration_test/test_http_server.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python
 
-from twisted.internet import reactor, endpoints
+from twisted.internet import reactor
 from twisted.web.server import Site
 from twisted.web.resource import Resource
-import time
 
 from test_fixtures import TestFixtures
+
+
 class TestPage(Resource):
     isLeaf = True
+
     def render_GET(self, request):
         if b"content" in request.args:
             return request.args[b"content"][0]
         else:
             return TestFixtures.TEST_PAGE_BODY
 
-def TestHttpServer(port):
-        return reactor.listenTCP(port, Site(TestPage()))
+
+def test_http_server(port):
+    return reactor.listenTCP(port, Site(TestPage()))
+
 
 if __name__ == "__main__":
-    my_http_server = TestHttpServer(8080)
+    my_http_server = test_http_server(8080)
     reactor.run()
