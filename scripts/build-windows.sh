a95b1a9de64236bf67be0e0e745997e269567e80
Miguel <miguel@equalitie.org>
2024-02-06 18:52:48 -0600

    Add a bash script to build Ouinet for Windows

---
+++ scripts/build-windows.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#
+# Builds ouinet from the local source containing this file
+#
+# If BOOST_ROOT env variable is set it adds -DBOOST_ROOT=$BOOST_ROOT
+# to cmake args
+
+set -e
+
+DIR=$(pwd)
+SOURCEDIR=$(dirname "$(realpath "${BASH_SOURCE}")")/..
+BINDIR="${DIR}"/ouinet-windows-bin
+BUILDDIR="${DIR}"/ouinet-windows-build
+GENERATOR="Unix Makefiles"
+
+if [[ ! -e ${BUILDDIR}/Makefile ]]; then
+	rm -rf "${BUILDDIR}"
+	mkdir "${BUILDDIR}"
+	cd "${BUILDDIR}"
+	cmake \
+    "${SOURCEDIR}" \
+    -G "${GENERATOR}" \
+    --compile-no-warning-as-error \
+    -Wno-error=nonnull \
+    -Wno-error=dev \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_INSTALL_PREFIX="${BINDIR}" \
+    -DBOOST_VERSION="1.77.0"
+fi
+
+# Using a single threaded compilation to simplify the debugging
+cmake \
+  --build "${BUILDDIR}"
+
+#cd "${BUILDDIR}"
+#make -j`nproc`
+
+# Not supported yet
+#make install
+
+cd "${DIR}"
