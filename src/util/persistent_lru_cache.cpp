2cf7367e6f4ce30605c5fc8115cc50b05dbef8f5
Miguel <miguel@equalitie.org>
2024-02-21 17:12:46 -0600

    Patch persistent_lru_cache to allow its build in Windows

--- src/util/persistent_lru_cache.cpp
+++ src/util/persistent_lru_cache.cpp
@@ -33,8 +33,14 @@ fs::path path_from_key(const fs::path& dir, const std::string& key) {
     return dir / util::bytes::to_hex(util::sha1_digest(key));
 }
 
-bool is_cache_entry(const struct dirent* entry) {
-    return (entry->d_type == DT_REG
+bool is_cache_entry(const struct dirent* entry, boost::filesystem::path& dir) {
+#ifdef _WIN32
+    auto entry_path = dir / entry->d_name;
+    bool is_regular = boost::filesystem::is_regular_file(entry_path);
+#else
+    bool is_regular = entry->d_type == DT_REG;
+#endif
+    return (is_regular
             && strstr(entry->d_name, temp_file_prefix) != entry->d_name);
 }
 
