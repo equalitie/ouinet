cd40c44787009959f5b13114ed48b5c28c36bdc4
Miguel <miguel@equalitie.org>
2024-03-15 22:02:30 -0600

    Refactoring random_access_handle_extended to build it with Boost >1.71

--- src/util/file_io/random_access_handle_extended.hpp
+++ src/util/file_io/random_access_handle_extended.hpp
@@ -1,121 +1,65 @@
-#ifndef RANDOM_ACCESS_HANDLE_ADAPTER_HPP
-#define RANDOM_ACCESS_HANDLE_ADAPTER_HPP
-
-#if defined(_MSC_VER) && (_MSC_VER >= 1200)
-#pragma once
-#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
-
 #include <boost/asio.hpp>
-#include "boost/asio/windows/random_access_handle.hpp"
-#include <boost/asio/detail/config.hpp>
-#include "../../../src/util/executor.h"
+#include <boost/system/error_code.hpp>
+#include <boost/asio/windows/random_access_handle.hpp>
+#include <windows.h>
 
-using io_context = boost::asio::io_context;
-using executor = ouinet::util::AsioExecutor;
-using native_handle_t = HANDLE;
+using namespace boost::asio;
+using namespace boost::system;
 using error_code = boost::system::error_code;
-namespace errc = boost::system::errc;
+using native_handle_t = HANDLE;
 
-//namespace boost {
-//namespace asio {
-//namespace windows {
-
-//template <typename Executor = boost::asio::any_io_executor>
-class basic_random_access_handle_extended : public boost::asio::windows::random_access_handle {
+class random_access_handle_extended : public boost::asio::windows::random_access_handle {
 public:
-  basic_random_access_handle_extended(io_context &ctx)
-      : boost::asio::windows::random_access_handle(ctx) {}
+    explicit random_access_handle_extended(const executor_type &ex) : basic_random_access_handle(ex) {}
 
-  basic_random_access_handle_extended(executor exe)
-      : boost::asio::windows::random_access_handle(exe) {}
+    template <typename MutableBufferSequence, typename ReadHandler>
+    void async_read_some(const MutableBufferSequence& buffer, ReadHandler handler) {
+        boost::system::error_code ec;
+        auto offset = current_position(ec);
+        this->async_read_some_at(offset, buffer, handler);
+    }
 
-  template <typename MutableBufferSequence, typename ReadHandler>
-  BOOST_ASIO_INITFN_RESULT_TYPE(ReadHandler,
-                                void (boost::system::error_code, std::size_t))
-  async_read_some(const MutableBufferSequence& buffers,
-                  BOOST_ASIO_MOVE_ARG(ReadHandler) handler)
-  {
-      // If you get an error on the following line it means that your handler does
-      // not meet the documented type requirements for a ReadHandler.
-      BOOST_ASIO_READ_HANDLER_CHECK(ReadHandler, handler) type_check;
+    template <typename ConstBufferSequence, typename WriteHandler>
+    void async_write_some(const ConstBufferSequence& buffer, WriteHandler handler) {
+        boost::system::error_code ec;
+        auto offset = end_position(ec);
+        this->async_write_some_at(offset, buffer, handler);
+    }
 
-      boost::system::error_code ec;
-      auto offset = current_position(ec);
-      boost::asio::async_completion<ReadHandler,
-              void (boost::system::error_code, std::size_t)> init(handler);
+    static
+    error_code last_error()
+    {
+        return make_error_code(static_cast<errc::errc_t>(errno));
+    }
 
-      this->impl_.get_service().async_read_some_at(
-              this->impl_.get_implementation(), offset,
-              buffers, init.completion_handler,
-              this->impl_.get_implementation_executor());
+    size_t
+    current_position(error_code& ec)
+    {
+        native_handle_t native_handle = this->native_handle();
+        auto offset = SetFilePointer(native_handle, 0, NULL, FILE_CURRENT);
+        if(INVALID_SET_FILE_POINTER ==  offset)
+        {
+            ec = last_error();
+            if (!ec) ec = make_error_code(errc::no_message);
+            return size_t(-1);
+        }
 
-      return init.result.get();
-  }
+        return offset;
+    }
 
-  template <typename ConstBufferSequence, typename WriteHandler>
-  BOOST_ASIO_INITFN_RESULT_TYPE(WriteHandler,
-                                void (boost::system::error_code, std::size_t))
-  async_write_some(const ConstBufferSequence& buffers,
-                   BOOST_ASIO_MOVE_ARG(WriteHandler) handler)
-  {
-      // If you get an error on the following line it means that your handler does
-      // not meet the documented type requirements for a WriteHandler.
-      BOOST_ASIO_WRITE_HANDLER_CHECK(WriteHandler, handler) type_check;
+    size_t
+    end_position(error_code& ec)
+    {
+        native_handle_t native_handle = this->native_handle();
+        auto offset = SetFilePointer(native_handle, 0, NULL, FILE_END);
+        if(INVALID_SET_FILE_POINTER ==  offset)
+        {
+            ec = last_error();
+            if (!ec) ec = make_error_code(errc::no_message);
+            return size_t(-1);
+        }
 
-      boost::system::error_code ec;
-      auto offset = end_position(ec);
-      boost::asio::async_completion<WriteHandler,
-              void (boost::system::error_code, std::size_t)> init(handler);
-
-      this->impl_.get_service().async_write_some_at(
-              this->impl_.get_implementation(), offset,
-              buffers, init.completion_handler,
-              this->impl_.get_implementation_executor());
-
-      return init.result.get();
-  }
-
-  static
-  error_code last_error()
-  {
-      return make_error_code(static_cast<errc::errc_t>(errno));
-  }
-
-  size_t
-  current_position(error_code& ec)
-  {
-      native_handle_t native_handle = this->native_handle();
-      auto offset = SetFilePointer(native_handle, 0, NULL, FILE_CURRENT);
-      if(INVALID_SET_FILE_POINTER ==  offset)
-      {
-          ec = last_error();
-          if (!ec) ec = make_error_code(errc::no_message);
-          return size_t(-1);
-      }
-
-      return offset;
-  }
-
-  size_t
-  end_position(error_code& ec)
-  {
-      native_handle_t native_handle = this->native_handle();
-      auto offset = SetFilePointer(native_handle, 0, NULL, FILE_END);
-      if(INVALID_SET_FILE_POINTER ==  offset)
-      {
-          ec = last_error();
-          if (!ec) ec = make_error_code(errc::no_message);
-          return size_t(-1);
-      }
-
-      return offset;
-  }
+        return offset;
+    }
 
 };
-typedef basic_random_access_handle_extended random_access_handle_extended;
-
-//} // namespace windows
-//} // namespace asio
-//} // namespace boost
-
-#endif // RANDOM_ACCESS_HANDLE_ADAPTER_HPP
