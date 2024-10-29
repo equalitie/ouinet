77454574a3736bfcef68fda31911b4aa9d6827eb
paidforby <20746091+paidforby@users.noreply.github.com>
2024-10-29 17:56:34 -0400

    Add changes to cmake needed to build ouinet for macOS (i.e. Darwin)

--- cmake/dependencies/boost.cmake
+++ cmake/dependencies/boost.cmake
@@ -77,6 +77,11 @@ if (${CMAKE_SYSTEM_NAME} STREQUAL "Android")
         toolset=clang-${BOOST_ARCH}
         abi=${BOOST_ABI}
     )
+#elseif (${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
+#    set(BOOST_CXXFLAGS "${CXXFLAGS} -std=c++14")
+#    set(BOOST_ARCH_CONFIGURATION
+#            cxxflags=${BOOST_CXXFLAGS}
+#    )
 else()
     set(BOOST_ENVIRONMENT )
     set(BOOST_ARCH_CONFIGURATION )
