605d5ba11ee91eaf73557b3853c93b57d4e4ee58
Miguel <miguel@equalitie.org>
2024-11-06 20:53:05 -0600

    Set watchdog to 600 milliseconds in bittorrent/collect.h

--- src/bittorrent/collect.h
+++ src/bittorrent/collect.h
@@ -157,7 +157,7 @@ void collect(
                         , local_cancel
                         , yield[ec]);
             } else {
-                WatchDog wd(exec, std::chrono::milliseconds(200), [&] () mutable {
+                WatchDog wd(exec, std::chrono::milliseconds(600), [&] () mutable {
                         if (dbg) cerr << dbg << "dismiss " << candidate << "\n";
                         on_finish();
                     });
