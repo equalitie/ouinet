ff1468cc7fb88ff24686b3e5fac7c9e9fee4a92b
paidforby <20746091+paidforby@users.noreply.github.com>
2024-11-08 11:25:36 -0500

    Add iOS support to cmake build
    Applies changed for CMakeLists.txt.
    Adds ouinet-ios-config.cmake, objective-C wrapper for iOS framework

---
+++ ios/ouinet/src/native-lib.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <vector>
+
+#if defined(__clang__)
+#define SHARED_EXPORT __attribute__((visibility("default")))
+#define SHARED_LOCAL __attribute__((visibility("hidden")))
+#endif
+
+#if defined(IS_BUILDING_SHARED)
+#define API SHARED_EXPORT
+#else
+#define API
+#endif
+
+class API NativeLib
+{
+public:
+  int getClientState();
+  std::string helloOuinet();
+  void startClient(const std::vector<std::string>& args);
+};
