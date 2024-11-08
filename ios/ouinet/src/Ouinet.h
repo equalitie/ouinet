f8b8df0e720aa4e3a8e3278b65f4e3a006a07adc
paidforby <20746091+paidforby@users.noreply.github.com>
2024-11-08 11:25:36 -0500

    Add iOS support to CMakeLists.txt, add ouinet-ios-config.cmake and objective-C wrapper for iOS framework

---
+++ ios/ouinet/src/Ouinet.h
@@ -0,0 +1,3 @@
+#import <Foundation/Foundation.h>
+#import "ouinet/OuinetConfig.h"
+#import "ouinet/OuinetClient.h"
---
+++ ios/ouinet/src/ouinet/OuinetConfig.mm
@@ -0,0 +1,174 @@
+#import "Ouinet.h"
+
+@interface OuinetConfig()
+- (NSString*)setupInjectorTlsCert:(NSString*)ouinetDirectory;
+@end
+
+@implementation OuinetConfig
+{
+  NSString* ouinetDirectory;
+  NSString* cacheHttpPubKey;
+  NSString* injectorCredentials;
+  NSString* injectorTlsCert;
+  NSString* injectorTlsCertPath;
+  NSString* tlsCaCertStorePath;
+  NSString* cacheType;
+  NSString* listenOnTcp;
+  NSString* frontEndEp;
+  BOOL disableOriginAccess;
+  BOOL disableProxyAccess;
+  BOOL disableInjectorAccess;
+}
+
+- (OuinetConfig*)init
+{
+  if (!(self = [super init]))
+    return nil;
+  NSError *error = nil;
+  NSFileManager *fileManager = [NSFileManager defaultManager];
+  NSString *documentDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+  ouinetDirectory = [NSString stringWithFormat: @"%@%@", documentDir, @"/ouinet"];
+  NSString* ouinetConfFile = [NSString stringWithFormat: @"%@%@", ouinetDirectory, @"/ouinet-client.conf"];
+  if(![fileManager createDirectoryAtPath:ouinetDirectory withIntermediateDirectories:YES attributes:nil error:&error]) {
+    // An error has occurred, do something to handle it
+    NSLog(@"Failed to create directory \"%@\". Error: %@", ouinetDirectory, error);
+  }
+  NSLog( @"text: Got ouinetDir: %@", ouinetDirectory);
+  NSString *content = @"";
+  NSData *fileContents = [content dataUsingEncoding:NSUTF8StringEncoding];
+  [fileManager createFileAtPath:ouinetConfFile
+                                contents:fileContents
+                                attributes:nil];
+  return self;
+}
+
+- (OuinetConfig*)setCacheHttpPubKey:(NSString*)key
+{
+  cacheHttpPubKey=key;
+  return self;
+}
+
+- (OuinetConfig*)setInjectorCredentials:(NSString*)credentials
+{
+  injectorCredentials=credentials;
+  return self;
+}
+
+- (OuinetConfig*)setInjectorTlsCert:(NSString*)cert
+{
+  injectorTlsCert = cert;
+  injectorTlsCertPath = [self setupInjectorTlsCert:ouinetDirectory]; 
+  return self;
+}
+
+- (OuinetConfig*)setCacheType:(NSString*)type
+{
+  cacheType=type;
+  return self;
+}
+
+
+- (OuinetConfig*)setListenOnTcp:(NSString*)address
+{
+  listenOnTcp = address;
+  return self;
+}
+
+- (OuinetConfig*)setFrontEndEp:(NSString*)address
+{
+  frontEndEp = address;
+  return self;
+}
+
+- (OuinetConfig*)setDisableOriginAccess:(BOOL)value
+{
+  disableOriginAccess = value;
+  return self;
+}
+
+- (OuinetConfig*)setDisableProxyAccess:(BOOL)value;
+{
+  disableProxyAccess = value;
+  return self;
+}
+
+- (OuinetConfig*)setDisableInjectorAccess:(BOOL)value;
+{
+  disableInjectorAccess = value;
+  return self;
+}
+
+- (NSString*)getOuinetDirectory
+{
+  return ouinetDirectory;
+}
+
+- (NSString*)getCacheHttpPubKey
+{
+  return cacheHttpPubKey;
+}
+
+- (NSString*)getInjectorCredentials
+{
+  return injectorCredentials;
+}
+
+- (NSString*)getInjectorTlsCertPath
+{
+  return injectorTlsCertPath;
+}
+
+- (NSString*)getTlsCaCertStorePath;
+{
+  return [NSString stringWithFormat: @"%@/cacert.pem", ouinetDirectory];
+}
+
+- (NSString*)getCacheType
+{
+  return cacheType;
+}
+
+- (NSString*)getListenOnTcp
+{
+  return listenOnTcp;
+}
+
+- (NSString*)getFrontEndEp
+{
+  return frontEndEp;
+}
+
+- (BOOL)getDisableOriginAccess
+{
+  return disableOriginAccess;
+}
+
+- (BOOL)getDisableProxyAccess
+{
+  return disableProxyAccess;
+}
+
+- (BOOL)getDisableInjectorAccess
+{
+  return disableInjectorAccess;
+}
+
+/**
+  * Writes the injector TLS Cert Store to the filesystem if necessary and returns the path to the
+  * certificate.
+  */
+- (NSString*)setupInjectorTlsCert:(NSString*)ouinetDir
+{
+  if (injectorTlsCert == nil) {
+      return nil;
+  }
+  NSString* tlsCertPath = [NSString stringWithFormat: @"%@%@", ouinetDir, @"/injector-tls-cert.pem"];
+  NSString *content = injectorTlsCert; 
+  NSData *fileContents = [content dataUsingEncoding:NSUTF8StringEncoding];
+  [[NSFileManager defaultManager] createFileAtPath:tlsCertPath
+                                contents:fileContents
+                                attributes:nil];
+  return tlsCertPath;
+}
+
+@end
