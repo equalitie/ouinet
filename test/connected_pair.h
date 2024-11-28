ada3954b68658c347ee82cf1550734e3b447c3a9
Miguel <miguel@equalitie.org>
2024-11-27 20:46:00 -0600

    Use loopback endpoints to prevent Windows Firewall from blocking the tests

--- test/connected_pair.h
+++ test/connected_pair.h
@@ -16,7 +16,8 @@ connected_pair(const AsioExecutor& ex, asio::yield_context yield)
     using tcp = asio::ip::tcp;
     using Ret = pair<tcp::socket, tcp::socket>;
 
-    tcp::acceptor a(ex, tcp::endpoint(tcp::v4(), 0));
+    auto loopback_ep = tcp::endpoint(boost::asio::ip::make_address("127.0.0.1"), 0);
+    tcp::acceptor a(ex, loopback_ep);
     tcp::socket s1(ex), s2(ex);
 
     sys::error_code accept_ec;
