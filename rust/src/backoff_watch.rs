69e7c6bc8d64973f8725bc4029daefd8e94a2234
inetic1 <p.jankuliak@gmail.com>
2025-03-05 13:50:51 +0100

    Don't accumulate sends in ConstantBackoffWatchSender

--- rust/src/backoff_watch.rs
+++ rust/src/backoff_watch.rs
@@ -36,6 +36,9 @@ impl ConstantBackoffWatchSender {
 
                     last_sent = Some(Instant::now());
                     user_tx.send_modify(|_| {});
+
+                    // Don't accumulate the send events while we were sleeping.
+                    runner_rx.mark_unchanged();
                 }
             }
         });
@@ -71,5 +74,63 @@ pub struct ConstantBackoffWatchReceiver {
 impl ConstantBackoffWatchReceiver {
     pub async fn changed(&mut self) -> Result<(), watch::error::RecvError> {
         self.user_rx.changed().await
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[tokio::test]
+    async fn sanity() {
+        let delay = Duration::from_millis(500);
+        let tx = ConstantBackoffWatchSender::new(delay);
+        let mut rx = tx.subscribe();
+
+        let start = Instant::now();
+
+        // This should be received almost immediatelly.
+        tx.send_modify(|_| ());
+        rx.changed().await.unwrap();
+
+        assert!(start.elapsed() < delay);
+
+        let start = Instant::now();
+
+        // This send should be received after `delay`.
+        tx.send_modify(|_| ());
+        rx.changed().await.unwrap();
+
+        assert!(start.elapsed() >= delay);
+    }
+
+    #[tokio::test]
+    async fn dont_accumulate_sends_during_timeout() {
+        let delay = Duration::from_millis(200);
+        let tx = ConstantBackoffWatchSender::new(delay);
+        let mut rx = tx.subscribe();
+
+        // The first send will be received immediatelly.
+        tx.send_modify(|_| ());
+        rx.changed().await.unwrap();
+
+        // This will trigger the delay timeout inside the runner
+        tx.send_modify(|_| ());
+
+        // Give the runner time to receive
+        time::sleep(Duration::from_millis(100)).await;
+
+        // Send again while the runner is still waiting, this send should not accumulate
+        tx.send_modify(|_| ());
+
+        // Receive expected event
+        rx.changed().await.unwrap();
+
+        // The last send event should not accumulate so we expect this sleep to timeout
+        assert!(
+            tokio::time::timeout(Duration::from_millis(300), rx.changed())
+                .await
+                .is_err()
+        );
     }
 }
