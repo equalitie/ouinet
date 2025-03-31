469900106aff19bd79be69847de9ab530c35a16d
inetic1 <p.jankuliak@gmail.com>
2025-03-31 10:41:58 +0200

    Remove one unneeded spawn from test-i2p.cpp

--- test/test-i2p.cpp
+++ test/test-i2p.cpp
@@ -43,76 +43,82 @@ BOOST_AUTO_TEST_CASE(test_connect_and_exchage) {
     Setup setup;
 
     asio::io_context ctx;
-    auto exec = ctx.get_executor();
 
-    asio::spawn(ctx, [&] (asio::yield_context yield) {
+    struct State {
+        State(const Setup& setup, AsioExecutor exec)
+            : exec(exec)
+            , service(make_shared<I2pOuiService>(setup.tempdir.string(), exec))
+            , server_ready(exec)
+            , client_finished(exec)
+            , client_finished_lock(client_finished.lock())
+            , work_guard(exec)
+        {}
 
-        struct State {
-            State(const Setup& setup, AsioExecutor exec)
-                : service(make_shared<I2pOuiService>(setup.tempdir.string(), exec))
-                , server_ready(exec)
-                , work_guard(exec)
-            {}
+        AsioExecutor exec;
+        shared_ptr<I2pOuiService> service;
+        WaitCondition server_ready;
+        WaitCondition client_finished;
+        WaitCondition::Lock client_finished_lock;
+        string server_ep;
+        string message = "hello";
+        // TODO: We should not need this here, the i2p service should take care of it.
+        asio::executor_work_guard<AsioExecutor> work_guard;
+    };
 
-            shared_ptr<I2pOuiService> service;
-            WaitCondition server_ready;
-            string server_ep;
-            string message = "hello";
-            // TODO: We should not need this here, the i2p service should take care of it.
-            asio::executor_work_guard<AsioExecutor> work_guard;
-        };
+    auto state = make_shared<State>(setup, ctx.get_executor());
 
-        auto state = make_shared<State>(setup, exec);
+    // Server
+    asio::spawn(ctx, [state, server_ready_lock = state->server_ready.lock()] (asio::yield_context yield) {
+        auto server = state->service->build_server("i2p-private-key");
 
-        // Server
-        asio::spawn(ctx, [state, server_ready_lock = state->server_ready.lock()] (asio::yield_context yield) {
-            auto server = state->service->build_server("i2p-private-key");
+        state->server_ep = server->public_identity();
 
-            state->server_ep = server->public_identity();
+        sys::error_code ec;
 
-            sys::error_code ec;
+        server->start_listen(yield[ec]);
+        BOOST_TEST_REQUIRE(!ec, ec.message());
 
-            server->start_listen(yield[ec]);
-            BOOST_TEST_REQUIRE(!ec, ec.message());
+        server_ready_lock.release();
 
-            server_ready_lock.release();
+        GenericStream conn = server->accept(yield[ec]);
+        BOOST_TEST_REQUIRE(!ec, ec.message());
+        cout << "Server accepted connection\n";
 
-            GenericStream conn = server->accept(yield[ec]);
-            BOOST_TEST_REQUIRE(!ec, ec.message());
-            cout << "Server accepted connection\n";
+        asio::async_write(conn, asio::buffer(state->message), yield[ec]);
+        BOOST_TEST_REQUIRE(!ec, ec.message());
+        cout << "Server sent \"" << state->message << "\"\n";
 
-            asio::async_write(conn, asio::buffer(state->message), yield[ec]);
-            BOOST_TEST_REQUIRE(!ec, ec.message());
-            cout << "Server sent \"" << state->message << "\"\n";
-        });
+        state->client_finished.wait(yield);
+    });
 
 
-        // Client
-        asio::spawn(ctx, [state] (asio::yield_context yield) {
-            cout << "Await server_ready (this may take a while)\n";
-            state->server_ready.wait(yield);
-            cout << "Server is ready\n";
+    // Client
+    asio::spawn(ctx, [state = std::move(state)] (asio::yield_context yield) {
+        cout << "Await server_ready (this may take a while)\n";
+        state->server_ready.wait(yield);
+        cout << "Server is ready\n";
 
-            auto client = state->service->build_client(state->server_ep);
+        auto client = state->service->build_client(state->server_ep);
 
-            sys::error_code ec;
+        sys::error_code ec;
 
-            client->start(yield[ec]);
-            BOOST_TEST_REQUIRE(!ec, ec.message());
+        client->start(yield[ec]);
+        BOOST_TEST_REQUIRE(!ec, ec.message());
 
-            Cancel cancel;
+        Cancel cancel;
 
-            auto conn = client->connect(yield[ec], cancel);
-            BOOST_TEST_REQUIRE(!ec, ec.message());
-            cout << "Client connected\n";
+        auto conn = client->connect(yield[ec], cancel);
+        BOOST_TEST_REQUIRE(!ec, ec.message());
+        cout << "Client connected\n";
 
-            std::string buffer = "XXXXX";
-            asio::async_read(conn, asio::buffer(buffer), yield[ec]);
-            BOOST_TEST_REQUIRE(!ec, ec.message());
+        std::string buffer(state->message.size(), 'X');
+        asio::async_read(conn, asio::buffer(buffer), yield[ec]);
+        BOOST_TEST_REQUIRE(!ec, ec.message());
 
-            cout << "Client read \"" << buffer << "\"\n";
-            BOOST_REQUIRE_EQUAL(buffer, state->message);
-        });
+        cout << "Client read \"" << buffer << "\"\n";
+        BOOST_REQUIRE_EQUAL(buffer, state->message);
+
+        state->client_finished_lock.release();
     });
 
     ctx.run();
