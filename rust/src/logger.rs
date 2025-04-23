d2d36469a0a2d93293f6a6f2f4ba6960eebd8879
inetic1 <p.jankuliak@gmail.com>
2025-04-23 11:43:54 +0200

    Rust: set default log level to "debug"

--- rust/src/logger.rs
+++ rust/src/logger.rs
@@ -1,7 +1,8 @@
 use env_logger::Env;
 
 pub fn init_idempotent() {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info"))
+    // TODO: Pass log level from C++ code
+    env_logger::Builder::from_env(Env::default().default_filter_or("debug"))
         .format_timestamp(None)
         .try_init()
         .ok(); // ignore if already initialized
