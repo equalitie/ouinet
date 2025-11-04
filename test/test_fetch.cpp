30299afdfe5a7151a12f53a0f971cadfe3033cf3
inetic1 <p.jankuliak@gmail.com>
2025-11-04 14:24:36 +0100

    Test that the cached and origin responses are equal

--- test/test_fetch.cpp
+++ test/test_fetch.cpp
@@ -4,6 +4,9 @@
 #include <boost/beast/core.hpp>
 #include <boost/asio.hpp>
 #include <boost/asio/spawn.hpp>
+#include <boost/stacktrace.hpp>
+#include <boost/asio/ssl.hpp>
+#include <boost/system/result.hpp>
 #include <namespaces.h>
 #include <iostream>
 #include <chrono>
@@ -21,6 +24,7 @@ using namespace ouinet;
 using namespace std::chrono_literals;
 using namespace boost::asio::ip;
 using bittorrent::MockDht;
+namespace ssl = asio::ssl;
 
 template<class Config>
 static Config make_config(const std::vector<std::string>& args) {
@@ -34,29 +38,87 @@ static Config make_config(const std::vector<std::string>& args) {
 }
 
 using Request = http::request<http::string_body>;
-using Response = http::response<http::dynamic_body>;
+using Response = http::response<http::string_body>;
 
-Response fetch(const Client& client, asio::yield_context yield) {
-    boost::beast::tcp_stream stream(client.get_executor());
-    stream.async_connect(client.get_proxy_endpoint(), yield);
+const util::Url test_url = util::Url::from("https://gitlab.com/ceno-app/ceno-android/-/raw/main/LICENSE").value();
 
+Request build_cache_request() {
     int version = 11;
-    std::string host = "gitlab.com";
-    std::string target = "https://" + host + "/ceno-app/ceno-android/-/raw/main/LICENSE";
+    std::string host = test_url.host;
+    std::string target = test_url.reassemble();
 
-    http::request<http::string_body> req{http::verb::get, target, version};
+    Request req{http::verb::get, target, version};
     req.set(http::field::host, host);
     req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
     req.set(http_::request_group_hdr, target);
+    return req;
+}
+
+Request build_origin_request() {
+    int version = 11;
+    std::string host = test_url.host;
+    std::string target = test_url.path;
+
+    Request req{http::verb::get, target, version};
+    req.set(http::field::host, host);
+    req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+    return req;
+}
+
+Response fetch_through(const Client& client, asio::yield_context yield) {
+    boost::beast::tcp_stream stream(client.get_executor());
+    stream.async_connect(client.get_proxy_endpoint(), yield);
+
+    auto req = build_cache_request();
 
     http::async_write(stream, req, yield);
 
     beast::flat_buffer b;
-    http::response<http::dynamic_body> res;
-
+    Response res;
     http::async_read(stream, b, res, yield);
 
-    stream.socket().shutdown(tcp::socket::shutdown_both);
+    return res;
+}
+
+Response fetch_from_origin(asio::yield_context yield) {
+    auto url = test_url;
+
+    if (url.port.empty()) url.port = "443";
+    if (url.path.empty()) url.path = "/";
+
+    auto exec = yield.get_executor();
+
+    asio::ip::tcp::resolver resolver(exec);
+    auto const results = resolver.async_resolve(url.host, url.port, yield);
+
+    ssl::context ctx{ssl::context::tlsv12_client};
+    ctx.set_default_verify_paths();
+    ctx.set_verify_mode(ssl::verify_peer);
+
+    auto req = build_origin_request();
+    std::string host = req[http::field::host];
+
+    ssl::stream<boost::beast::tcp_stream> stream(exec, ctx);
+
+    if(! SSL_set_tlsext_host_name(stream.native_handle(), host.c_str())) {
+        sys::error_code ec;
+        ec.assign(static_cast<int>(::ERR_get_error()), asio::error::get_ssl_category());
+        static boost::source_location loc = BOOST_CURRENT_LOCATION;
+        sys::throw_exception_from_error(ec, loc);
+    }
+
+    stream.set_verify_callback(ssl::host_name_verification(host));
+
+    get_lowest_layer(stream).async_connect(results, yield);
+    stream.async_handshake(ssl::stream_base::client, yield);
+
+    http::async_write(stream, req, yield);
+
+    beast::flat_buffer b;
+    Response res;
+    http::async_read(stream, b, res, yield);
+
+    BOOST_REQUIRE_EQUAL(res.result(), http::status::ok);
 
     return res;
 }
@@ -122,17 +184,21 @@ BOOST_AUTO_TEST_CASE(fetch_through_injector_public) {
     leecher.start();
 
     asio::spawn(ctx, [&] (asio::yield_context yield) {
+        auto control_body = fetch_from_origin(yield).body();
+
         // The "seeder" fetches the signed content through the "injector"
-        auto rs1 = fetch(seeder, yield);
+        auto rs1 = fetch_through(seeder, yield);
 
         BOOST_CHECK_EQUAL(rs1.result(), http::status::ok);
         BOOST_CHECK_EQUAL(rs1[http_::response_source_hdr], http_::response_source_hdr_injector);
+        BOOST_CHECK_EQUAL(rs1.body(), control_body);
 
         // The "leecher" client fetches the signed content from the "seeder"
-        auto rs2 = fetch(leecher, yield);
+        auto rs2 = fetch_through(leecher, yield);
 
         BOOST_CHECK_EQUAL(rs2.result(), http::status::ok);
         BOOST_CHECK_EQUAL(rs2[http_::response_source_hdr], http_::response_source_hdr_dist_cache);
+        BOOST_CHECK_EQUAL(rs2.body(), control_body);
 
         injector.stop();
         seeder.stop();
