bba068a0addc388ad4b46264c368aff19f2fa35b
Houkime <>
2025-11-04 14:25:32 +0000

    more debug for stopping processes in tests

--- test/integration_test/test_fixtures.py
+++ test/integration_test/test_fixtures.py
@@ -24,10 +24,8 @@ class TestFixtures:
         "test_bep44_seed": BEP44_CACHE_TIMEOUT,
         "test_tcp_cache": TCP_TRANSPORT_TIMEOUT,
         "test_cache": TCP_TRANSPORT_TIMEOUT,
-        "test_wikipedia_mainline": BEP44_CACHE_TIMEOUT,
     }
 
-    MAINNET_INJECTOR_HASH = "zh6ylt6dghu6swhhje2j66icmjnonv53tstxxvj6acu64sc62fnq"
     # BENCHMARK REGEX INDICES
     READY_REGEX_INDEX = 0
     INDEX_READY_REGEX_INDEX = 1
@@ -57,7 +55,7 @@ class TestFixtures:
     RESPONSE_LENGTH = 20
 
     CLIENT_CONFIG_FILE_NAME = "ouinet-client.conf"
-    TCP_CLIENT = {"name": "tcp_client", "port": 8075, "fe_port": 8078}
+    TCP_CLIENT = {"name": "tcp_client", "port": 8075}
     TCP_CLIENT_PORT_READY_REGEX = r"[\s\S]*listening to browser requests[\s\S]*"
     TCP_CLIENT_DISCOVERY_START = (
         r"[\s\S]*LocalPeerDiscovery: starting with advertised endpoints[\s\S]*"
@@ -87,30 +85,8 @@ class TestFixtures:
     BEP44_RESPONSE_CACHED_REGEX = r"[\s\S]*BEP44 index: insertion finished[\s\S]*"
     NO_OF_CACHED_MESSAGES_REQUIRED = 1
     RETRIEVED_FROM_CACHE_REGEX = r"[\s\S]*Response was retrieved from cache[\s\S]*"
-    FRESH_SUCCESS_REGEX = r'[\s\S]*fresh/injector Finish; ec="Success"[\s\S]*'
-    DHT_CONTACTS_STORED_REGEX = r"[\s\S]*DHT: Successfully stored contacts[\s\S]*"
     MAX_NO_OF_TRIAL_CACHE_REQUESTS = 5
     TRIAL_CACHE_REQUESTS_WAIT = 20
 
     I2P_DHT_ADVERTIZE_WAIT_PERIOD = 30
     I2P_TUNNEL_HEALING_PERIOD = 10
-    INJECTOR_CERT_PATH = ""  # will be filled during setup
-    INJECTOR_CERTIFICATE = """
------BEGIN CERTIFICATE-----
-MIICyTCCAbGgAwIBAgIGAWwvE3jIMA0GCSqGSIb3DQEBCwUAMBQxEjAQBgNVBAMM
-CWxvY2FsaG9zdDAeFw0xOTA3MjQxNjE4MjFaFw0zNDA3MjIxNjE4MjFaMBQxEjAQ
-BgNVBAMMCWxvY2FsaG9zdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB
-AOQ6tX1fh1JQJGMEpgEaqFdVpl2Jz39s+3pFJAHRQMxvQa1a4pGwlc4smrhh8Y2Z
-Kli8zhIzFPATZ3ipdBwnLBBUnDqpZWEqsKdBGGJghM+8EitXJwtSWjR2qqZcz3Xz
-60MKt2S2IeL6L3/HtHM1bN93Xo3hQK/WYDQ6BEeLd6JSsns1mwwccTStu/kc3Y2E
-IXPh1otQ624QXb9szIdwQw7vzi0saXONdaFFbpRyoa6KKCEC7iHHfUbEhCSRpL8Y
-Mrl5z9mKqA8y+5tl3jzTHRtYE4SVG60pmd9nMQ33ue8m5ADq5Bd8Jg2qOmmg0KNF
-V1RHB3pljMGco6eP9zmb3jsCAwEAAaMhMB8wHQYDVR0OBBYEFMCGT2KEmo4kM08C
-E/rv/BbnmVfnMA0GCSqGSIb3DQEBCwUAA4IBAQBWxR7x1vADpkpVRzNxicLgd0CY
-itmhEWtRQp9kE33O5BjRlHQ5TTA0WBp8Nc3c5ZZ1qAnQx3tXVZ7W1QY2XjiQpsPE
-hFcPsAtFLP+kpDEFPi39iFv4gunR4M1zReCDTGTJ48bLtqONZ9XgJ7obW8r+TjuJ
-yI/i11NWUwKldg0NevF1Bkddbhpt7PJHUpSSbwr3GJOKHfRw9ZaX6P86MVcJd0Ta
-AzZPXqk+2eab43GbbD6keXRGIufMThKGyrRX+9aIaV3tx3uWAOfWVmlzf9w3gV3D
-lmjPSOXmUsOLk0PFwoy7O7n9zJKNrUy1N2O+j0tH5HVXOnSjpS8aNrMtpfHS
------END CERTIFICATE-----
-"""
