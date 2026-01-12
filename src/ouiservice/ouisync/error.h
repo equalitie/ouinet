97b8b49654ff15b467adbf36752620e0bfc74658
olaf <olaf@equalitie.org>
2026-01-12 11:16:00 +0100

    Move ouisync_service::ErrorCategory from .h to .cpp

--- src/ouiservice/ouisync/error.h
+++ src/ouiservice/ouisync/error.h
@@ -8,36 +8,6 @@ enum Error {
     // TODO:
 };
 
-class ErrorCategory : public boost::system::error_category {
-public:
-    /**
-     * Describe category
-     */
-    const char* name() const noexcept override {
-        return "ouisync::ErrorCategory";
-    }
-
-    /**
-     * Get error message
-     */
-    std::string message(int ev) const override {
-        switch (static_cast<Error>(ev)) {
-            default: return "Unknown error";
-        }
-    }
-
-    /**
-     * Map to error condition
-     */
-    boost::system::error_condition default_error_condition(int ev) const noexcept override {
-        // TODO: Map certain errors to a generic condition
-        switch (static_cast<Error>(ev)) {
-            default:
-                return boost::system::error_condition(ev, *this);
-        }
-    }
-};
-
 const boost::system::error_category& error_category();
 
 inline
