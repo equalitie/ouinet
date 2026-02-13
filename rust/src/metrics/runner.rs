e3f40643462c941a68e739771466c0d7381a8372
Miguel <miguel@equalitie.org>
2026-02-13 11:04:33 -0600

    Replace DELETE_RECORDS_AFTER with a C++ variable

--- rust/src/metrics/runner.rs
+++ rust/src/metrics/runner.rs
@@ -340,7 +340,8 @@ mod tests {
             let tmpdir = TmpDir::new("metrics_runner_store").await.unwrap();
             let dk = DecryptionKey::random(&mut rand::rng());
             let ek = EncryptionKey::from(&dk);
-            let store = Store::new(tmpdir.as_ref().into(), ek).await.unwrap();
+            // TODO: Check if we need to set a value here
+            let store = Store::new(tmpdir.as_ref().into(), ek, None).await.unwrap();
             let event_handler = EventHandler::new();
             let collector = Mutex::new(Collector::new(&runtime::Handle::current()));
 
