f1e541f4f4585605249d70c1a46f8816916e7859
algae <>
2026-02-26 15:34:52 +0000

    minimize i2pd restart test

--- test/test-i2p-restart.cpp
+++ test/test-i2p-restart.cpp
@@ -1,18 +1,16 @@
 #include "../test/util/i2p_utils.hpp"
+#include "i2pd/libi2pd/api.h"
 
 BOOST_AUTO_TEST_SUITE(ouinet_i2p)
 
 BOOST_AUTO_TEST_CASE(test_connect_and_exchange) {
     Setup setup;
     asio::io_context ctx;
+    string datadir_arg = "--datadir="+setup.tempdir.string();
+    std::vector<const char*> argv({"i2pouiservice", datadir_arg.data()});
 
-    // shared_ptr<Service> service(make_shared<Service>(setup.tempdir.string(), ctx.get_executor()));
-    {
-        Service service(setup.tempdir.string(), ctx.get_executor());
-    }
-    {
-        Service service(setup.tempdir.string(), ctx.get_executor());
-    }
+    i2p::api::InitI2P(argv.size(), (char**) argv.data(), argv[0]);
+    i2p::api::InitI2P(argv.size(), (char**) argv.data(), argv[0]);
 
 }
 
