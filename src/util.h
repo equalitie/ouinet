ba7b248e338d1c3a118d5808164b63e7d8beb8b1
Miguel <miguel@equalitie.org>
2026-04-07 10:43:03 -0600

    Fix typo in regex that covers IPv6 ULA

--- src/util.h
+++ src/util.h
@@ -63,7 +63,7 @@ static const std::string _localhost_re =
 #define _IP4_PRIV4_RE "169\\.254(?:\\.[0-9]{1,3}){2}"
 #define _IP6_PRIV1_RE "fe80::(?:%[0-9a-zA-Z]+)?"
 #define _IP6_PRIV2_RE "fe80:(?:(?:[0-9a-f]{1,4}:)*:(?:[0-9a-f]{1,4}:)*[0-9a-f]{1,4}|(?:[0-9a-f]{1,4}:){1,7}[0-9a-f]{1,4})(?:%[0-9a-zA-Z]+)?"
-#define _IP6_PRIV3_RE "f[cd][0-0a-f]{2}::(?:%[0-9a-zA-Z]+)?"
+#define _IP6_PRIV3_RE "f[cd][0-9a-f]{2}::(?:%[0-9a-zA-Z]+)?"
 #define _IP6_PRIV4_RE "f[cd][0-9a-f]{2}:(?:(?:[0-9a-f]{1,4}:)*:(?:[0-9a-f]{1,4}:)*[0-9a-f]{1,4}|(?:[0-9a-f]{1,4}:){1,7}[0-9a-f]{1,4})(?:%[0-9a-zA-Z]+)?"
 static const std::string _private_addr_re =
     "^(?:"
