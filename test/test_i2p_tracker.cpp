4c911f842ae87753a53f9ad2fa21573e3fcc7e9e
olaf <olaf@equalitie.org>
2026-04-22 09:56:04 +0200

    test_i2p_tracker.cpp: add test to receive tracker status

--- test/test_i2p_tracker.cpp
+++ test/test_i2p_tracker.cpp
@@ -3,6 +3,7 @@
 #include <boost/filesystem.hpp>
 
 #include <boost/asio/spawn.hpp>
+#include <boost/beast.hpp>
 #include <namespaces.h>
 #include <bittorrent/bep3_tracker.h>
 #include <ouiservice/i2p.h>
@@ -23,6 +24,46 @@ void handle_exception(std::exception_ptr ep) {
     catch (...) {
         BOOST_ERROR("Unknown exception");
     }
+}
+
+BOOST_AUTO_TEST_CASE(tracker_status) {
+    TestDir dir;
+
+    asio::io_context ctx;
+
+    const auto tracker_id = *I2pAddress::parse("z2tfkf4t23gig3nfybnat2qarjl2f7dctcj63khfluqt2fdoikpa.b32.i2p");
+
+    asio::spawn(ctx, [&] (asio::yield_context yield) mutable {
+        Cancel cancel;
+        sys::error_code ec;
+
+        auto service = std::make_shared<I2pService>(dir.string(), yield.get_executor(), 1);
+        auto client = service->build_client(tracker_id);
+
+        client->start(yield[ec]);
+        BOOST_TEST_REQUIRE(!ec, ec.message());
+
+        auto conn = client->connect_without_handshake(yield[ec], cancel);
+        BOOST_TEST_REQUIRE(!ec, "Client connect: " << ec.message());
+
+        std::string target = "http://" + tracker_id.value + "/a";
+
+        http::request<http::empty_body> request{http::verb::get, target, 11};
+        request.set(http::field::host, tracker_id.value);
+        request.set(http::field::user_agent, "Ouinet/1.0");
+
+        http::async_write(conn, request, yield[ec]);
+        BOOST_TEST_REQUIRE(!ec, "Client send: " << ec.message());
+
+        beast::flat_buffer buffer;
+        http::response<http::string_body> response;
+        http::async_read(conn, buffer, response, yield[ec]);
+        BOOST_TEST_REQUIRE(!ec, "Client receive: " << ec.message());
+        BOOST_TEST_REQUIRE(response.result() == http::status::ok, "Response status: " << response.result());
+    },
+    handle_exception);
+
+    ctx.run();
 }
 
 BOOST_AUTO_TEST_CASE(announce_and_get_peers) {
