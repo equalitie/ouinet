514684cb514137a8e30fa4b37e6a5b4e701b0f37
algae <>
2026-05-05 11:18:36 +0000

    explicitly check for an applied patch

--- cmake/dependencies/boost.cmake
+++ cmake/dependencies/boost.cmake
@@ -226,7 +226,7 @@ set(BOOST_PATCH_COMMAND
     cd ${OUINET_BOOST_PREFIX}/src/built_boost
 )
 foreach (patch ${BOOST_PATCHES})
-    set(BOOST_PATCH_COMMAND ${BOOST_PATCH_COMMAND} && patch -N -p1 -i ${patch} || true)
+    set(BOOST_PATCH_COMMAND ${BOOST_PATCH_COMMAND} && patch --dry-run -R -N -p1 -i ${patch} || patch -N -p1 -i ${patch})
 endforeach()
 
 execute_process(COMMAND nproc OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE NPROC)
