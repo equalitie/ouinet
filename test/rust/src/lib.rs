78bf7390a9adb678972b7c7b6a1ac985e1e65648
Adam Cigánek <adam.ciganek@proton.me>
2026-05-18 09:28:40 +0200

    Add rust test wrapper for Injector + fix lifetime issues

--- test/rust/src/lib.rs
+++ test/rust/src/lib.rs
@@ -1,14 +1,11 @@
 use std::{
-    ffi::CString,
+    ffi::{CString, c_char},
+    fmt,
     net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr},
-    pin::Pin,
 };
 
 use cxx::UniquePtr;
-use tokio::{
-    sync::oneshot,
-    task::{self, JoinHandle},
-};
+use tokio::sync::oneshot;
 
 #[cxx::bridge(namespace = "ouinet::test")]
 mod ffi {
@@ -37,19 +34,30 @@ mod ffi {
 
         type Context;
 
-        fn new_context() -> UniquePtr<Context>;
+        fn context_new() -> UniquePtr<Context>;
         fn run(self: Pin<&mut Context>) -> usize;
 
         type Client;
 
-        fn new_client(
+        fn client_new(
             ctx: Pin<&mut Context>,
             config: &[*const c_char],
             log_tag: &str,
         ) -> Result<UniquePtr<Client>>;
         fn start(self: Pin<&mut Client>);
-        fn stop(client: Pin<&mut Client>, completer: Box<Completer>);
-        fn get_proxy_endpoint(client: &Client) -> SocketAddr;
+        fn client_stop(client: UniquePtr<Client>, completer: Box<Completer>);
+        fn client_get_proxy_endpoint(client: &Client) -> SocketAddr;
+
+        type Injector;
+
+        fn injector_new(
+            ctx: Pin<&mut Context>,
+            config: &[*const c_char],
+            log_tag: &str,
+        ) -> Result<UniquePtr<Injector>>;
+        fn injector_stop(injector: UniquePtr<Injector>, completer: Box<Completer>);
+        fn injector_cache_http_public_key(injector: &Injector) -> String;
+        fn injector_tls_cert_file(injector: &Injector) -> String;
     }
 }
 
@@ -69,99 +77,146 @@ impl From<ffi::SocketAddr> for SocketAddr {
 
 pub struct Context {
     inner: UniquePtr<ffi::Context>,
-    run_handle: Option<JoinHandle<()>>,
 }
 
 impl Context {
     pub fn new() -> Self {
         Self {
-            inner: ffi::new_context(),
-            run_handle: None,
+            inner: ffi::context_new(),
         }
     }
 
+    // This function is blocking
     pub fn run(&mut self) {
-        if self.run_handle.is_some() {
-            return;
-        }
-
-        let ptr = self.inner.as_mut_ptr();
-        let ptr = ptr as usize;
-
-        let run_handle = task::spawn_blocking(move || {
-            let ptr = ptr as *mut ffi::Context;
-            let ctx = unsafe { &mut *ptr };
-            let ctx = unsafe { Pin::new_unchecked(ctx) };
-            ctx.run();
-        });
-
-        self.run_handle = Some(run_handle);
-    }
-
-    pub fn is_running(&self) -> bool {
-        self.run_handle.is_some()
-    }
-
-    pub async fn stopped(&mut self) {
-        if let Some(handle) = self.run_handle.take() {
-            handle.await.unwrap();
-        }
+        self.inner.pin_mut().run();
     }
 }
 
 pub struct Client {
-    inner: UniquePtr<ffi::Client>,
+    inner: Option<UniquePtr<ffi::Client>>,
 }
 
 impl Client {
     pub fn new(ctx: &mut Context, config: Config, log_tag: &str) -> Result<Self, anyhow::Error> {
-        let argv: Vec<_> = config.args.iter().map(|a| a.as_ptr()).collect();
-
         Ok(Self {
-            inner: ffi::new_client(ctx.inner.pin_mut(), &argv, log_tag)?,
+            inner: Some(ffi::client_new(ctx.inner.pin_mut(), &config.args, log_tag)?),
         })
     }
 
     pub fn start(&mut self) {
-        self.inner.pin_mut().start();
+        self.inner.as_mut().unwrap().pin_mut().start();
     }
 
-    pub async fn stop(&mut self) {
-        let (tx, rx) = oneshot::channel();
-        let tx = Completer { tx: Some(tx) };
-        let tx = Box::new(tx);
-
-        ffi::stop(self.inner.pin_mut(), tx);
-
-        rx.await.ok();
+    pub async fn stop(mut self) {
+        if let Some(inner) = self.inner.take() {
+            Completer::wait(|tx| ffi::client_stop(inner, Box::new(tx))).await;
+        }
     }
 
     pub fn get_proxy_endpoint(&self) -> SocketAddr {
-        ffi::get_proxy_endpoint(&self.inner).into()
+        ffi::client_get_proxy_endpoint(self.inner.as_ref().unwrap()).into()
     }
 }
 
+impl Drop for Client {
+    fn drop(&mut self) {
+        if let Some(inner) = self.inner.take() {
+            ffi::client_stop(inner, Box::new(Completer::detached()));
+        }
+    }
+}
+
+impl fmt::Debug for Client {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Client").finish_non_exhaustive()
+    }
+}
+
+// SAFETY: All non thread-safe operations are invoked via `asio::post`.
+unsafe impl Send for Client {}
+
+pub struct Injector {
+    inner: Option<UniquePtr<ffi::Injector>>,
+}
+
+impl Injector {
+    pub fn new(ctx: &mut Context, config: Config, log_tag: &str) -> Result<Self, anyhow::Error> {
+        Ok(Self {
+            inner: Some(ffi::injector_new(
+                ctx.inner.pin_mut(),
+                &config.args,
+                log_tag,
+            )?),
+        })
+    }
+
+    pub async fn stop(mut self) {
+        if let Some(inner) = self.inner.take() {
+            Completer::wait(|tx| ffi::injector_stop(inner, Box::new(tx))).await;
+        }
+    }
+
+    pub fn cache_http_public_key(&self) -> String {
+        ffi::injector_cache_http_public_key(self.inner.as_ref().unwrap())
+    }
+
+    pub fn tls_cert_file(&self) -> String {
+        ffi::injector_tls_cert_file(self.inner.as_ref().unwrap())
+    }
+}
+
+impl Drop for Injector {
+    fn drop(&mut self) {
+        if let Some(inner) = self.inner.take() {
+            ffi::injector_stop(inner, Box::new(Completer::detached()));
+        }
+    }
+}
+
+impl fmt::Debug for Injector {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Injector").finish_non_exhaustive()
+    }
+}
+
+// SAFETY: All non thread-safe operations are invoked via `asio::post`.
+unsafe impl Send for Injector {}
+
 pub struct Config {
-    args: Vec<CString>,
+    args: Vec<*const c_char>,
 }
 
 impl Config {
     pub fn new() -> Self {
         Self {
             // the 0-th arg needs to be the executable name. Use a dummy one here.
-            args: vec![c"_".into()],
+            args: vec![c"_".to_owned().into_raw()],
         }
     }
 
     pub fn flag(mut self, name: impl Into<String>) -> Self {
-        self.args.push(CString::new(name.into()).unwrap());
+        self.args
+            .push(CString::new(name.into()).unwrap().into_raw());
         self
     }
 
     pub fn arg(mut self, name: impl Into<String>, value: impl ToString) -> Self {
-        self.args.push(CString::new(name.into()).unwrap());
-        self.args.push(CString::new(value.to_string()).unwrap());
+        self.args
+            .push(CString::new(name.into()).unwrap().into_raw());
+        self.args
+            .push(CString::new(value.to_string()).unwrap().into_raw());
         self
+    }
+}
+
+impl Drop for Config {
+    fn drop(&mut self) {
+        for arg in self.args.drain(..) {
+            // SAFETY: The pointers were obtained from CString::into_raw
+            unsafe {
+                let _ = CString::from_raw(arg as *mut _);
+            }
+        }
     }
 }
 
@@ -170,6 +225,21 @@ pub struct Completer {
 }
 
 impl Completer {
+    pub async fn wait<F>(f: F)
+    where
+        F: FnOnce(Self),
+    {
+        let (tx, rx) = oneshot::channel();
+        let tx = Self { tx: Some(tx) };
+        f(tx);
+
+        rx.await.ok();
+    }
+
+    pub fn detached() -> Self {
+        Self { tx: None }
+    }
+
     pub fn complete(&mut self) {
         if let Some(tx) = self.tx.take() {
             tx.send(()).ok();
@@ -177,6 +247,6 @@ impl Completer {
     }
 
     pub fn is_closed(&self) -> bool {
-        self.tx.as_ref().map(|tx| tx.is_closed()).unwrap_or(true)
+        self.tx.as_ref().map(|tx| tx.is_closed()).unwrap_or(false)
     }
 }
