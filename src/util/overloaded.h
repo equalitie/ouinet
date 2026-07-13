098ee7c11b744eaca24415c619a6055448eaefe7
olaf <olaf@equalitie.org>
2026-07-13 15:14:03 +0200

    Add Promise::Future::make_ready fn

--- src/util/overloaded.h
+++ src/util/overloaded.h
@@ -4,6 +4,20 @@ namespace ouinet {
 
 // Utility for using std::variant
 // See: https://en.cppreference.com/w/cpp/utility/variant/visit.html
+//
+// Example:
+//   std::variant<std::string, int> v;
+//
+//   std::visit(overloaded {
+//           [&] (std::string const& s) {
+//               std::cout << s;
+//           },
+//           [&] (int i) {
+//               std::cout << i;
+//           },
+//       },
+//       v);
+
 template<class... Ts>
 struct overloaded : Ts... { using Ts::operator()...; };
 
