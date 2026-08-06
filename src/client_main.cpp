b26bad4970131cec6b23f731aa544c3492d9bd1d
Veggie Monster <vmon@riseup.net>
2026-08-06 11:06:56 -0400

    Don't Ignore SIGPIPE.

--- src/client_main.cpp
+++ src/client_main.cpp
@@ -1,7 +1,6 @@
 #include "client.h"
 #include "logger.h"
 #include "force_exit_on_signal.h"
-#include <csignal>
 #include <iostream>
 
 using namespace ouinet;
@@ -9,11 +8,6 @@ using namespace std;
 
 int main(int argc, const char* argv[])
 {
-    // When stdout/stderr is piped to a slow reader (e.g. the pytest harness),
-    // a filled pipe + write() raises SIGPIPE, whose default action terminates
-    // the process. Ignore it so heavy logging under tests doesn't kill us.
-    std::signal(SIGPIPE, SIG_IGN);
-
     ClientConfig cfg;
 
     try {
