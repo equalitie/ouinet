7df2c682700a00478c5cf5d031f211f0f1fa56bc
Miguel <miguel@equalitie.org>
2026-09-23 14:59:34 -0600

    Revert "Temporarily use a sandbox injector"
    This reverts commit dd3dd967b16366c1c422395d2172aff8209fc57b.

--- test/test_fetch_random_pages.cpp
+++ test/test_fetch_random_pages.cpp
@@ -16,7 +16,7 @@
 using namespace std;
 using namespace ouinet;
 
-namespace test_constants = test::constants::sandbox;
+namespace test_constants = test::constants::ceno;
 
 using Request = http::request<http::string_body>;
 using Response = http::response<http::string_body>;
